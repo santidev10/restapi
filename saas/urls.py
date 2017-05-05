6c21ac6e9c1a7296c12ac48793c7242746b7aa5f
Pylypenko, Heorhii <heorhii.pylypenko@sigma.software>
2017-05-05 16:15:12 +0300

    SAAS-44: optimize imports

--- saas/urls.py
+++ saas/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+
 from userprofile.api import urls as userprofile_api_urls
 
 urlpatterns = [
