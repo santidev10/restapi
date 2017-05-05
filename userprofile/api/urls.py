29922926d9025dbb1238d35784add993ea3d1e17
Pylypenko, Heorhii <heorhii.pylypenko@sigma.software>
2017-05-05 17:48:36 +0300

    SAAS-28: add user login/logout endpoint

--- userprofile/api/urls.py
+++ userprofile/api/urls.py
@@ -3,8 +3,9 @@ Userprofile api urls module
 """
 from django.conf.urls import url
 
-from userprofile.api.views import UserCreateApiView
+from userprofile.api.views import UserCreateApiView, UserAuthApiView
 
 urlpatterns = [
     url(r'^users/$', UserCreateApiView.as_view(), name="user_create"),
+    url(r'^auth/$', UserAuthApiView.as_view(), name="user_auth"),
 ]
--- userprofile/api/views.py
+++ userprofile/api/views.py
@@ -2,9 +2,11 @@
 Userprofile api views module
 """
 from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.generics import CreateAPIView, GenericAPIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_201_CREATED, HTTP_401_UNAUTHORIZED
 
 from userprofile.api.serializers import UserCreateSerializer, UserSerializer
 
@@ -27,3 +29,31 @@ class UserCreateApiView(CreateAPIView):
         user = serializer.save()
         response_data = self.retrieve_serializer_class(user).data
         return Response(response_data, status=HTTP_201_CREATED)
+
+
+class UserAuthApiView(GenericAPIView):
+    """
+    Login / logout endpoint
+    """
+    permission_classes = tuple()
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        """
+        Login user
+        """
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        Token.objects.get_or_create(user=user)
+        response_data = self.serializer_class(user).data
+        return Response(response_data)
+
+    def delete(self, request):
+        """
+        Logout user
+        """
+        if not request.user.is_authenticated():
+            return Response(status=HTTP_401_UNAUTHORIZED)
+        Token.objects.get(user=request.user).delete()
+        return Response()
