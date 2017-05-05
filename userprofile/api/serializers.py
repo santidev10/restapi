e123bcc4a3eed197b171ad2171f19330ea31321b
Pylypenko, Heorhii <heorhii.pylypenko@sigma.software>
2017-05-05 19:14:38 +0300

    SAAS-28: fix user register endpoint, add 'auto_login' ability to auth endpoint

--- userprofile/api/serializers.py
+++ userprofile/api/serializers.py
@@ -4,7 +4,7 @@ Userprofile api serializers module
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from rest_framework.serializers import ModelSerializer, CharField, \
-    ValidationError, SerializerMethodField
+    ValidationError, SerializerMethodField, RegexValidator
 
 
 class UserCreateSerializer(ModelSerializer):
@@ -12,6 +12,14 @@ class UserCreateSerializer(ModelSerializer):
     Serializer for create user
     """
     verify_password = CharField(max_length=255, required=True)
+    company = CharField(max_length=255, required=True)
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered"
+                " in the format: '+999999999'. Up to 15 digits allowed."
+    )
+    phone_number = CharField(
+        max_length=15, required=True, validators=[phone_regex])
 
     class Meta:
         """
