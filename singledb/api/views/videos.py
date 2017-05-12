ee567f509e2a8582d4b154f1b8840e6d23aba0f0
Andrii Dobrovolskyi <andrii.dobrovolskyi@sigma.software>
2017-05-12 12:31:45 +0300

    SAAS-73 current update for video filters

--- singledb/api/views/videos.py
+++ singledb/api/views/videos.py
@@ -25,15 +25,11 @@ class RetrieveUpdateDeleteApiView(RetrieveUpdateDestroyAPIView):
 class ListFiltersApiView(APIView):
     permission_classes = tuple()
     allowed_filters = ["channels", "languages", "statuses", "categories", "countries", "content_owners"]
-    trending_allowed_filters = ["categories", "languages", "countries", "networks"]
-    default_trending = 'viral'
 
-    def prepare_queryset(self, trending=False, segment_id=None):
-        if trending:
-            name = self.request.query_params.get("trending_name")
-            if name not in TRENDINGS.fget():
-                name = self.default_trending
-            model = get_trending_model_by_name(name)
+    def prepare_queryset(self):
+        trending = self.request.query_params.get("trending")
+        if trending in TRENDINGS.fget():
+            model = get_trending_model_by_name(trending)
             queryset = model.objects.filter(rate__gt=0)
             video_ids = list(set(queryset.values_list('video', flat=True)))
             queryset = Video.objects.filter(id__in=video_ids)
@@ -41,23 +37,15 @@ class ListFiltersApiView(APIView):
             queryset = Video.objects.all()
         return queryset
 
-    def call_generator(self, trending=False, segment_id=None, user=None):
-        video_filter = self.request.query_params.get('filter')
-        queryset = self.prepare_queryset(trending, segment_id)
-        filters_generator = VideoFiltersGenerator(queryset)
-        generator = getattr(filters_generator, "generate_{}_filter".format(video_filter))
-        return generator()
-
     def get(self, request):
         error_filter_response = Response({"error": ["invalid filter"]}, HTTP_400_BAD_REQUEST)
         video_filter = request.query_params.get('filter')
-        trending = request.query_params.get("trending")
-        trending = isinstance(trending, str) and trending == "1"
-        allowed = self.trending_allowed_filters if trending else self.allowed_filters
-        if video_filter not in allowed:
-            return error_filter_response
-        requested_filter = self.call_generator(trending=trending)
-        return Response(data=requested_filter)
+        if video_filter not in self.allowed_filters:
+            return Response( {"error": ["invalid filter"]}, HTTP_400_BAD_REQUEST)
+        queryset = self.prepare_queryset()
+        filters_generator = VideoFiltersGenerator(queryset)
+        requested_filter = getattr(filters_generator, "generate_{}_filter".format(video_filter))
+        return Response(data=requested_filter())
 
 
 class ListApiView(ListAPIView):
