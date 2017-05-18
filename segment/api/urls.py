1a676f733e08ecca4704b2f811595aebdc1d3edf
Pylypenko, Heorhii <heorhii.pylypenko@sigma.software>
2017-05-18 15:00:02 +0300

    SAAS-50: move segment channel list endpoint functionality to channel list endpoint

--- segment/api/urls.py
+++ segment/api/urls.py
@@ -3,12 +3,9 @@ Segment api urls endpoint
 """
 from django.conf.urls import url
 
-from segment.api.views import SegmentListCreateApiView, \
-    SegmentChannelListApiView
+from segment.api.views import SegmentListCreateApiView
 
 urlpatterns = [
     url(r'^segments/$', SegmentListCreateApiView.as_view(),
         name="segment_list"),
-    url(r'^segments/(?P<pk>\d+)/channels/$',
-        SegmentChannelListApiView.as_view(), name="segment_list"),
 ]
