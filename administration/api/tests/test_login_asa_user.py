aa19a9a3b371223517ba65bfa6f6952937400ee8
astrukov <aleksey.stryukov@gmail.com>
2017-05-18 15:11:04 +0300

    SAAS-72 Admin > Users: add ability to log in as a user

--- administration/api/tests/test_login_asa_user.py
+++ administration/api/tests/test_login_asa_user.py
@@ -2,16 +2,15 @@ from django.core.urlresolvers import reverse
 from rest_framework.status import HTTP_200_OK, HTTP_403_FORBIDDEN, \
     HTTP_404_NOT_FOUND
 from saas.utils_tests import ExtendedAPITestCase
+from django.contrib.auth import get_user_model
 
 
 class LoginAsAUserAPITestCase(ExtendedAPITestCase):
 
     def test_fail_permission(self):
         self.user = self.create_test_user()
-        test_user = self.create_test_user(auth=False)
-
         url = reverse("admin_api_urls:user_auth_admin",
-                      args=(test_user.id,))
+                      args=(self.user.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
 
@@ -29,10 +28,9 @@ class LoginAsAUserAPITestCase(ExtendedAPITestCase):
         self.user = self.create_test_user()
         self.user.is_staff = True
         self.user.save()
-        test_user = self.create_test_user(auth=False)
-        print(self.user.id)
-        print(test_user.id)
-
+        test_user = get_user_model().objects.create(
+            email="mr_bond_james_bond@mail.kz"
+        )
         url = reverse("admin_api_urls:user_auth_admin",
                       args=(test_user.id,))
         response = self.client.get(url)
