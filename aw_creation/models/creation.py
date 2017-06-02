684f45c76362de068e1490f19fc35a4960c1df67
astrukov <aleksey.stryukov@gmail.com>
2017-06-02 09:59:09 +0300

    SAAS-230 MB > Track: no clicks data on hourly trends, though clicks are available on daily trends

--- aw_creation/models/creation.py
+++ aw_creation/models/creation.py
@@ -515,7 +515,7 @@ class FrequencyCap(models.Model):
     )
     level = models.CharField(max_length=20, choices=LEVELS,
                              default=CAMPAIGN_LVL)
-    limit = models.PositiveSmallIntegerField(
+    limit = models.PositiveIntegerField(
         validators=[MinValueValidator(1)],
     )
     DAY_TIME_UNIT = "DAY"
