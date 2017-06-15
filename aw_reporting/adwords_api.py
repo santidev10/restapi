d78989fdc1de6d621fb4aee1d1bf07de25cd739a
Alexander Dobrzhansky <alexander.dobrzhansky@sigma.software>
2017-06-15 16:47:22 +0300

    SAAS-286 update vertical keywords segments

--- aw_reporting/adwords_api.py
+++ aw_reporting/adwords_api.py
@@ -45,8 +45,9 @@ def get_client(**kwargs):
             return client_obj
 
 
-def optimize_keyword(query, client=None):
+def optimize_keyword(query, client=None, request_type='IDEAS'):
     service_client = client or get_client()
+    request_type = request_type
     offset = 0
     page_size = 1000
 
@@ -83,7 +84,7 @@ def optimize_keyword(query, client=None):
             },
         ],
         'ideaType': 'KEYWORD',
-        'requestType': 'IDEAS',
+        'requestType': request_type,
 
         'requestedAttributeTypes': ['KEYWORD_TEXT', 'SEARCH_VOLUME',
                                     'CATEGORY_PRODUCTS_AND_SERVICES',
