117ab2c28169e5bb560fe7066140e840a0a1244a
Alexander Dobrzhansky <alexander.dobrzhansky@sigma.software>
2017-08-08 14:52:10 +0300

    SAAS-595 review comments

--- keyword_tool/management/commands/keyword_tool_remove_old_data.py
+++ keyword_tool/management/commands/keyword_tool_remove_old_data.py
@@ -14,13 +14,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         expired_date = timezone.now().date() - timedelta(days=7)
         Query.objects.filter(updated_at__lte=expired_date).delete()
-        self.filter_by_volume()
+        self.delete_by_volume()
 
-    def filter_by_volume(self):
+    def delete_by_volume(self):
         KeyWord.objects.filter(search_volume__lte=100000).delete()
 
-    def filter_by_stats(self, expired_date):
-        keywords_res = KeyWord.objects.filter(
+    def delete_by_stats(self, expired_date):
+        KeyWord.objects.filter(
             Q(updated_at__lte=expired_date) &
             (Q(average_cpc__isnull=True) | Q(average_cpc__lte=0)) &
             (Q(competition__isnull=True) | Q(competition__lte=0)) &
