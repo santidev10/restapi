7299f73e941f2a07962bacbd288fe5322a77b36d
Pylypenko, Heorhii <heorhii.pylypenko@sigma.software>
2017-10-02 18:50:02 +0300

    SAAS-976: clean up

--- channel/api/views.py
+++ channel/api/views.py
@@ -2,7 +2,6 @@
 Channel api views module
 """
 from django.db.models import Q
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.status import HTTP_408_REQUEST_TIMEOUT, HTTP_404_NOT_FOUND
