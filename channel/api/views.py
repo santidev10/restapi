1db70f10fb4f4b9dba5e590032d463d1eca16815
Alexander Bykov <alexander.bykov@sigma.software>
2017-10-06 14:41:40 +0300

    Incorporated export from release/2.0

--- channel/api/views.py
+++ channel/api/views.py
@@ -1,6 +1,9 @@
 """
 Channel api views module
 """
+from copy import deepcopy
+import re
+
 from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -14,6 +17,7 @@ from segment.models import SegmentChannel
 from singledb.api.views.base import SingledbApiView
 from singledb.connector import SingleDatabaseApiConnector as Connector, \
     SingleDatabaseApiConnectorException
+from utils.csv_export import list_export
 # pylint: enable=import-error
 
 
@@ -21,6 +25,22 @@ class ChannelListApiView(APIView):
     """
     Proxy view for channel list
     """
+    fields_to_export = [
+        "title",
+        "url",
+        "country",
+        "category",
+        "emails",
+        "description",
+        "subscribers",
+        "thirty_days_subscribers",
+        "thirty_days_views",
+        "views_per_video",
+        "sentiment",
+        "engage_rate"
+    ]
+    export_file_title = "channel"
+
     def obtain_segment(self, segment_id):
         """
         Try to get segment from db
@@ -36,12 +56,13 @@ class ChannelListApiView(APIView):
             return None
         return segment
 
+    @list_export
     def get(self, request):
         """
         Get procedure
         """
         # prepare query params
-        query_params = request.query_params
+        query_params = deepcopy(request.query_params)
         query_params._mutable = True
         # segment
         segment = query_params.get("segment")
@@ -62,6 +83,10 @@ class ChannelListApiView(APIView):
                 return Response(empty_response)
             query_params.pop("segment")
             query_params.update(ids=",".join(channels_ids))
+
+        # adapt the request params
+        self.adapt_query_params(query_params)
+
         # make call
         connector = Connector()
         try:
@@ -70,7 +95,102 @@ class ChannelListApiView(APIView):
             return Response(
                 data={"error": " ".join(e.args)},
                 status=HTTP_408_REQUEST_TIMEOUT)
+
+        # adapt the response data
+        self.adapt_response_data(response_data)
+
         return Response(response_data)
+
+    @staticmethod
+    def adapt_query_params(query_params):
+        """
+        Adapt SDB request format
+        """
+        # sorting --->
+        sorting = query_params.pop("sort_by", ["subscribers"])[0]
+        if sorting in ["subscribers", "sentiment", "views_per_video", "thirty_days_views", "thirty_days_subscribers", "score_total"]:
+            query_params.update(sort="{}:desc".format(sorting))
+        elif sorting == "engagement":
+            query_params.update(sort="engage_rate:desc")
+        else:
+            query_params.update(sort="subscribers:desc")
+        # <--- sorting
+
+        # filters --->
+        def make_range(name, name_min=None, name_max=None):
+            if name_min is None:
+                name_min = "min_{}".format(name)
+            if name_max is None:
+                name_max = "max_{}".format(name)
+            _range = [
+                query_params.pop(name_min, [None])[0],
+                query_params.pop(name_max, [None])[0],
+            ]
+            _range = [str(v) if v is not None else '' for v in _range]
+            _range = ','.join(_range)
+            if _range != ',':
+                query_params.update(**{"{}__range".format(name): _range})
+
+        def make(_type, name, name_in=None):
+            if name_in is None:
+                name_in = name
+            value = query_params.pop(name_in, [None])[0]
+            if value is not None:
+                query_params.update(**{"{}__{}".format(name, _type): value})
+
+        # min_subscribers_yt, max_subscribers_yt
+        make_range('subscribers', 'min_subscribers_yt', 'max_subscribers_yt')
+
+        # country
+        make('terms', 'country')
+
+        # language
+        make('terms', 'language')
+
+        # min_thirty_days_subscribers, max_thirty_days_subscribers
+        make_range('thirty_days_subscribers')
+
+        # min_thirty_days_views, max_thirty_days_views
+        make_range('thirty_days_views')
+
+        # min_sentiment, max_sentiment
+        make_range('sentiment')
+
+        # min_engage_rate, max_engage_rate
+        make_range('engage_rate')
+
+        # min_views_per_video, max_views_per_video
+        make_range('views_per_video')
+
+        # category
+        category = query_params.pop('category', [None])[0]
+        if category is not None:
+            regexp = '|'.join(['.*' + c + '.*' for c in category.split(',')])
+            query_params.update(category__regexp=regexp)
+
+        # search
+        make('term', 'text_search', 'search')
+        # <--- filters
+
+    @staticmethod
+    def adapt_response_data(response_data):
+        """
+        Adapt SDB response format
+        """
+        items = response_data.get("items", [])
+        for item in items:
+            if "channel_id" in item:
+                item["id"] = item.get("channel_id", "")
+                del item["channel_id"]
+            if "history_date" in item:
+                item["history_date"] = item["history_date"][:10]
+            if "youtube_published_at" in item:
+                item["youtube_published_at"] = re.sub(
+                    "^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$",
+                    "\g<0>Z",
+                    item["youtube_published_at"]
+                )
+        return response_data
 
 
 class ChannelListFiltersApiView(SingledbApiView):
@@ -84,7 +204,7 @@ class ChannelRetrieveUpdateApiView(SingledbApiView):
 
     def put(self, *args, **kwargs):
         if 'channel_group' in self.request.data \
-            and self.request.data['channel_group'] not in ['influencers','new','media','brands']:
+           and self.request.data['channel_group'] not in ['influencers', 'new', 'media', 'brands']:
                 return Response(status=HTTP_400_BAD_REQUEST)
         return super().put(*args, **kwargs)
 
---
+++ utils/csv_export.py
@@ -0,0 +1,92 @@
+"""
+CSV export mechanism module for objects list
+"""
+import csv
+from io import StringIO
+
+from django.http import StreamingHttpResponse
+from django.utils import timezone
+
+
+class CSVExportException(Exception):
+    """
+    Exception class from csv export
+    """
+    pass
+
+
+class CSVExport(object):
+    """
+    Class for csv export
+    """
+    def __init__(self, fields, data, file_title):
+        """
+        Set up params
+        """
+        self.fields = fields
+        self.data = data
+        self.file_title = file_title
+
+    def export_generator(self):
+        """
+        Export data generator
+        """
+        yield self.fields
+        for obj in self.data:
+            row = []
+            for field in self.fields:
+                row.append(obj.get(field))
+            yield row
+
+    def prepare_csv_file_response(self):
+        """
+        Prepare streaming response obj
+        :return: file response
+        """
+        def stream_generator():
+            for row in self.export_generator():
+                output = StringIO()
+                writer = csv.writer(output)
+                writer.writerow(row)
+                yield output.getvalue()
+
+        response = StreamingHttpResponse(
+            stream_generator(), content_type='text/csv')
+        filename = "{title}_export_report {date}.csv".format(
+            title=self.file_title,
+            date=timezone.now().strftime("%d-%m-%Y.%H:%M%p")
+        )
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(
+            filename)
+        return response
+
+
+def list_export(method):
+    """
+    Decorator function for objects list export
+    """
+    def wrapper_get(self, request, *args, **kwargs):
+        """
+        Extended get method
+        """
+        is_export = request.query_params.get("export")
+        if is_export == "1":
+            # check required export options
+            assert self.fields_to_export and self.export_file_title
+            # prepare api call
+            request.query_params._mutable = True
+            request.query_params["flat"] = "1"
+            response = method(
+                self=self, request=request, *args, **kwargs)
+            if response.status_code > 300:
+                return response
+            # generate csv file
+            csv_generator = CSVExport(
+                fields=self.fields_to_export,
+                data=response.data, file_title=self.export_file_title)
+            response = csv_generator.prepare_csv_file_response()
+            return response
+        response = method(
+            self=self, request=request, *args, **kwargs)
+        return response
+    return wrapper_get
