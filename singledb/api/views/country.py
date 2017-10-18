4163aa7b84d2db952d087a751b4645084f40c799
Alexander Bykov <alexander.bykov@sigma.software>
2017-10-18 16:03:43 +0300

    Fixed permissions

--- singledb/api/views/country.py
+++ singledb/api/views/country.py
@@ -3,4 +3,5 @@ from singledb.connector import SingleDatabaseApiConnector as Connector
 
 
 class CountryListApiView(SingledbApiView):
+    permission_required = tuple()
     connector_get = Connector().get_country_list
