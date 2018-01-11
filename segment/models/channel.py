bd8ac825b5caa7738c5e9f4ca94552d4f31ae377
Alexander Bykov <alexander.bykov@sigma.software>
2018-01-11 11:56:01 +0200

    channel query sources

--- segment/models/channel.py
+++ segment/models/channel.py
@@ -8,6 +8,8 @@ from django.db import models
 
 from aw_reporting.models import YTChannelStatistic
 from singledb.connector import SingleDatabaseApiConnector as Connector
+from singledb.settings import DEFAULT_CHANNEL_LIST_SOURCES, \
+    DEFAULT_VIDEO_LIST_SOURCES
 from .base import BaseSegment
 from .base import BaseSegmentRelated
 from .base import SegmentManager
@@ -64,6 +66,7 @@ class SegmentChannel(BaseSegment):
         params = {
             "ids_hash": ids_hash,
             "fields": "channel_id,title,thumbnail_image_url",
+            "sources": DEFAULT_CHANNEL_LIST_SOURCES,
             "sort": "subscribers:desc",
             "size": 3
         }
@@ -72,6 +75,7 @@ class SegmentChannel(BaseSegment):
         params = {
             "ids_hash": ids_hash,
             "fields": "videos",
+            "sources": DEFAULT_VIDEO_LIST_SOURCES,
             "size": 10000
         }
         base_data = self.singledb_method(query_params=params)
