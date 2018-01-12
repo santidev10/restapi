e26f101acab8b44ce3bf0ddfb8dd44b29107d828
Alexander Bykov <alexander.bykov@sigma.software>
2018-01-12 12:20:19 +0200

    refactor

---
+++ segment/models/__init__.py
@@ -0,0 +1,7 @@
+from .base import BaseSegment
+from .channel import SegmentChannel
+from .channel import SegmentRelatedChannel
+from .keyword import SegmentKeyword
+from .keyword import SegmentRelatedKeyword
+from .video import SegmentVideo
+from .video import SegmentRelatedVideo
