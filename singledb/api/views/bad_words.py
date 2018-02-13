8f8fd466ec8a6e3b5ee4f61314fa32b6796e2b1f
Alexander Dobrzhansky <alexander.dobrzhansky@sigma.software>
2018-02-13 17:26:09 +0200

    SAAS-1943 BS > New section in admin for managing

---
+++ singledb/api/views/bad_words.py
@@ -0,0 +1,27 @@
+from rest_framework.permissions import IsAdminUser
+
+from singledb.api.views import SingledbApiView
+from singledb.connector import SingleDatabaseApiConnector as Connector
+
+
+class BadWordListApiView(SingledbApiView):
+    permission_classes = (IsAdminUser, )
+    connector_get = Connector().get_bad_words_list
+    connector_post = Connector().post_bad_word
+
+
+class BadWordRetrieveUpdateDeleteApiView(SingledbApiView):
+    permission_classes = (IsAdminUser, )
+    connector_get = Connector().get_bad_word
+    connector_put = Connector().put_bad_word
+    connector_delete = Connector().delete_bad_word
+
+
+class BadWordHistoryListApiView(SingledbApiView):
+    permission_classes = (IsAdminUser, )
+    connector_get = Connector().get_bad_words_history_list
+
+
+class BadWordCategoriesListApiView(SingledbApiView):
+    permission_classes = (IsAdminUser, )
+    connector_get = Connector().get_bad_words_categories_list
