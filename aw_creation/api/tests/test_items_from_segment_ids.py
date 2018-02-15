612234d741f89d6ca5782e5e9c55661b8e649094
Alexander Dobrzhansky <alexander.dobrzhansky@sigma.software>
2018-02-15 18:08:59 +0200

    SAAS-2025 Fixed tests

--- aw_creation/api/tests/test_items_from_segment_ids.py
+++ aw_creation/api/tests/test_items_from_segment_ids.py
@@ -75,26 +75,27 @@ class ItemsFromIdsAPITestCase(ExtendedAPITestCase):
         if settings.DATABASES['default']['ENGINE'] != "django.db.backends.postgresql_psycopg2":
             logger.warning("This test requires postgres")
             return
-
-        from keyword_tool.models import KeywordsList, KeyWord
+        from segment.models import SegmentKeyword, SegmentRelatedKeyword
         ids = []
         item_ids = []
         keywords = ["spam", "ham", "test", "batman"]
         j = 0
         for i in range(2):
-            segment = KeywordsList.objects.create()
+            segment = SegmentKeyword.objects.create()
             for _ in range(2):
-                item = KeyWord.objects.create(text=keywords[j])
-                segment.keywords.add(item)
-                item_ids.append(item.text)
+                item = SegmentRelatedKeyword.objects.create(segment=segment, related_id=keywords[j])
+                item_ids.append(item.related_id)
                 j += 1
             ids.append(segment.id)
 
         url = reverse("aw_creation_urls:items_from_segment_ids",
                       args=("keyword",))
-        response = self.client.post(
-            url, json.dumps(ids), content_type='application/json',
-        )
+        with patch("aw_creation.api.serializers.SingleDatabaseApiConnector",
+                   new=SingleDatabaseApiConnectorPatcher):
+            response = self.client.post(
+                url, json.dumps(ids), content_type='application/json',
+            )
+
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertEqual(len(response.data), 4)
         self.assertEqual(set(response.data[0].keys()), {"name", "criteria"})
