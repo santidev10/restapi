3e04d24d0d4d68ba7cebd07d1867e978b3a41c45
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-04-22 13:56:01 +0300

    IQD-2813: Fix roles filtering for daily reports

--- email_reports/tests/test_send_daily_email.py
+++ email_reports/tests/test_send_daily_email.py
@@ -8,8 +8,9 @@ from django.utils import timezone
 from lxml import etree
 
 from aw_reporting.models import SalesForceGoalType, User, Opportunity, \
-    OpPlacement, Flight, Account, Campaign, CampaignStatistic
+    OpPlacement, Flight, Account, Campaign, CampaignStatistic, UserRole
 from email_reports.models import SavedEmail
+from email_reports.reports.daily_campaign_report import OpportunityManager
 from utils.utils_tests import ExtendedAPITestCase as APITestCase, \
     patch_settings, patch_now
 
@@ -50,8 +51,9 @@ class SendDailyEmailsTestCase(APITestCase):
 
     def test_do_not_send_un_subscribed(self):
         ad_ops = User.objects.create(id="1", name="Paul", email="1@mail.cz")
-        get_user_model().objects.create(email=ad_ops.email,
-                                        is_subscribed_to_campaign_notifications=False)
+        get_user_model().objects.create(
+            email=ad_ops.email,
+            is_subscribed_to_campaign_notifications=False)
         today = timezone.now().date()
         opportunity = Opportunity.objects.create(
             id="solo", name="Opportunity",
@@ -516,6 +518,29 @@ class SendDailyEmailsTestCase(APITestCase):
         self.assertEqual(before_yesterday_impressions_width, width_1)
         self.assertEqual(yesterday_impressions_width, width_2)
         self.assertEqual(today_impressions_width, width_3)
+
+    def test_send_report_only_to_account_manager(self):
+        now = datetime(2017, 1, 15)
+        am_role = UserRole.objects.create(id="1",
+                                          name=UserRole.ACCOUNT_MANAGER_NAME)
+        ad_ops = User.objects.create(id="1", name="Paul", email="1@mail.cz",
+                                     role=am_role)
+        am = User.objects.create(id="2", name="Paul", email="2@mail.cz",
+                                 role=am_role)
+        Opportunity.objects.create(
+            id="1",
+            ad_ops_manager=ad_ops,
+            account_manager=am,
+            start=now - timedelta(days=3),
+            end=now + timedelta(days=2),
+            probability=100)
+        with patch_now(now):
+            call_command("send_daily_email_reports",
+                         reports="DailyCampaignReport",
+                         roles=OpportunityManager.ACCOUNT_MANAGER)
+        self.assertEqual(len(mail.outbox), 1)
+        email = mail.outbox[0]
+        self.assertEqual(email.to, [am.email])
 
 
 def get_xpath_text(tree, xpath):
