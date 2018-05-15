1d28fa81af0d2d3c6e88aedba8f6c3a7c6e3362f
Andrii Dobrovolskyi <andrii.dobrovolskyi@sigma.software>
2018-05-15 20:29:08 +0300

    SAAS-2471 Trends > Incorrect Plan CPV and CPM chart values

--- aw_reporting/charts.py
+++ aw_reporting/charts.py
@@ -168,13 +168,13 @@ class DeliveryChart:
 
     def _plan_placement_value_for_date(self, placement, date) -> tuple:
         if placement["start"] > date or placement["end"] < date:
-            return 0,
+            return 0, 1
         indicator = self.params.get("indicator")
         total_days = (placement["end"] - placement["start"]).days + 1
         if indicator in (Indicator.IMPRESSIONS, Indicator.VIEWS):
-            return placement["ordered_units"] / total_days,
+            return placement["ordered_units"] / total_days, 1
         if indicator == Indicator.COSTS:
-            return placement["total_cost"] / total_days,
+            return placement["total_cost"] / total_days, 1
         if indicator == Indicator.CPV:
             return placement["total_cost"], placement["ordered_units"]
         if indicator == Indicator.CPM:
@@ -184,15 +184,9 @@ class DeliveryChart:
     def _plan_value_for_date(self, placements, date):
         values = [self._plan_placement_value_for_date(p, date) for p in
                   placements]
-        values_groups = [
-            val if len(val) > 1
-            else (val[0], 1) if len(val) > 0
-            else (0, 1)
-            for val in values
-        ]
-        numerator = sum(values_groups[0]) if len(values_groups) > 0 else 0
-        denominator = sum(values_groups[1]) if len(values_groups) > 1 else 1
-
+        values_groups = list(zip(*values))
+        numerator = sum(values_groups[0])
+        denominator = sum(values_groups[1])
         return dict(value=numerator / denominator,
                     label=date)
 
