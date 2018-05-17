0561abe60d716f1dcdc7895d47a1f802628a4dc4
Alexander Dobrzhansky <alexander.dobrzhansky@sigma.software>
2018-05-17 14:48:55 +0300

    SAAS-2439 make instance settings user specific

--- aw_reporting/api/views/visible_accounts_api_view.py
+++ aw_reporting/api/views/visible_accounts_api_view.py
@@ -2,10 +2,12 @@ from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
 from rest_framework.status import HTTP_202_ACCEPTED
+from rest_framework.status import HTTP_404_NOT_FOUND
 from rest_framework.views import APIView
 from aw_reporting.demo.models import DemoAccount
 from aw_reporting.models import Account
 from aw_reporting.settings import AdwordsAccountSettings
+from userprofile.models import UserProfile
 from utils.cache import cache_reset
 from utils.cache import cached_view_decorator as cached_view
 
@@ -16,7 +18,19 @@ class AdWordsTopManagerSerializer(ModelSerializer):
         fields = ("id", "name")
 
 
-class VisibleAccountsApiView(APIView):
+class GetUserMixin:
+    def get_user_by_id(self, user_id):
+        user_id = self.request.query_params.get('user_id')
+
+        try:
+            user = UserProfile.objects.get(id=user_id)
+        except UserProfile.DoesNotExist:
+            user = None
+
+        return user
+
+
+class VisibleAccountsApiView(APIView, GetUserMixin):
     """
     Visible account list view/edit
     """
@@ -26,7 +40,11 @@ class VisibleAccountsApiView(APIView):
 
     def get(self, request):
         data = self.serializer_class(self.queryset.all().distinct(), many=True).data
-        settings = request.user.aw_settings
+        user_id = self.request.query_params.get('user_id')
+        user = self.get_user_by_id(user_id)
+        if user is None:
+            return Response(status=HTTP_404_NOT_FOUND)
+        settings = user.aw_settings
         visible_ids = settings.get('visible_accounts')
         types_settings = settings.get('hidden_campaign_types')
         campaign_types = AdwordsAccountSettings.CAMPAIGN_TYPES
@@ -67,7 +85,11 @@ class VisibleAccountsApiView(APIView):
 
     @cached_view
     def put(self, request):
-        settings_obj = request.user.aw_settings
+        user_id = self.request.query_params.get('user_id')
+        user = self.get_user_by_id(user_id)
+        if user is None:
+            return Response(status=HTTP_404_NOT_FOUND)
+        settings_obj = user.aw_settings
         if 'accounts' in request.data:
             accounts = request.data.get('accounts')
 
@@ -97,26 +119,34 @@ class VisibleAccountsApiView(APIView):
             update = dict(visible_accounts=list(sorted(visible_accounts)),
                           hidden_campaign_types=hidden_types)
             settings_obj.update(update)
-            request.user.save()
+            user.save()
 
         cache_reset()
         return self.get(request)
 
 
-class UserAWSettingsApiView(APIView):
+class UserAWSettingsApiView(APIView, GetUserMixin):
     """
     Visible account list view/edit
     """
     permission_classes = tuple()
 
     def get(self, request):
-        user_aw_settings = request.user.aw_settings
+        user_id = self.request.query_params.get('user_id')
+        user = self.get_user_by_id(user_id)
+        if user is None:
+            return Response(status=HTTP_404_NOT_FOUND)
+        user_aw_settings = user.aw_settings
         return Response(data=user_aw_settings)
 
     @cached_view
     def put(self, request):
         # get user settings
-        user_aw_settings = request.user.aw_settings
+        user_id = self.request.query_params.get('user_id')
+        user = self.get_user_by_id(user_id)
+        if user is None:
+            return Response(status=HTTP_404_NOT_FOUND)
+        user_aw_settings = user.aw_settings
 
         # check for valid data in request body
         keys_to_update = request.data.keys() & set(AdwordsAccountSettings.AVAILABLE_KEYS)
@@ -125,6 +155,6 @@ class UserAWSettingsApiView(APIView):
         for key in keys_to_update:
             user_aw_settings[key] = request.data[key]
 
-        request.user.save()
+        user.save()
         return Response(data=user_aw_settings,
                         status=HTTP_202_ACCEPTED)
