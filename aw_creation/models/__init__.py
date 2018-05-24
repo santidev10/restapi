5f0d9ae39d5052c69792e0f35af7eff84ef20a46
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-05-24 20:00:54 +0300

    SAAS-2522: Fix migration import v2

--- aw_creation/models/__init__.py
+++ aw_creation/models/__init__.py
@@ -1,6 +1,6 @@
 from .creation import AccountCreation, CampaignCreation, AdGroupCreation, \
     AdCreation, LocationRule, AdScheduleRule, FrequencyCap, Language, \
-    default_languages, get_uid
+    default_languages, get_uid, get_version
 from .optimization import AccountOptimizationSetting, \
     CampaignOptimizationSetting
 from .targeting import TargetingItem
