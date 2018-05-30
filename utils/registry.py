6488d18f23bcc8f57da1002935d7c679fb478cc1
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-05-30 11:58:22 +0300

    SAAS-2334: Refactoring: revert lo thread locals

--- utils/registry.py
+++ utils/registry.py
@@ -1,35 +1,28 @@
 from __future__ import absolute_import, division, print_function
 
 from contextlib import contextmanager
+from threading import local
 
 
 class Registry:
-    _registry = None
-
-    @classmethod
-    def reset(cls):
-        cls._registry = None
-
-    @classmethod
-    def init(cls):
-        Registry._registry = dict()
+    _registry = local()
 
     @classmethod
     def get_request(cls):
         """ returns the request object """
         if cls._registry is not None:
-            return Registry._registry.get("request")
+            return getattr(cls._registry, "request", None)
 
     @classmethod
     def set_request(cls, request):
         """ save the request object """
-        cls._registry["request"] = request
+        setattr(cls._registry, "request", request)
 
     @classmethod
     def get_user(cls):
         """ returns the current user, if exist, otherwise returns None """
-        if cls._registry is not None and "user" in cls._registry:
-            return cls._registry.get("user")
+        if cls._registry is not None and hasattr(cls._registry, "user"):
+            return getattr(cls._registry, "user")
 
         request = cls.get_request()
         if request:
@@ -38,7 +31,7 @@ class Registry:
     @classmethod
     def set_user(cls, user):
         """ save a user as current """
-        cls._registry["user"] = user
+        setattr(cls._registry, "user", user)
 
 
 class RegistryMiddleware:
@@ -47,12 +40,12 @@ class RegistryMiddleware:
     """
 
     def process_request(self, request):
-        Registry.init()
         Registry.set_request(request)
 
     def process_response(self, request, response):
-        Registry.reset()
+        Registry.set_request(None)
         return response
+
 
 @contextmanager
 def current_user(user):
