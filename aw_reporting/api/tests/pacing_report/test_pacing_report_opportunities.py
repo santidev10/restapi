c0e54cc2a741ac294b8f2312190b3d63bcdd7398
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-06-18 15:55:24 +0300

    SAAS-2649: Fix test

--- aw_reporting/api/tests/pacing_report/test_pacing_report_opportunities.py
+++ aw_reporting/api/tests/pacing_report/test_pacing_report_opportunities.py
@@ -1129,38 +1129,6 @@ class PacingReportOpportunitiesTestCase(APITestCase):
         self.assertEqual(opp_2["bill_of_third_party_numbers"],
                          opportunity_2.bill_of_third_party_numbers)
 
-    def test_hard_cost_margin(self):
-        today = date(2018, 1, 1)
-        total_cost = 6543
-        our_cost = 1234
-        days_pass, days_left = 3, 6
-        total_days = days_pass + days_left
-        self.assertGreater(days_pass, 0)
-        self.assertGreater(days_left, 0)
-        start = today - timedelta(days=(days_pass - 1))
-        end = today + timedelta(days=days_left)
-        opportunity = Opportunity.objects.create(
-            id="1", name="1", start=today - timedelta(days=3),
-            end=today + timedelta(days=3), probability=100)
-        hard_cost_placement = OpPlacement.objects.create(
-            id="2", name="Hard cost placement", opportunity=opportunity,
-            start=start, end=end,
-            goal_type_id=SalesForceGoalType.HARD_COST)
-        Flight.objects.create(
-            start=start, end=end, total_cost=total_cost,
-            placement=hard_cost_placement, cost=1)
-        campaign = Campaign.objects.create(
-            salesforce_placement=hard_cost_placement)
-        CampaignStatistic.objects.create(date=start, campaign=campaign,
-                                         cost=our_cost)
-        client_cost = total_cost / total_days * days_pass
-        expected_margin = (1 - our_cost / client_cost) * 100
-        with patch_now(today):
-            response = self.client.get(self.url)
-        self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertEqual(response.data["items_count"], 1)
-        self.assertEqual(response.data["items"][0]["margin"], expected_margin)
-
     @skipIf(get_current_release() < "3.1", "New requirements. Ticket SAAS-2605")
     def test_hard_cost_margin(self):
         today = date(2018, 1, 1)
@@ -1214,6 +1182,7 @@ class PacingReportOpportunitiesTestCase(APITestCase):
             start=start, end=end, total_cost=total_cost,
             placement=hard_cost_placement, cost=1)
         Flight.objects.create(
+            id=2,
             start=today + timedelta(days=1), end=today + timedelta(days=1),
             total_cost=999999,
             placement=hard_cost_placement, cost=1)
