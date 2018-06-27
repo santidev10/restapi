78ff7b5a694d02edbac4688cf23b1749f38587af
Pylypenko, Heorhii <heorhii.pylypenko@sigma.software>
2018-06-27 10:52:07 +0300

    SAAS-2715: fix performance chart items api demo view

--- aw_creation/demo/views.py
+++ aw_creation/demo/views.py
@@ -486,7 +486,8 @@ class PerformanceChartItemsApiView:
     def post(original_method):
         def method(view, request, pk, dimension, **kwargs):
             if request.data.get("is_chf") == 1 and pk != DEMO_ACCOUNT_ID:
-                return original_method(view, request, pk=pk, **kwargs)
+                return original_method(
+                    view, request, pk=pk, dimension=dimension, **kwargs)
             if pk == DEMO_ACCOUNT_ID or show_demo_data(request, pk):
                 filters = view.get_filters()
                 account = DemoAccount()
