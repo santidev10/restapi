c41f4097e6246af6e2a4d8f7f6c8d8e2d9e843b5
Pylypenko, Heorhii <heorhii.pylypenko@sigma.software>
2018-08-06 15:13:49 +0300

    VIQ-26: forecast tool constants refactor

--- aw_reporting/tools/forecast_tool/constants.py
+++ aw_reporting/tools/forecast_tool/constants.py
@@ -1,13 +1,36 @@
 TARGETING_TYPES = (
-    "interests", "keywords", "channels", "videos", "remarketing", "topics")
+    "interests",
+    "keywords",
+    "channels",
+    "videos",
+    "remarketing",
+    "topics",
+)
 DEVICE_FIELDS = (
-    "device_computers", "device_mobile", "device_tablets", "device_other")
+    "device_computers",
+    "device_mobile",
+    "device_tablets",
+    "device_other",
+)
 PARENT_FIELDS = (
-    "parent_parent", "parent_not_parent", "parent_undetermined")
-GENDER_FIELDS = ("gender_undetermined", "gender_female", "gender_male")
+    "parent_parent",
+    "parent_not_parent",
+    "parent_undetermined",
+)
+GENDER_FIELDS = (
+    "gender_undetermined",
+    "gender_female",
+    "gender_male",
+)
 AGE_FIELDS = (
-    "age_undetermined", "age_18_24", "age_25_34", "age_35_44", "age_45_54",
-    "age_55_64", "age_65")
+    "age_undetermined",
+    "age_18_24",
+    "age_25_34",
+    "age_35_44",
+    "age_45_54",
+    "age_55_64",
+    "age_65",
+)
 VIDEO_LENGTHS = (
     (0, 6),
     (6, 15),
