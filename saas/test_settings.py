c130f9d055dd41af6b38b6645bb94d97b5cf1ed1
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-08-21 15:07:44 +0300

    VIQ-115: Fix migration v2

--- saas/test_settings.py
+++ saas/test_settings.py
@@ -6,22 +6,22 @@ except ImportError:
 IS_TEST = True
 DISABLE_ACCOUNT_CREATION_AUTO_CREATING = True
 
-MIGRATION_MODULES = {
-    "administration": None,
-    "auth": None,
-    "authtoken": None,
-    "aw_creation": None,
-    "aw_reporting": None,
-    "contenttypes": None,
-    "djcelery": None,
-    "keyword_tool": None,
-    "landing": None,
-    "payments": None,
-    "segment": None,
-    "sessions": None,
-    "userprofile": None,
-    "email_reports": None,
-}
+# MIGRATION_MODULES = {
+#     "administration": None,
+#     "auth": None,
+#     "authtoken": None,
+#     "aw_creation": None,
+#     "aw_reporting": None,
+#     "contenttypes": None,
+#     "djcelery": None,
+#     "keyword_tool": None,
+#     "landing": None,
+#     "payments": None,
+#     "segment": None,
+#     "sessions": None,
+#     "userprofile": None,
+#     "email_reports": None,
+# }
 for logger_config in LOGGING["handlers"].values():
     logger_config["filters"] = ["hide_all"] \
                                + logger_config.get("filters", [])
