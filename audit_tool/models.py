6f17695bb88f285d5d7e7eedcc6b97a93e1bc99a
Andrii Dobrovolskyi <andrii.dobrovolskyi@sigma.software>
2018-08-22 08:05:43 +0300

    SAAS-2899 Current update

---
+++ audit_tool/models.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from datetime import timedelta
+
+from django.db import models
+
+
+class BaseManager(models.Manager.from_queryset(models.QuerySet)):
+    LIFE_TIME_DAYS = 30
+
+    def cleanup(self):
+        now = datetime.now().date()
+        boundary_date = now - timedelta(self.LIFE_TIME_DAYS)
+        queryset = self.filter(date__lt=boundary_date)
+        queryset.delete()
+
+
+class BaseModel(models.Model):
+    date = models.DateField(db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class VideoAudit(BaseModel):
+    video_id = models.CharField(max_length=30, db_index=True)
+    video_title = models.CharField(max_length=255)
+    channel_id = models.CharField(max_length=30)
+    channel_title = models.CharField(max_length=255, default="No title")
+    preferred = models.BooleanField()
+    impressions = models.BigIntegerField()
+    sentiment = models.FloatField(null=True, blank=True, default=None)
+    hits = models.BigIntegerField()
+    words = models.TextField()
+    account_info = models.TextField()
+
+    objects = BaseManager()
+
+    class Meta:
+        unique_together = ("date", "video_id")
+
+
+class KeywordAudit(BaseModel):
+    keyword = models.CharField(max_length=255, db_index=True)
+    videos = models.BigIntegerField()
+    impressions = models.BigIntegerField()
+
+    objects = BaseManager()
+
+    class Meta:
+        unique_together = ("date", "keyword")
