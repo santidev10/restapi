b0a5b2ac758d33e159e5c2b443687b5219d50377
Pylypenko, Heorhii <heorhii.pylypenko@sigma.software>
2018-09-04 16:21:50 +0300

    VIQ-66: fix tests

--- aw_reporting/api/tests/test_analyze_chart_items.py
+++ aw_reporting/api/tests/test_analyze_chart_items.py
@@ -87,7 +87,12 @@ class AccountNamesAPITestCase(AwReportingAPITestCase):
                 'video25rate',
                 'average_cpm',
                 'ctr_v',
-                "video_clicks"
+                "video_clicks",
+                'clicks_end_cap',
+                'clicks_cards',
+                'clicks_app_store',
+                'clicks_website',
+                'clicks_call_to_action_overlay'
             }
         )
 
