31f908d5d2144f68ae465e8c91e78707a9ed5daa
Andrew <andrew@kabuto.com>
2018-10-05 09:07:18 -0700

    VIQ-510 added support for match_phrase in ES search

--- channel/api/views.py
+++ channel/api/views.py
@@ -245,7 +245,7 @@ class ChannelListApiView(
         # text_search
         text_search = query_params.pop("text_search", [None])[0]
         if text_search:
-            query_params.update(text_search__match=text_search)
+            query_params.update(text_search__match_phrase=text_search)
 
         # channel_group
         make("term", "channel_group")
