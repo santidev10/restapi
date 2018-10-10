00f51a2d29a47cd0bc31a2b1712c1fd9bb431a8a
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-10-10 16:47:05 +0300

    VIQ-551: show aw_data for everybody

--- channel/api/views.py
+++ channel/api/views.py
@@ -283,10 +283,6 @@ class ChannelListApiView(
             if not user.is_staff:
                 item.pop("cms__title", None)
 
-            if not user.has_perm('userprofile.channel_aw_performance') \
-                    and not is_own:
-                item.pop('aw_data', None)
-
             for field in ["youtube_published_at", "updated_at"]:
                 if field in item and item[field]:
                     item[field] = re.sub(
