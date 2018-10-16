5625d336bd6ce0c958efe04ab8fb1c6e47e70c41
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-10-16 15:38:17 +0300

    VIQ-8: Update default recipients of the UI Error Reports

--- saas/settings.py
+++ saas/settings.py
@@ -304,8 +304,9 @@ PAYMENT_ACTION_EMAIL_ADDRESSES = [
 ]
 
 CONTACT_FORM_EMAIL_ADDRESSES = [
-    "anna.chumak@sigma.software",
+    "alex.klinovoy@sigma.software",
     "maria.konareva@sigma.software",
+    "maryna.antonova@sigma.software",
     "yulia.prokudina@sigma.software",
 ]
 
