f39ea85c2eddd6f37cfadfe8796e4b6ea4b00fe9
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-10-25 16:15:48 +0300

    VIQ-663: Remove print

--- aw_reporting/demo/charts.py
+++ aw_reporting/demo/charts.py
@@ -67,11 +67,7 @@ class DemoChart:
         if dimension in CLICKS_TYPES_DIMENSIONS:
             all_stats += CLICKS_STATS
         summary = {}
-        try:
-            items = {i['label']: i for i in dimensions}
-        except:
-            print(dimensions)
-            return
+        items = {i['label']: i for i in dimensions}
         dim_len = len(dimensions)
         for stat in all_stats:
             value = getattr(item, stat)
