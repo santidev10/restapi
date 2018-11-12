8a29dc6e76ac936a8b51be2ac2d0345a1875c0d3
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-11-12 14:01:37 +0200

    VIQ-716: Add logging of all celery tasks

--- saas/celery.py
+++ saas/celery.py
@@ -1,6 +1,8 @@
+import logging
 import os
 
 from celery import Celery
+from celery.signals import task_failure
 
 from utils.celery.fix_email_logging import fix_email_logging
 
@@ -11,3 +13,9 @@ app.config_from_object("django.conf:settings", namespace='CELERY')
 app.autodiscover_tasks()
 
 fix_email_logging()
+
+
+@task_failure.connect
+def on_task_failure(sender, exception, **_):
+    logger = logging.getLogger(sender.name)
+    logger.exception(exception)
