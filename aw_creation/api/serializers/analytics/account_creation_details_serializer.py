2e2abbe93bc77d5af95c493728a73be84023df94
heorhii.pylypenko <pilipenko.g.k@gmail.com>
2018-12-05 15:40:47 +0200

    VIQ-910: clean up

--- aw_creation/api/serializers/analytics/account_creation_details_serializer.py
+++ aw_creation/api/serializers/analytics/account_creation_details_serializer.py
@@ -3,7 +3,6 @@ from django.db.models import Sum
 from aw_creation.api.serializers import AnalyticsAccountCreationListSerializer
 from aw_creation.api.serializers.analytics.base_account_creation_serializer import BaseAccountCreationSerializer
 from aw_reporting.models import AdGroup
-from aw_reporting.models import base_stats_aggregator
 from utils.serializers.fields import StatField
 
 
@@ -13,8 +12,6 @@ class AnalyticsAccountCreationDetailsSerializer(AnalyticsAccountCreationListSeri
     clicks_app_store = StatField()
     clicks_cards = StatField()
     clicks_end_cap = StatField()
-
-    stats_aggregations = base_stats_aggregator()
 
     class Meta(BaseAccountCreationSerializer.Meta):
         fields = (
