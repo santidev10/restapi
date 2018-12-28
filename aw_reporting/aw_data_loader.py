eec328c40fa836a324e2e58c074adbce0ccde67e
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2018-12-28 14:58:17 +0200

    VIQ-1002: Fix default value for "canManageClient" on account update

--- aw_reporting/aw_data_loader.py
+++ aw_reporting/aw_data_loader.py
@@ -86,7 +86,7 @@ class AWDataLoader:
                         name=e['name'],
                         currency_code=e['currencyCode'],
                         timezone=e['dateTimeZone'],
-                        can_manage_clients=e["canManageClients"],
+                        can_manage_clients=e.get("canManageClients", False),
                     )
                 )
                 a.managers.add(manager)
