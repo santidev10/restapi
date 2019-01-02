c0a2e0950c488b0e39ff4f5764e3ac4da717db76
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2019-01-02 22:24:49 +0200

    VIQ-1011: Add documentation for MCC account list

--- aw_reporting/api/views/connect_aw_account_list.py
+++ aw_reporting/api/views/connect_aw_account_list.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import ListAPIView
 
 from aw_reporting.api.serializers import AWAccountConnectionRelationsSerializer
@@ -11,3 +12,10 @@ class ConnectAWAccountListApiView(ListAPIView):
         qs = AWConnectionToUserRelation.objects.filter(
             user=self.request.user).order_by("connection__email")
         return qs
+
+    @swagger_auto_schema(
+        operation_description="List of AdWords accounts allowed to push data"
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
