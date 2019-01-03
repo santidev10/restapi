5a14e1a7a6de7c8d081125ba4e2937642972aef9
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2019-01-03 12:01:50 +0200

    VIQ-1011: Add documentation for campaign creation update

--- aw_creation/api/views/views.py
+++ aw_creation/api/views/views.py
@@ -661,6 +661,21 @@ class CampaignCreationSetupApiView(RetrieveUpdateAPIView):
         MediaBuyingAddOnPermission),
     )
 
+    @swagger_auto_schema(
+        operation_description="Update campaign creation",
+        manual_parameters=[
+            openapi.Parameter(
+                name="id",
+                required=True,
+                in_=openapi.IN_PATH,
+                description="Campaign creation id",
+                type=openapi.TYPE_STRING,
+            ),
+        ],
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
     def get_queryset(self):
         queryset = CampaignCreation.objects.filter(
             account_creation__owner=self.request.user,
