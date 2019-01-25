7b4ddde92c1685411060fcb708dabc3d584d7a3a
Andrii Dobrovolskyi <andrii.dobrovolskyi@sigma.software>
2019-01-25 15:54:14 +0200

    VIQ-1106 renames

--- segment/api/views.py
+++ segment/api/views.py
@@ -383,11 +383,11 @@ class PersistentMasterSegmentsListApiView(ListAPIView):
 
     def get_queryset(self):
         channels_segment_queryset = PersistentSegmentChannel.objects\
-            .filter(title__endswith=PersistentSegmentTitles.CHANNELS_MASTER_WHITELIST_SEGMENT_TITLE)\
+            .filter(title=PersistentSegmentTitles.CHANNELS_MASTER_WHITELIST_SEGMENT_TITLE)\
             .annotate(segment_type=Value(PersistentSegmentType.CHANNEL, output_field=CharField()))
 
         videos_segment_queryset = PersistentSegmentVideo.objects\
-            .filter(title__endswith=PersistentSegmentTitles.VIDEOS_MASTER_WHITELIST_SEGMENT_TITLE)\
+            .filter(title=PersistentSegmentTitles.VIDEOS_MASTER_WHITELIST_SEGMENT_TITLE)\
             .annotate(segment_type=Value(PersistentSegmentType.VIDEO, output_field=CharField()))
 
         return videos_segment_queryset.union(channels_segment_queryset)
