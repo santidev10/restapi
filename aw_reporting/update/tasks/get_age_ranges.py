a58cc74597cd086c9b969bc93912d7a12f552e72
Pylypenko, Heorhii <heorhii.pylypenko@sigma.software>
2019-02-28 15:25:25 +0200

    VIQ-1285: optimize age ranges update

--- aw_reporting/update/tasks/get_age_ranges.py
+++ aw_reporting/update/tasks/get_age_ranges.py
@@ -10,6 +10,24 @@ from aw_reporting.update.tasks.utils.drop_latest_stats import drop_latest_stats
 from aw_reporting.update.tasks.utils.get_account_border_dates import get_account_border_dates
 from aw_reporting.update.tasks.utils.get_base_stats import get_base_stats
 from aw_reporting.update.tasks.utils.quart_views import quart_views
+from utils.utils import chunks_generator
+
+
+def _generate_stat_instances(model, age_ranges, report, click_type_data):
+    for row_obj in report:
+        stats = {
+            "age_range_id": age_ranges.index(row_obj.Criteria),
+            "date": row_obj.Date,
+            "ad_group_id": row_obj.AdGroupId,
+            "video_views_25_quartile": quart_views(row_obj, 25),
+            "video_views_50_quartile": quart_views(row_obj, 50),
+            "video_views_75_quartile": quart_views(row_obj, 75),
+            "video_views_100_quartile": quart_views(row_obj, 100),
+        }
+        stats.update(get_base_stats(row_obj))
+        update_stats_with_click_type_data(
+            stats, click_type_data, row_obj, DAILY_STATISTICS_CLICK_TYPE_REPORT_UNIQUE_FIELD_NAME)
+        yield model(**stats)
 
 
 def get_age_ranges(client, account, today):
@@ -36,26 +54,11 @@ def get_age_ranges(client, account, today):
         report = age_range_performance_report(
             client, dates=(min_date, max_date),
         )
-        bulk_data = []
         click_type_report = age_range_performance_report(
             client, dates=(min_date, max_date), fields=DAILY_STATISTICS_CLICK_TYPE_REPORT_FIELDS)
         click_type_data = format_click_types_report(click_type_report,
                                                     DAILY_STATISTICS_CLICK_TYPE_REPORT_UNIQUE_FIELD_NAME)
-        for row_obj in report:
-            stats = {
-                "age_range_id": AgeRanges.index(row_obj.Criteria),
-                "date": row_obj.Date,
-                "ad_group_id": row_obj.AdGroupId,
-
-                "video_views_25_quartile": quart_views(row_obj, 25),
-                "video_views_50_quartile": quart_views(row_obj, 50),
-                "video_views_75_quartile": quart_views(row_obj, 75),
-                "video_views_100_quartile": quart_views(row_obj, 100),
-            }
-            stats.update(get_base_stats(row_obj))
-            update_stats_with_click_type_data(
-                stats, click_type_data, row_obj, DAILY_STATISTICS_CLICK_TYPE_REPORT_UNIQUE_FIELD_NAME)
-            bulk_data.append(AgeRangeStatistic(**stats))
-
-        if bulk_data:
-            AgeRangeStatistic.objects.safe_bulk_create(bulk_data)
+        generator = _generate_stat_instances(AgeRangeStatistic, AgeRanges, report, click_type_data)
+        chunk_size = 10000
+        for chunk in chunks_generator(generator, chunk_size):
+            AgeRangeStatistic.objects.safe_bulk_create(chunk)
