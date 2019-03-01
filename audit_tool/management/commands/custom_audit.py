508061f1b5f28d1ad9e9183ad0ce04bbf992fef1
Kenneth <kenneth.oh@channelfactory.com>
2019-03-01 13:40:59 -0800

    Add script to run topic audit against all videos in our database

--- audit_tool/management/commands/custom_audit.py
+++ audit_tool/management/commands/custom_audit.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from audit_tool.custom_audit import CustomAudit
 import logging
+import os
+import csv
 
 logger = logging.getLogger(__name__)
 
@@ -18,21 +20,48 @@ class Command(BaseCommand):
             '--title',
             help='Set Segment title'
         )
+        parser.add_argument(
+            '--export',
+            help='Export file path.'
+        )
 
     def handle(self, *args, **kwargs):
         if kwargs['file'] is None:
             self.stdout.write('You must provide a csv file path to read. --file CSV_PATH')
         elif kwargs['title'] is None:
             self.stdout.write('You must provide a title for this segment. --title SEGMENT_TITLE')
+        elif kwargs['export'] is None:
+            self.stdout.write('You must a file path to export to. --export')
         else:
             csv_file_path = kwargs['file']
             segment_title = kwargs['title']
+            csv_export_path = kwargs['export']
+
+            try:
+                os.path.exists(csv_export_path)
+            except OSError:
+                self.stdout.write('The provided --export path is invalid.')
 
             self.stdout.write('Starting custom audit...\n', ending='')
             custom_audit = CustomAudit(
                 csv_file_path=csv_file_path,
                 segment_title=segment_title,
             )
-            custom_audit.run()
+            results = custom_audit.run()
+
+            with open(csv_export_path, mode='w') as csv_file:
+                headers = ['Channel ID', 'Found words']
+                writer = csv.DictWriter(csv_file, fieldnames=headers)
+
+                writer.writeheader()
+                for key, value in results.items():
+                    writer.writerow({
+                        headers[0]: key,
+                        headers[1]: value
+                    })
+
+            self.stdout.write('Custom audit results exported to: {}'.format(csv_export_path))
+
+
 
 
