f1738857276671f32ed5547b33a4c93c11156356
Kenneth <kenneth.oh@channelfactory.com>
2019-03-26 15:33:08 -0700

    Change allowed update parameters to tuple for immutability

--- aw_reporting/api/views/pacing_report/pacing_report_opportunity_buffer_update.py
+++ aw_reporting/api/views/pacing_report/pacing_report_opportunity_buffer_update.py
@@ -21,14 +21,13 @@ class PacingReportOpportunityBufferUpdateApiView(UpdateAPIView, PacingReportHelp
         except Opportunity.DoesNotExist:
             return Response(status=HTTP_404_NOT_FOUND)
 
-        allowed_updates = ['cpm_buffer', 'cpv_buffer']
+        allowed_updates = ('cpm_buffer', 'cpv_buffer')
 
         if not set(request.data.keys()).issubset(allowed_updates):
             return Response(status=HTTP_400_BAD_REQUEST, data='You may only update cpm_buffer or cpv_buffer values.')
 
         Opportunity.objects.filter(pk=pk).update(**request.data)
 
-        # Get placement for opportunity
         report = PacingReport()
         placements = report.get_placements(opportunity)
         self.multiply_percents(placements)
