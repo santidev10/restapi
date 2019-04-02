143cad2e31b17b8c2fc573d00941e5c3cbb07316
Kenneth <kenneth.oh@channelfactory.com>
2019-04-01 20:45:31 -0700

    Remove changes over validating inputs
    Validating for category id involves too much complexity. Simply get or create the related BadWordCategory model.

--- brand_safety/api/serializers/bad_word_serializer.py
+++ brand_safety/api/serializers/bad_word_serializer.py
@@ -1,29 +1,17 @@
-from rest_framework.serializers import ModelSerializer, IntegerField, ValidationError
+from rest_framework.serializers import ModelSerializer, CharField, ValidationError
 
 from brand_safety.models import BadWord, BadWordCategory
 
 
 class BadWordSerializer(ModelSerializer):
-    category_ref_id = IntegerField(required=True)
+    category = CharField(max_length=80, required=True)
 
     def validate(self, data):
-        """
-        Accepts category_ref_id field for setting BadWordCategory values
-        :param data: Request data to validate
-        :return: dict -> Validated data
-        """
-        try:
-            category_id = data["category_ref_id"]
-        except KeyError:
-            raise ValidationError('You must provide the field category as an id.')
-        try:
-            category = BadWordCategory.objects.get(id=category_id)
-            data['category'] = category.name
-            data['category_ref'] = category
-        except BadWordCategory.DoesNotExist:
-            raise ValidationError('The provided category_id {} was not found.'.format(category_id))
+        category_name = data['category']
+        category_ref = BadWordCategory.from_string(category_name)
+        data['category_ref'] = category_ref
         return data
 
     class Meta:
         model = BadWord
-        fields = ("id", "name", "category_ref_id")
+        fields = ("id", "name", "category")
