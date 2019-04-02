d7efdd3cbd2e96dfebe59ea92315841476e6adce
Kenneth <kenneth.oh@channelfactory.com>
2019-04-01 18:03:45 -0700

    Add BadWordCategory table
    BadWordCategory table will be used as primary store of brand safety tag categories to ensure uniformity of categories across all brand safety features. Add overridden validate method to BadWord serializer to validate and accept foriegn category_ref ids for more strucutured data.

--- brand_safety/models/__init__.py
+++ brand_safety/models/__init__.py
@@ -1,3 +1,4 @@
 from .bad_video import BadVideo
 from .bad_channel import BadChannel
 from .bad_word import BadWord
+from .bad_word import BadWordCategory
--- brand_safety/models/bad_word.py
+++ brand_safety/models/bad_word.py
@@ -3,10 +3,20 @@ from django.db import models
 from aw_reporting.models import BaseQueryset
 
 
+class BadWordCategory(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+
+    @staticmethod
+    def from_string(in_var):
+        db_result, _ = BadWordCategory.objects.get_or_create(name=in_var.lower())
+        return db_result
+
+
 class BadWord(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=80)
-    category = models.CharField(max_length=80)
+    category = models.CharField(max_length=80) # tech debt: remove in 3.15
+    category_ref = models.ForeignKey(BadWordCategory, db_index=True, default=None, null=True)
 
     objects = BaseQueryset.as_manager()
 
