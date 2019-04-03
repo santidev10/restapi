9bc2d0446de5517f9786c9f6ac244d2893278a67
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2019-04-03 15:48:54 +0300

    VIQ-1253: Refactoring

--- aw_reporting/tests/test_browse_salesforce_data.py
+++ aw_reporting/tests/test_browse_salesforce_data.py
@@ -14,7 +14,7 @@ from aw_reporting.models import Flight
 from aw_reporting.models import OpPlacement
 from aw_reporting.models import Opportunity
 from aw_reporting.models import User
-from aw_reporting.models.salesforce_constants import DynamicPlacementType
+from aw_reporting.models.salesforce_constants import DynamicPlacementType, SalesforceFields
 from aw_reporting.models.salesforce_constants import SalesForceGoalType
 from aw_reporting.reports.pacing_report import PacingReport
 from aw_reporting.reports.pacing_report import get_pacing_from_flights
