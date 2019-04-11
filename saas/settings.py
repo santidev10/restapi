1d9a4eba36469562c1262d5199b1517758447824
Kharko, Yaryna <yaryna.kharko@sigma.software>
2019-04-11 13:33:28 +0300

    VIQ-1426 Update settings

--- saas/settings.py
+++ saas/settings.py
@@ -404,10 +404,8 @@ if is_running_under_teamcity():
 
 AMAZON_S3_BUCKET_NAME = "viewiq-dev"
 AMAZON_S3_REPORTS_BUCKET_NAME = "viewiq-reports-local"
-AMAZON_S3_ACCESS_KEY_ID = "<put_aws_access_key_id_here>"
-AMAZON_S3_SECRET_ACCESS_KEY = "<put_aws_secret_access_key>"
-#AMAZON_S3_ACCESS_KEY_ID = "<put_aws_access_key_id_here>"
-#AMAZON_S3_SECRET_ACCESS_KEY = "<put_aws_secret_access_key>"
+AMAZON_S3_ACCESS_KEY_ID = None
+AMAZON_S3_SECRET_ACCESS_KEY = None
 AMAZON_S3_LOGO_STORAGE_URL_FORMAT = "https://s3.amazonaws.com/viewiq-prod/logos/{}.png"
 
 MAX_AVATAR_SIZE_MB = 10.
