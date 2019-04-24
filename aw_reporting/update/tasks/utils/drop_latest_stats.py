74e8e348a3594ddcc7ead6d22afacc62477f875e
Kharko, Yaryna <yaryna.kharko@sigma.software>
2019-04-24 16:14:40 +0300

    VIQ-1541 Added docs

--- aw_reporting/update/tasks/utils/drop_latest_stats.py
+++ aw_reporting/update/tasks/utils/drop_latest_stats.py
@@ -4,13 +4,13 @@ AD_WORDS_STABILITY_STATS_DAYS_COUNT = 11
 
 
 def drop_latest_stats(queryset, today):
-    # delete stats for ten days
+    """ Delete stats for ten days """
     date_delete = date_to_refresh_statistic(today)
     queryset.filter(date__gte=date_delete).delete()
 
 
 def drop_custom_stats(queryset, min_date, max_date):
-    # delete stats for custom range
+    """ Delete stats for custom range """
     queryset.filter(date__gte=min_date, date__lte=max_date).delete()
 
 
