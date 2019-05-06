9333db53ef004e354fbf98ea46c77f9b54644407
Kenneth <kenneth.oh@channelfactory.com>
2019-05-06 14:52:57 -0700

    Remove test comments / statements

--- segment/api/views.py
+++ segment/api/views.py
@@ -496,15 +496,14 @@ class PersistentSegmentPreviewAPIView(APIView):
             segment = segment_model.objects.get(id=kwargs["pk"])
         except segment_model.DoesNotExist:
             raise Http404
-        # total_related_items = segment.related.count()
-        total_related_items = segment.related.select_related("segment").count()
-        max_page = total_related_items // size
-        page = page if page <= max_page else max_page
-        page_start = (page - 1) * size
-        # related_items_page = segment.related.select_related("segment").order_by("related_id")[page_start:page_start + size]
-        related_items_page = segment.related.order_by("related_id")[
-                             page_start:page_start + size]
-        related_ids = [item.related_id for item in related_items_page]
+        related_items = segment.related.select_related("segment").order_by("related_id")[:self.MAX_ITEMS]
+        paginator = Paginator(related_items, size)
+        try:
+            preview_page = paginator.page(page)
+        except EmptyPage:
+            page = paginator.num_pages
+            preview_page = paginator.page(page)
+        related_ids = [item.related_id for item in preview_page.object_list]
         # Helper function to set SDB connector config since Channel and Video SDB querying is similar
         config = get_persistent_segment_connector_config_by_type(segment_type, related_ids)
         if config is None:
@@ -515,7 +514,7 @@ class PersistentSegmentPreviewAPIView(APIView):
             item[item_id_key]: item for item in response.get("items")
         }
         preview_data = []
-        for item in related_items_page.values("related_id", "title", "category", "details", "thumbnail_image_url"):
+        for item in preview_page.object_list.values("related_id", "title", "category", "details", "thumbnail_image_url"):
             if response_items.get(item["related_id"]):
                 # Map Cassandra data item id fields to just id
                 data = response_items[item["related_id"]]
@@ -527,9 +526,8 @@ class PersistentSegmentPreviewAPIView(APIView):
             preview_data.append(data)
         result = {
             "items": preview_data,
-            "items_count": total_related_items,
             "current_page": page,
-            "max_page": max_page,
+            "max_page": paginator.num_pages,
         }
         return Response(status=HTTP_200_OK, data=result)
 
@@ -554,3 +552,4 @@ class PersistentSegmentPreviewAPIView(APIView):
             mapped_data["subscribers"] = data["details"].get("subscribers")
             mapped_data["audited_videos"] = data["details"].get("audited_videos")
         return mapped_data
+
