48555153ea7d7541b56a736f9de3e0d0d92e34a7
George Su <george.su@channelfactory.com>
2019-05-13 15:39:03 -0700

    Removed some unnecessary packages from audit list test.

--- audit_tool/api/tests/test_audit_list.py
+++ audit_tool/api/tests/test_audit_list.py
@@ -1,8 +1,4 @@
-import datetime
-
 from rest_framework.status import HTTP_200_OK
-from rest_framework.status import HTTP_400_BAD_REQUEST
-from rest_framework.status import HTTP_404_NOT_FOUND
 
 from audit_tool.models import AuditProcessor
 
@@ -16,7 +12,6 @@ class AuditListAPITestCase(ExtendedAPITestCase):
     url = reverse(AuditPathName.AUDIT_LIST, [Namespace.AUDIT_TOOL])
     def test_success_get(self):
         self.create_admin_user()
-        now = datetime.datetime.now()
         audit_1 = AuditProcessor.objects.create()
         audit_2 = AuditProcessor.objects.create()
         audit_3 = AuditProcessor.objects.create()
