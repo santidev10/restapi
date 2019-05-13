259141367d554c02f31d848e545258102deebbb8
George Su <george.su@channelfactory.com>
2019-05-13 16:12:03 -0700

    Changed url route.

--- audit_tool/api/urls/urls.py
+++ audit_tool/api/urls/urls.py
@@ -8,5 +8,5 @@ from audit_tool.api.views import AuditListApiView
 from .names import AuditPathName
 
 urlpatterns = [
-    url(r"^audit_tool/audits/$", AuditListApiView.as_view(), name=AuditPathName.AUDIT_LIST)
+    url(r"^audit_tool/audits/list/$", AuditListApiView.as_view(), name=AuditPathName.AUDIT_LIST)
 ]
