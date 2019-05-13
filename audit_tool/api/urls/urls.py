546ef47d648eecba0a5f52baebd78d55f2fcc77a
George Su <george.su@channelfactory.com>
2019-05-13 14:29:34 -0700

    Refactored routing for audit_list by modifying init file in views.

--- audit_tool/api/urls/urls.py
+++ audit_tool/api/urls/urls.py
@@ -3,7 +3,7 @@ Audit Tool api urls module
 """
 from django.conf.urls import url
 
-from audit_tool.api.views.audit_list import AuditListApiView
+from audit_tool.api.views import AuditListApiView
 
 from .names import AuditPathName
 
