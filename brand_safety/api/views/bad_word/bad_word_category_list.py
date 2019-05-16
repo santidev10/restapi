82c291ba9908c4ef56fa16df749ecc85c9afc3d7
George Su <george.su@channelfactory.com>
2019-05-16 12:31:59 -0700

    Resolved merge conflicts in BadWordListApiView.

--- brand_safety/api/views/bad_word/bad_word_category_list.py
+++ brand_safety/api/views/bad_word/bad_word_category_list.py
@@ -5,10 +5,7 @@ from rest_framework.permissions import IsAdminUser
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 
-<<<<<<< HEAD
 from brand_safety.api.serializers.bad_word_category_serializer import BadWordCategorySerializer
-=======
->>>>>>> 2583278aabef393f2b9cd722dfc70ca3793c3e8a
 from brand_safety.models import BadWordCategory
 
 
@@ -17,11 +14,6 @@ class BadWordCategoryListApiView(ListCreateAPIView):
     serializer_class = BadWordCategorySerializer
     queryset = BadWordCategory.objects.all()
 
-<<<<<<< HEAD
+
     filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     search_fields = ("name", "=id",)
-=======
-    def get(self, *_, **__):
-        categories_list = BadWordCategory.objects.values("id", "name")
-        return Response(data=categories_list)
->>>>>>> 2583278aabef393f2b9cd722dfc70ca3793c3e8a
