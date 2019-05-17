96f9585a05f81d4076137f75dcb880c90d2037f2
George Su <george.su@channelfactory.com>
2019-05-17 14:27:53 -0700

    Created custom POST endpoint for BadWordListApiView.

--- brand_safety/api/views/bad_word/bad_word_list.py
+++ brand_safety/api/views/bad_word/bad_word_list.py
@@ -2,9 +2,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
 from brand_safety.api.serializers.bad_word_serializer import BadWordSerializer
-from brand_safety.models import BadWord
+from brand_safety.models import BadWord, BadWordCategory
 
 
 class BadWordListApiView(ListCreateAPIView):
@@ -15,3 +16,15 @@ class BadWordListApiView(ListCreateAPIView):
     filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     search_fields = ("name",)
     filter_fields = ("category__name",)
+
+    def create(self, request):
+        name = request.data.get("name")
+        category = request.data.get("category")
+        negative_score = request.data.get("negative_score")
+        BadWord.objects.get_or_create(name=name, category=BadWordCategory.from_string(category), negative_score=negative_score)
+
+        queryset = self.get_queryset()
+        serializer = BadWordSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
