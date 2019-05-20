506ea8724514b984911437b8b7d54f3db860e757
George Su <george.su@channelfactory.com>
2019-05-20 10:32:56 -0700

    Changed BadWordSerializer to only accept category_id values for PUT/POST requests. Category string values will raise errors.

--- brand_safety/api/serializers/bad_word_serializer.py
+++ brand_safety/api/serializers/bad_word_serializer.py
@@ -10,16 +10,14 @@ class BadWordSerializer(ModelSerializer):
 
     def validate_category(self, value):
         try:
-            if type(value) is str:
-                category = BadWordCategory.from_string(value)
-            else:
-                try:
-                    category = BadWordCategory.objects.get(pk=value)
-                except BadWordCategory.DoesNotExist:
-                    raise ValidationError("Invalid category value: {}".format(value))
+            category_id = int(value)
+            try:
+                category = BadWordCategory.objects.get(pk=category_id)
+            except BadWordCategory.DoesNotExist:
+                raise ValidationError("Category with ID: {} does not exist. Please enter a valid category ID value.".format(value))
             return category
-        except KeyError:
-            raise ValidationError("category required.")
+        except ValueError:
+            raise ValidationError("Expected Category ID value. Received: {}".format(value))
 
     class Meta:
         model = BadWord
