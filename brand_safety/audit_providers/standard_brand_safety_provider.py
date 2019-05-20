60e3165b0190d4b2f8f4ce79b91fc8059b49e800
Kenneth <kenneth.oh@channelfactory.com>
2019-05-19 19:41:17 -0700

    Increase processors and only add metadata hits for channel if keyword category is not in excluded categories

--- brand_safety/audit_providers/standard_brand_safety_provider.py
+++ brand_safety/audit_providers/standard_brand_safety_provider.py
@@ -18,9 +18,9 @@ class StandardBrandSafetyProvider(object):
     """
     Interface for reading source data and providing it to services
     """
-    channel_id_master_batch_limit = 150
+    channel_id_master_batch_limit = 240
     channel_id_pool_batch_limit = 30
-    max_process_count = 5
+    max_process_count = 8
     # Multiplier to apply for brand safety hits
     brand_safety_score_multiplier = {
         "title": 4,
