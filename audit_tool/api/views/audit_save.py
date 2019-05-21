b21e7d5861055619ebb9d3f851e4c7580d6e3916
Andrew <andrew@kabuto.com>
2019-05-21 11:23:38 -0700

    fix string

--- audit_tool/api/views/audit_save.py
+++ audit_tool/api/views/audit_save.py
@@ -18,7 +18,7 @@ class AuditSaveApiView(APIView):
         # handle inclusion_file upload IF inclusion_file (put in s3)
 
         if str(audit_type) not in AuditProcessor.AUDIT_TYPES:
-            raise PROBLEM
+            raise "PROBLEM"
         params = {
             'name': name,
             'audit_type': audit_type,
