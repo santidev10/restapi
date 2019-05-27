97bc020763a5664dd60c6a9c008784d78ba25f30
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2019-05-27 11:33:29 +0300

    VIQ-1635: Update logging

--- userprofile/management/commands/grant_access_to_media_planning_brand_safety.py
+++ userprofile/management/commands/grant_access_to_media_planning_brand_safety.py
@@ -1,19 +1,21 @@
 import logging
-from userprofile.permissions import PermissionGroupNames
-from userprofile.permissions import Permissions
+
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
+
+from userprofile.permissions import PermissionGroupNames
+from userprofile.permissions import Permissions
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        logger.info("Start")
+        logger.info("Start grant_access_to_media_planning_brand_safety")
         Permissions.sync_groups()
         count = 0
         for user in get_user_model().objects.all():
-            count += 1
             if PermissionGroupNames.MEDIA_PLANNING_BRAND_SAFETY not in user.get_user_groups():
+                count += 1
                 user.add_custom_user_group(PermissionGroupNames.MEDIA_PLANNING_BRAND_SAFETY)
-        logger.info("DONE {} users processed.".format(count))
+        logger.info("DONE grant_access_to_media_planning_brand_safety. {} users processed.".format(count))
