a7a88ef353fbcaf09f0552386083d63d48310c14
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2019-05-27 12:15:06 +0300

    VIQ-1641: Refactoring

--- aw_creation/api/tests/dashboard/test_account_creation_campaigns.py
+++ aw_creation/api/tests/dashboard/test_account_creation_campaigns.py
@@ -1,12 +1,15 @@
-from django.core.urlresolvers import reverse
 from rest_framework.status import HTTP_200_OK
 
 from aw_creation.api.urls.names import Name
 from aw_creation.api.urls.namespace import Namespace
-from aw_reporting.models import Account, Campaign, AdGroup
+from aw_reporting.models import Account
+from aw_reporting.models import AdGroup
+from aw_reporting.models import Campaign
 from saas.urls.namespaces import Namespace as RootNamespace
 from userprofile.constants import UserSettingsKey
+from userprofile.permissions import PermissionGroupNames
 from userprofile.permissions import Permissions
+from utils.utittests.reverse import reverse
 from utils.utittests.test_case import ExtendedAPITestCase
 
 
@@ -17,25 +20,24 @@ class DashboardAccountCreationCampaignsAPITestCase(ExtendedAPITestCase):
         Permissions.sync_groups()
 
     def _get_url(self, account_id):
-        return reverse(RootNamespace.AW_CREATION + ":" + Namespace.DASHBOARD + ":" + Name.Dashboard.CAMPAIGNS,
+        return reverse(Name.Dashboard.CAMPAIGNS, [RootNamespace.AW_CREATION, Namespace.DASHBOARD],
                        args=(account_id,))
 
     def test_success_get_chf_account(self):
         user = self.create_test_user()
         account = Account.objects.create(id=1, name="")
-        user.aw_settings[UserSettingsKey.VISIBLE_ACCOUNTS] = [account.id]
-        user.aw_settings[UserSettingsKey.HIDDEN_CAMPAIGN_TYPES] = {
-            "".format(account.id): []
+        user_settings = {
+            UserSettingsKey.VISIBLE_ACCOUNTS: [account.id]
         }
-        user.update_access([{"name": "Managed Service", "value": True}])
-        user.save()
+        user.add_custom_user_group(PermissionGroupNames.MANAGED_SERVICE)
         campaign_id = "1"
         ad_group_id = "1"
         campaign = Campaign.objects.create(
             id=campaign_id, name="", account=account)
         AdGroup.objects.create(id=ad_group_id, name="", campaign=campaign)
         url = self._get_url(account.account_creation.id)
-        response = self.client.get(url)
+        with self.patch_user_settings(**user_settings):
+            response = self.client.get(url)
         self.assertEqual(response.status_code, HTTP_200_OK)
         campaign = response.data[0]
         self.assertEqual(campaign["id"], campaign_id)
