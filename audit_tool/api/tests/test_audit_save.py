15cb1ba148cc40da3316c1ab79edab308411e4ee
George Su <george.su@channelfactory.com>
2019-05-29 15:23:06 -0700

    Refactored unit tests to be more DRY.

--- audit_tool/api/tests/test_audit_save.py
+++ audit_tool/api/tests/test_audit_save.py
@@ -19,50 +19,39 @@ class AuditSaveAPITestCase(ExtendedAPITestCase):
         self.tmp_file = mkstemp(suffix=".csv")
         self.tmp_name = self.tmp_file[1]
         self.body = {}
-
-    def test_audit_save_success(self):
         with open(self.tmp_name, 'w+', encoding='utf-8-sig') as f:
             f.write("testing")
             self.body['source_file'] = f
-            response = self.client.post(self.url + "?name=test&audit_type=0&language=en/", self.body)
-        self.assertEqual(response.status_code, HTTP_200_OK)
-        audit = AuditProcessor.objects.get(id=response.data["id"])
-        key = audit.params['seed_file']
+            self.response = self.client.post(self.url + "?name=test&audit_type=0&language=en/", self.body)
+        self.audit = AuditProcessor.objects.get(id=self.response.data["id"])
+        self.key = self.audit.params['seed_file']
+
+    def tearDown(self):
+        try:
+            self.s3.delete_object(
+                Bucket=AuditFileS3Exporter.bucket_name,
+                Key=self.key
+            )
+        except Exception as e:
+            raise KeyError("Failed to delete object. Object with key {} not found in bucket.".format(self.key))
+        self.audit.delete()
+
+    def test_audit_save_success(self):
+        self.assertEqual(self.response.status_code, HTTP_200_OK)
         try:
             s3_object = self.s3.get_object(
                 Bucket=AuditFileS3Exporter.bucket_name,
-                Key=key
+                Key=self.key
             )
         except Exception as e:
-            raise KeyError("Object with key {} not found in bucket.".format(key))
+            raise KeyError("Object with key {} not found in bucket.".format(self.key))
         self.assertEqual(s3_object['ResponseMetadata']['HTTPStatusCode'], HTTP_200_OK)
-        try:
-            self.s3.delete_object(
-                Bucket=AuditFileS3Exporter.bucket_name,
-                Key=key
-            )
-        except Exception as e:
-            raise KeyError("Failed to delete object. Object with key {} not found in bucket.".format(key))
 
     def test_seed_file_content_retrieval(self):
-        with open(self.tmp_name, 'w+', encoding='utf-8-sig') as f:
-            f.write("testing")
-            self.body['source_file'] = f
-            response = self.client.post(self.url + "?name=test&audit_type=0&language=en/", self.body)
-        self.assertEqual(response.status_code, HTTP_200_OK)
-        audit = AuditProcessor.objects.get(id=response.data["id"])
-        key = audit.params['seed_file']
         try:
-            f = AuditFileS3Exporter.get_s3_export_csv(key)
+            f = AuditFileS3Exporter.get_s3_export_csv(self.key)
         except Exception as e:
-            raise KeyError("Could not get csv from s3 for seed_file: {}.".format(audit.params['seed_file']))
+            raise KeyError("Could not get csv from s3 for seed_file: {}.".format(self.audit.params['seed_file']))
         reader = csv.reader(f)
         for row in reader:
             self.assertEqual(row[0], "testing")
-        try:
-            self.s3.delete_object(
-                Bucket=AuditFileS3Exporter.bucket_name,
-                Key=key
-            )
-        except Exception as e:
-            raise KeyError("Failed to delete object. Object with key {} not found in bucket.".format(key))
