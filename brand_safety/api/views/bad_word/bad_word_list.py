24a3a2332e857a4210cd11de952cf87899ea7ae1
Kenneth <kenneth.oh@channelfactory.com>
2019-05-31 16:20:53 -0700

    Add id for response when restoring soft deleted items
    Since serializer.data is immutable, unable to add id key. Must create a return value to mutate with id field and serializer data.

--- brand_safety/api/views/bad_word/bad_word_list.py
+++ brand_safety/api/views/bad_word/bad_word_list.py
@@ -57,20 +57,26 @@ class BadWordListApiView(ListCreateAPIView):
         serializer = BadWordSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             try:
-                data = serializer.validated_data
-                existing_word = BadWord.all_objects.get(name=data["name"], language=data["language"])
+                validated_data = serializer.validated_data
+                existing_word = BadWord.all_objects.get(name=validated_data["name"], language=validated_data["language"])
                 # If the word has been soft deleted, reset its deleted_at
                 if existing_word.deleted_at is not None:
                     existing_word.deleted_at = None
                     existing_word.save()
+
+                    return_data = serializer.data
+                    return_data["id"] = existing_word.id
+                    status = HTTP_201_CREATED
                 else:
                     # Reject trying to create a word that has not been soft deleted
-                    return Response(
-                        data="{} and {} must make a unique set.".format(data["name"], str(data["language"])),
-                        status=HTTP_400_BAD_REQUEST
-                    )
+                    return_data = "{} and {} must make a unique set.".format(validated_data["name"], str(validated_data["language"]))
+                    status = HTTP_400_BAD_REQUEST
             except BadWord.DoesNotExist:
                 serializer.save()
-            return Response(serializer.data, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+                return_data = serializer.data
+                status = HTTP_201_CREATED
+        else:
+            return_data = serializer.errors
+            status = HTTP_400_BAD_REQUEST
+        return Response(data=return_data, status=status)
 
