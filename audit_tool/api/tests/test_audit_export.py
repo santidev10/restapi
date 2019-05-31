e17db03eacbbc160bbbdb5c0497ed787843b716d
George Su <george.su@channelfactory.com>
2019-05-31 12:56:13 -0700

    Working on unit test.

--- audit_tool/api/tests/test_audit_export.py
+++ audit_tool/api/tests/test_audit_export.py
@@ -1,6 +1,7 @@
 from tempfile import mkstemp
 import csv
 from rest_framework.status import HTTP_200_OK
+from django.utils import timezone
 
 from audit_tool.models import AuditProcessor
 from audit_tool.models import AuditVideoProcessor
@@ -14,19 +15,20 @@ from audit_tool.models import AuditCategory
 from audit_tool.models import AuditCountry
 from audit_tool.api.urls.names import AuditPathName
 from audit_tool.api.views.audit_export import AuditS3Exporter
-from audit_tool.api.views import AuditExportApiView
 
 from saas.urls.namespaces import Namespace
 from utils.utittests.reverse import reverse
 from utils.utittests.test_case import ExtendedAPITestCase
 
-class AuditSaveAPITestCase(ExtendedAPITestCase):
+
+class AuditExportAPITestCase(ExtendedAPITestCase):
     url = reverse(AuditPathName.AUDIT_EXPORT, [Namespace.AUDIT_TOOL])
 
     def setUp(self):
         self.create_admin_user()
+        self.s3 = AuditS3Exporter._s3()
         video_params = {
-            'name': 'test_video',
+            'name': str(timezone.now()),
             'language': 'en'
         }
 
@@ -36,22 +38,73 @@ class AuditSaveAPITestCase(ExtendedAPITestCase):
         }
 
         test_channel = AuditChannel.objects.create(id=2)
-        test_video = AuditVideo.objects.create(channel=test_channel, video_id=4)
+        test_video = AuditVideo.objects.create(channel=test_channel, video_id=4, video_id_hash=4)
 
-        video_category = AuditCategory(category="Gaming", category_display="Gaming")
-        video_language = AuditLanguage(language="ru")
-        channel_language = AuditLanguage(language="en")
-        test_country = AuditCountry(country="CN")
+        video_category = AuditCategory.objects.create(category="Gaming", category_display="Gaming")
+        video_language = AuditLanguage.objects.create(language="ru")
+        channel_language = AuditLanguage.objects.create(language="en")
+        test_country = AuditCountry.objects.create(country="CN")
 
         test_video_meta = AuditVideoMeta.objects.create(video=test_video, name="Dota 2", language=video_language,
                                                         category=video_category, views=9000, likes=9999, dislikes=1)
         test_channel_meta = AuditChannelMeta.objects.create(channel=test_channel, name="Valve",
                                                             language=channel_language, view_count=50000,
-                                                            subscribers=30000, country=test_country
+                                                            subscribers=30000, country=test_country, video_count=1
                                                             )
 
-        video_audit = AuditProcessor.objects.create(audit_type=1, params=video_params, completed=True)
-        channel_audit = AuditProcessor.objects.create(audit_type=2, params=channel_params, completed=True)
+        self.video_audit = AuditProcessor.objects.create(audit_type=1, params=video_params, completed=timezone.now())
+        self.channel_audit = AuditProcessor.objects.create(audit_type=2, params=channel_params, completed=timezone.now())
 
-        video_processor = AuditVideoProcessor.objects.create(audit=video_audit, )
-        channel_processor = AuditChannelProcessor.objects.create(audit=channel_audit, )
+        video_processor = AuditVideoProcessor.objects.create(audit=self.video_audit, video=test_video, clean=False,
+                                                             word_hits={"exclusion": ["cyka", "blyat", "fuck", "shit"]})
+        channel_processor = AuditChannelProcessor.objects.create(audit=self.channel_audit,
+                                                                 channel=test_channel, clean=True,
+                                                                 word_hits={
+                                                                     "exclusion": ["cyka", "blyat", "fuck", "shit"]
+                                                                 })
+
+    def tearDown(self):
+        if 'export_false' in self.video_audit.params:
+            video_audit_key = 'export_{}_{}_false.csv'.format(self.video_audit.id, self.video_audit.name)
+            try:
+                self.s3.delete_object(
+                    Bucket=AuditS3Exporter.bucket_name,
+                    Key=video_audit_key
+                )
+            except Exception as e:
+                raise KeyError("Failed to delete object. Object with key {} not found in bucket."
+                               .format(video_audit_key))
+
+        if 'export_true' in self.channel_audit.params:
+            channel_audit_key = 'export_{}_{}_true.csv'.format(self.channel_audit.id, self.channel_audit.name)
+            try:
+                self.s3.delete_object(
+                    Bucket=AuditS3Exporter.bucket_name,
+                    Key=channel_audit_key
+                )
+            except Exception as e:
+                raise KeyError(
+                    "Failed to delete object. Object with key {} not found in bucket."
+                        .format(channel_audit_key))
+
+
+    def test_video_export(self):
+        try:
+            video_response = self.client.get(self.url + "?audit_id={}&clean=False".format(self.video_audit.id))
+        except Exception as e:
+            raise KeyError("No Audit with id: {} found.".format(self.video_audit.id))
+        reader = csv.reader(video_response)
+        for row in reader:
+            print(row)
+        return
+
+
+    def test_channel_export(self):
+        try:
+            channel_response = self.client.get(self.url + "?audit_id={}&clean=True".format(self.channel_audit.id))
+        except Exception as e:
+            raise KeyError("No Audit with id: {} found.".format(self.channel_audit.id))
+        reader = csv.reader(channel_response)
+        for row in reader:
+            print(row)
+        return
