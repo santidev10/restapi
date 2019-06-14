dec04cb9a2720fbc8d735e54ba53193bd8822d1f
Kenneth <kenneth.oh@channelfactory.com>
2019-06-14 10:18:17 -0700

    API post development

--- segment/api/views/custom_segment/segment_wizard.py
+++ segment/api/views/custom_segment/segment_wizard.py
@@ -1,7 +1,96 @@
+from django.conf import settings
+
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
+
 class SegmentWizardAPIView(APIView):
-    pass
+    REQUIRED_CUSTOM_SEGMENT_FIELDS = "type, content, score_threshold, brand_safety_categories, youtube_categories"
+
+    def post(self, request, *args, **kwargs):
+        query = {
+            "query": {
+                "bool": {
+                    "must": {
+                        "nested": {
+                            "path": "categories",
+                            "query": {
+                                "bool": {
+                                    "filter": []
+                                }
+                            }
+                        }
+                    },
+                    "should": []
+                }
+            }
+        }
+        data = request.data
+        segment_filters = query["query"]["bool"]["should"]
+        category_score_filters = query["query"]["bool"]["must"]["nested"]["query"]["bool"]["filter"]
+        try:
+            segment_type = data["type"]
+            segment_content = data["segment_content"]
+            languages = data["langauges"]
+            youtube_categories = data["youtube_categories"]
+            score_threshold = data["score_threshold"]
+        except KeyError:
+            return Response(status=HTTP_400_BAD_REQUEST,
+                            data="You must provide these fields: {}".format(self.REQUIRED_CUSTOM_SEGMENT_FIELDS))
+
+        config = self._get_segment_config(segment_content, segment_type)
+        if config is None:
+            return Response(status=HTTP_400_BAD_REQUEST, data="Invalid options: {}".format(",".join([segment_type, segment_content])))
+
+        # {"range": {"categories.1.category_score": {"gte": 50}}}
+        category_score_params = [
+            {
+                "range": {"categories.{}.category_score".format(category): {range_operator: score_threshold}}
+            } for category in data["brand_safety_categories"]
+        ]
+        youtube_categories = [
+            {
+                "term": {"youtube_category": category}
+            } for category in youtube_categories
+        ]
+        languages = [
+            {
+                "term": {"language": language}
+            } for language in languages
+        ]
+
+        category_score_filters.extend(category_score_params)
+        segment_filters.extend(youtube_categories)
+        segment_filters.extend(languages)
+
+
+
+
+    def _get_segment_config(self, segment_content, segment_type):
+        options = {
+            "channel": {
+                "index": settings.BRAND_SAFETY_CHANNEL_INDEX,
+                "minimum_option": "subscribers"
+            },
+            "video": {
+                "index": settings.BRAND_SAFETY_VIDEO_INDEX,
+                "minimum_option": "views"
+            },
+            "range_operator": {
+                "blacklist": "lte",
+                "whitelist": "gte"
+            },
+        }
+        try:
+            config = {
+                "index": options[segment_content]["index"],
+                "minimum_option": options[segment_content]["minimum_option"],
+                "range_operator": options[segment_type],
+            }
+        except KeyError:
+            config = None
+        return config
 
 
 # {
