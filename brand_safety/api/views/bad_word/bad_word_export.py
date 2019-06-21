767cd755ca168f5d4a71cf425792fa1e826fa4c3
George Su <george.su@channelfactory.com>
2019-06-21 12:09:03 -0700

    Added language filtering to BadWordExport queryset.

--- brand_safety/api/views/bad_word/bad_word_export.py
+++ brand_safety/api/views/bad_word/bad_word_export.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import IsAdminUser
 from rest_framework_csv.renderers import CSVStreamingRenderer
+from django_filters.rest_framework import DjangoFilterBackend
 
 from brand_safety.api.serializers.bad_word_serializer import BadWordSerializer
 from brand_safety.models import BadWord
@@ -7,9 +8,8 @@ from utils.api.file_list_api_view import FileListApiView
 
 
 class BadWordCSVRendered(CSVStreamingRenderer):
-    header = ["id", "name", "category", "language"]
+    header = ["name", "category", "language"]
     labels = {
-        "id": "Id",
         "name": "Name",
         "category": "Category",
         "language": "Language"
@@ -20,5 +20,20 @@ class BadWordExportApiView(FileListApiView):
     permission_classes = (IsAdminUser,)
     serializer_class = BadWordSerializer
     renderer_classes = (BadWordCSVRendered,)
-    queryset = BadWord.objects.all().order_by("name")
     filename = "Bad Words.csv"
+
+    def do_filters(self, queryset):
+        filters = {}
+
+        language = self.request.query_params.get("language")
+        if language:
+            filters["language__language"] = language
+
+        if filters:
+            queryset = queryset.filter(**filters)
+        return queryset
+
+    def get_queryset(self):
+        queryset = BadWord.objects.all().order_by("name")
+        queryset = self.do_filters(queryset)
+        return queryset
