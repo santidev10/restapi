e08968a4e45af3c4b0f9c8ddfb828736a172975a
George Su <george.su@channelfactory.com>
2019-06-21 14:43:12 -0700

    Added category filtering to csv export.

--- brand_safety/api/views/bad_word/bad_word_export.py
+++ brand_safety/api/views/bad_word/bad_word_export.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import IsAdminUser
 from rest_framework_csv.renderers import CSVStreamingRenderer
+from rest_framework.serializers import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
 
 from brand_safety.api.serializers.bad_word_serializer import BadWordSerializer
@@ -20,12 +21,22 @@ class BadWordExportApiView(FileListApiView):
     permission_classes = (IsAdminUser,)
     serializer_class = BadWordSerializer
     renderer_classes = (BadWordCSVRendered,)
+    queryset = BadWord.objects.all().order_by("name")
     filename = "Bad Words.csv"
 
     def do_filters(self, queryset):
         filters = {}
 
+        category = self.request.query_params.get("category")
+        if category:
+            try:
+                category_id = int(category)
+                filters["category_id"] = category_id
+            except ValueError:
+                raise ValidationError("Category filter param must be Category ID value. Received: {}.".format(category))
+
         language = self.request.query_params.get("language")
+
         if language:
             filters["language__language"] = language
 
@@ -34,6 +45,5 @@ class BadWordExportApiView(FileListApiView):
         return queryset
 
     def get_queryset(self):
-        queryset = BadWord.objects.all().order_by("name")
-        queryset = self.do_filters(queryset)
-        return queryset
+        self.queryset = self.do_filters(self.queryset)
+        return self.queryset
