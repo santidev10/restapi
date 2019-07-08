f419f715cf5fc6bedd541f0bc64ee4f5e262ba15
Andrew <andrew@kabuto.com>
2019-07-08 14:16:09 -0700

    change name of resumed audit;

--- audit_tool/api/views/audit_resume.py
+++ audit_tool/api/views/audit_resume.py
@@ -19,7 +19,7 @@ class AuditResumeApiView(APIView):
                 else:
                     related_audits = related_audits + [audit.id]
                 params['related_audits'] = related_audits
-                params['name'] = '{}: resumed'.format(params['name'])
+                params['name'] = 'Resumed: {}'.format(params['name'])
                 new_audit = AuditProcessor.objects.create(
                     audit_type=0,
                     params=params,
