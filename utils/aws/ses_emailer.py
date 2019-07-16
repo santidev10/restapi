7dc75230b00288cf182da493b5f10266e2ab3898
Kenneth <kenneth.oh@channelfactory.com>
2019-07-16 11:10:13 -0700

    Create custom SESEmailer exception; encountering pickling build errors

--- utils/aws/ses_emailer.py
+++ utils/aws/ses_emailer.py
@@ -1,6 +1,5 @@
 import boto3
 from botocore.exceptions import ClientError
-from django.core.exceptions import ValidationError
 
 from django.conf import settings
 
@@ -42,7 +41,11 @@ class SESEmailer(object):
                 Source=host,
             )
         except ClientError:
-            raise ValidationError("Failed to send email. Either the sender ({}) or the "
-                                  "recipient ({}) is an invalid email address.".format(host, recipients))
+            raise SESEmailerException("Failed to send email. Either the sender ({}) or the "
+                "recipient ({}) is an invalid email address.".format(host, recipients))
         except Exception as e:
             raise e
+
+
+class SESEmailerException(Exception):
+    pass
