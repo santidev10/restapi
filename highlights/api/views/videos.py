c45d6db0077ce245711728e7a22c637fa9075da3
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2019-07-29 10:21:35 +0300

    VIQ-2007: Fix permissions

--- highlights/api/views/videos.py
+++ highlights/api/views/videos.py
@@ -1,30 +1,32 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-from rest_framework.status import HTTP_408_REQUEST_TIMEOUT
 
-from highlights.api.views.highlights_query import HighlightsQuery
 from singledb.api.views import SingledbApiView
-from singledb.connector import SingleDatabaseApiConnector as Connector
-from singledb.connector import SingleDatabaseApiConnectorException
 from utils.brand_safety_view_decorator import add_brand_safety_data
-from video.api.views import VideoListApiView
+from utils.permissions import or_permission_classes
+from utils.permissions import user_has_permission
 
 
 class HighlightVideosListApiView(SingledbApiView):
-    permission_required = (
-        "userprofile.view_highlights",
+    permission_classes = (
+        or_permission_classes(
+            user_has_permission("userprofile.view_highlights"),
+            IsAdminUser
+        ),
     )
 
     @add_brand_safety_data
     def get(self, request, *args, **kwargs):
-        request_query_params = request.query_params
-        query_params = HighlightsQuery(request_query_params).prepare_query(mode="video")
-        connector = Connector()
-        try:
-            response_data = connector.get_highlights_videos(query_params)
-        except SingleDatabaseApiConnectorException as e:
-            return Response(
-                data={"error": " ".join(e.args)},
-                status=HTTP_408_REQUEST_TIMEOUT)
-        VideoListApiView.adapt_response_data(response_data, request.user)
-        response_data = HighlightsQuery.adapt_language_aggregation(response_data)
-        return Response(response_data)
+        return Response(None)
+        # request_query_params = request.query_params
+        # query_params = HighlightsQuery(request_query_params).prepare_query(mode="video")
+        # connector = Connector()
+        # try:
+        #     response_data = connector.get_highlights_videos(query_params)
+        # except SingleDatabaseApiConnectorException as e:
+        #     return Response(
+        #         data={"error": " ".join(e.args)},
+        #         status=HTTP_408_REQUEST_TIMEOUT)
+        # VideoListApiView.adapt_response_data(response_data, request.user)
+        # response_data = HighlightsQuery.adapt_language_aggregation(response_data)
+        # return Response(response_data)
