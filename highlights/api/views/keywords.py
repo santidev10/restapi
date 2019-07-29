7e5f42c898ff962e33c003ea5093e8768e5ec221
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2019-07-29 17:01:17 +0300

    VIQ-2007: Update keywords permissions

--- highlights/api/views/keywords.py
+++ highlights/api/views/keywords.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.status import HTTP_408_REQUEST_TIMEOUT
 
@@ -6,11 +7,16 @@ from keywords.api import KeywordListApiView
 from singledb.api.views import SingledbApiView
 from singledb.connector import SingleDatabaseApiConnector as Connector
 from singledb.connector import SingleDatabaseApiConnectorException
+from utils.permissions import or_permission_classes
+from utils.permissions import user_has_permission
 
 
 class HighlightKeywordsListApiView(SingledbApiView):
-    permission_required = (
-        "userprofile.view_highlights",
+    permission_classes = (
+        or_permission_classes(
+            user_has_permission("userprofile.view_highlights"),
+            IsAdminUser
+        ),
     )
 
     def get(self, request, *args, **kwargs):
--- highlights/tests/api/views/test_keywords.py
+++ highlights/tests/api/views/test_keywords.py
@@ -1,35 +1,185 @@
-from unittest.mock import patch
-from urllib import parse
-
 from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_401_UNAUTHORIZED
+from rest_framework.status import HTTP_403_FORBIDDEN
 
-import singledb.connector
+from es_components.constants import Sections
+from es_components.managers import KeywordManager
+from es_components.models import Keyword
+from es_components.tests.utils import ESTestCase
 from highlights.api.urls.names import HighlightsNames
 from saas.urls.namespaces import Namespace
-from utils.utittests.response import MockResponse
+from utils.lang import ExtendedEnum
+from utils.utittests.int_iterator import int_iterator
 from utils.utittests.reverse import reverse
 from utils.utittests.test_case import ExtendedAPITestCase
 
 
-class HighlightKeywordsListApiViewTestCase(ExtendedAPITestCase):
-    @patch("singledb.connector.requests")
-    def test_list_requests_views_from_sdb(self, requests_mock):
+class HighlightKeywordPermissionsApiViewTestCase(ExtendedAPITestCase):
+
+    def test_unauthorized(self):
+        url = get_url()
+        response = self.client.get(url)
+
+        self.assertEqual(
+            HTTP_401_UNAUTHORIZED,
+            response.status_code,
+        )
+
+    def test_forbidden(self):
         self.create_test_user()
-        requests_mock.get.return_value = MockResponse(json=dict())
-        url = reverse(HighlightsNames.KEYWORDS, [Namespace.HIGHLIGHTS],
-                      query_params=dict(page=1, sort_by="thirty_days_views"))
-        with patch("highlights.api.views.keywords.Connector",
-                   new=singledb.connector.SingleDatabaseApiConnector_origin):
-            response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTP_200_OK)
-        call_url = requests_mock.get.call_args[0][0]
-        requests_mock.get.assert_called_once_with(call_url, headers={"Content-Type": "application/json"}, verify=False)
-        parsed_url = parse.urlparse(call_url)
-        query_params = parse.parse_qs(parsed_url.query)
-        fields = query_params.get("fields", [])
-        self.assertEqual(len(fields), 1)
-        fields = fields[0].split(",")
-        self.assertIn("daily_views", fields)
-        self.assertIn("weekly_views", fields)
-        self.assertIn("thirty_days_views", fields)
-        self.assertIn("views", fields)
+
+        url = get_url()
+        response = self.client.get(url)
+
+        self.assertEqual(
+            HTTP_403_FORBIDDEN,
+            response.status_code,
+        )
+
+    def test_has_permission(self):
+        user = self.create_test_user()
+        user.add_custom_user_permission("view_highlights")
+
+        url = get_url()
+        response = self.client.get(url)
+
+        self.assertEqual(
+            HTTP_200_OK,
+            response.status_code,
+        )
+
+    def test_admin(self):
+        self.create_admin_user()
+
+        url = get_url()
+        response = self.client.get(url)
+
+        self.assertEqual(
+            HTTP_200_OK,
+            response.status_code,
+        )
+
+
+class HighlightKeywordBaseApiViewTestCase(ExtendedAPITestCase, ESTestCase):
+
+    def setUp(self):
+        super(HighlightKeywordBaseApiViewTestCase, self).setUp()
+        self.user = self.create_test_user()
+        self.user.add_custom_user_permission("view_highlights")
+
+
+class HighlightKeywordAggregationsApiViewTestCase(HighlightKeywordBaseApiViewTestCase):
+
+    def test_aggregations_no_aggregations(self):
+        url = get_url()
+        response = self.client.get(url)
+
+        self.assertIn("aggregations", response.data)
+        self.assertIsNone(response.data["aggregations"])
+
+    def test_aggregations_empty(self):
+        url = get_url(size=0, aggregations=",".join(AllowedAggregations.values()))
+        response = self.client.get(url)
+
+        self.assertIn("aggregations", response.data)
+        expected_aggregations = {
+            agg: dict(buckets=[], doc_count_error_upper_bound=0, sum_other_doc_count=0)
+            for agg in AllowedAggregations.values()
+        }
+        self.assertEqual(expected_aggregations, response.data["aggregations"])
+
+    def test_aggregations_categories(self):
+        category = "Music"
+        keyword = Keyword(id=next(int_iterator))
+        keyword.populate_general_data(category=category)
+        KeywordManager(Sections.GENERAL_DATA).upsert([keyword])
+
+        url = get_url(size=0, aggregations=AllowedAggregations.CATEGORY.value)
+        response = self.client.get(url)
+
+        self.assertIn("aggregations", response.data)
+        self.assertIn("general_data.category", response.data["aggregations"])
+        self.assertEqual(
+            [dict(key=category, doc_count=1)],
+            response.data["aggregations"]["general_data.category"]["buckets"]
+        )
+
+    def test_aggregations_languages(self):
+        language = "English"
+        keyword = Keyword(id=next(int_iterator))
+        keyword.populate_general_data(language=language)
+        KeywordManager(Sections.GENERAL_DATA).upsert([keyword])
+
+        url = get_url(size=0, aggregations=AllowedAggregations.LANGUAGE.value)
+        response = self.client.get(url)
+
+        self.assertIn("aggregations", response.data)
+        self.assertIn("general_data.language", response.data["aggregations"])
+        self.assertEqual(
+            [dict(key=language, doc_count=1)],
+            response.data["aggregations"]["general_data.language"]["buckets"]
+        )
+
+
+class HighlightKeywordItemsApiViewTestCase(HighlightKeywordBaseApiViewTestCase):
+
+    def test_no_items(self):
+        url = get_url(size=0)
+        response = self.client.get(url)
+
+        self.assertIn("items", response.data)
+        self.assertEqual([], response.data["items"])
+
+    def test_items_page_size(self):
+        page_size = 20
+        keywords = [Keyword(id=next(int_iterator)) for _ in range(page_size + 1)]
+        KeywordManager(Sections.GENERAL_DATA).upsert(keywords)
+
+        url = get_url(page=1, sort=AllowedSorts.VIEWS_30_DAYS_DESC.value)
+        response = self.client.get(url)
+
+        self.assertEqual(
+            page_size,
+            len(response.data["items"])
+        )
+
+    def test_max_items(self):
+        max_page = 5
+        page_size = 20
+        total_items = page_size * max_page + 1
+        keywords = [Keyword(id=next(int_iterator)) for _ in range(total_items)]
+        KeywordManager(Sections.GENERAL_DATA).upsert(keywords)
+
+        url = get_url(page=1, sort=AllowedSorts.VIEWS_30_DAYS_DESC.value)
+        response = self.client.get(url)
+
+        self.assertEqual(
+            max_page,
+            response.data["max_page"]
+        )
+
+    def test_filter_languages(self):
+        language = "lang"
+        keywords = [Keyword(id=next(int_iterator)) for _ in range(2)]
+        keywords[0].populate_general_data(language=language)
+        KeywordManager(Sections.GENERAL_DATA).upsert(keywords)
+
+        url = get_url(**{AllowedAggregations.LANGUAGE.value: language})
+        response = self.client.get(url)
+
+        self.assertEqual(1, response.data["items_count"])
+        self.assertEqual(keywords[0].main.id, response.data["items"][0]["main"]["id"])
+
+
+class AllowedAggregations(ExtendedEnum):
+    CATEGORY = "general_data.category"
+    LANGUAGE = "general_data.language"
+
+
+class AllowedSorts(ExtendedEnum):
+    VIEWS_30_DAYS_DESC = "stats.last_30day_views:desc"
+
+
+def get_url(**kwargs):
+    return reverse(HighlightsNames.KEYWORDS, [Namespace.HIGHLIGHTS],
+                   query_params=kwargs)
