cbf4ab89a110aae1ce2bb392c39ca151d3612353
Kenneth <kenneth.oh@channelfactory.com>
2019-08-08 12:27:04 -0700

    Fix manual audit methods

---
+++ brand_safety/auditors/brand_safety_audit.py
@@ -0,0 +1,318 @@
+from collections import Counter
+from collections import defaultdict
+from datetime import datetime
+import logging
+import multiprocessing as mp
+import time
+
+from brand_safety.constants import BRAND_SAFETY_SCORE
+from brand_safety.audit_models.brand_safety_channel_audit import BrandSafetyChannelAudit
+from brand_safety.audit_models.brand_safety_video_audit import BrandSafetyVideoAudit
+from brand_safety.auditors.utils import AuditUtils
+from es_components.constants import MAIN_ID_FIELD
+from es_components.constants import Sections
+from es_components.constants import VIDEO_CHANNEL_ID_FIELD
+from es_components.managers import ChannelManager
+from es_components.managers import VideoManager
+from es_components.query_builder import QueryBuilder
+from es_components.connections import init_es_connection
+
+init_es_connection()
+
+logger = logging.getLogger(__name__)
+
+
+class BrandSafetyAudit(object):
+    """
+    Interface for reading source data and providing it to services
+    """
+    MAX_POOL_COUNT = 4
+    CHANNEL_POOL_BATCH_SIZE = 10
+    CHANNEL_MASTER_BATCH_SIZE = MAX_POOL_COUNT * CHANNEL_POOL_BATCH_SIZE
+    # Hours in which a channel should be updated
+    UPDATE_TIME_THRESHOLD = 24 * 7
+    CHANNEL_BATCH_COUNTER_LIMIT = 500
+    ES_LIMIT = 10000
+    SLEEP = 2
+    channel_batch_counter = 0
+
+    def __init__(self, *_, **kwargs):
+        # If initialized with an APIScriptTracker instance, then expected to run full brand safety
+        # else main run method should not be called since it relies on an APIScriptTracker instance
+        try:
+            self.script_tracker = kwargs["api_tracker"]
+            self.cursor_id = self.script_tracker.cursor_id
+            self.is_manual = False
+        except KeyError:
+            self.is_manual = True
+        self.audit_utils = AuditUtils()
+        self.channel_manager = ChannelManager(
+            sections=(Sections.GENERAL_DATA, Sections.MAIN, Sections.STATS),
+            upsert_sections=(Sections.BRAND_SAFETY,)
+        )
+        self.video_manager = VideoManager(
+            sections=(Sections.GENERAL_DATA, Sections.MAIN, Sections.STATS, Sections.CHANNEL, Sections.CAPTIONS),
+            upsert_sections=(Sections.BRAND_SAFETY,)
+        )
+
+    def run(self):
+        """
+        Pools processes to handle main audit logic and processes results
+            If initialized with an APIScriptTracker instance, then expected to run full brand safety
+                else main run method should not be called since it relies on an APIScriptTracker instance
+        :return: None
+        """
+        if self.is_manual:
+            raise ValueError("Provider was not initialized with an APIScriptTracker instance.")
+        pool = mp.Pool(processes=self.MAX_POOL_COUNT)
+        for channel_batch in self._channel_generator(self.cursor_id):
+            # Some batches may be empty if none of the channels retrieved have full data to be audited
+            # _channel_generator will stop when no items are retrieved from Elasticsearch
+            if not channel_batch:
+                continue
+            results = pool.map(self._process_audits,
+                               self.audit_utils.batch(channel_batch, self.CHANNEL_POOL_BATCH_SIZE))
+            # Extract nested results from each process and index into es
+            video_audits, channel_audits = self._extract_results(results)
+            # Index items
+            self._index_results(video_audits, channel_audits)
+            # Update config in case they have been modified since last batch
+            self.audit_utils.update_config()
+            time.sleep(self.SLEEP)
+        logger.error("Complete.")
+
+    def _process_audits(self, channels: list) -> dict:
+        """
+        Drives main brand safety logic for each process
+        :param channels: Channel documents with videos to retrieve video data for
+        :return:
+        """
+        results = {
+            "video_audits": [],
+            "channel_audits": []
+        }
+        for channel in channels:
+            video_audits = self.audit_videos(videos=channel["videos"])
+            channel["video_audits"] = video_audits
+            channel_audit = self.audit_channel(channel)
+
+            results["video_audits"].extend(video_audits)
+            results["channel_audits"].append(channel_audit)
+        return results
+
+    def audit_video(self, video_data: dict, full_audit=True) -> BrandSafetyVideoAudit:
+        """
+        Audit single video
+        :param video_data: dict -> Data to audit
+            Required keys: video_id, title
+            Optional keys: description, tags, transcript
+        :return:
+        """
+        # Every audit should have language_processors in config
+        audit = BrandSafetyVideoAudit(
+            video_data,
+            self.audit_utils
+        )
+        audit.run()
+        if not full_audit:
+            audit = getattr(audit, BRAND_SAFETY_SCORE).overall_score
+        return audit
+
+    def audit_videos(self, channels=None, videos=None):
+        """
+        :param channels: list (dict) -> Channels to audit videos for
+        :param videos: list (dict) -> Data to provide to BrandSafetyVideoAudit
+        :return: list (int | BrandSafetyVideoAudit) ->
+            full_audit=False: (int) BrandSafetyVideoAudit score
+            full_audit=True: BrandSafetyVideoAudit object
+        """
+        # Set defaults here to get access to self
+        video_audits = []
+        if videos and channels:
+            raise ValueError("You must either provide video data to audit or channels to retrieve video data for.")
+        if videos is None:
+            videos = self._get_channel_videos(channels)
+        for video in videos:
+            try:
+                audit = self.audit_video(video)
+                video_audits.append(audit)
+            except KeyError as e:
+                # Ignore videos without full data
+                continue
+        return video_audits
+
+    def audit_channel(self, channel_data, full_audit=True):
+        """
+        Audit single channel
+        :param channel_data: dict -> Data to audit
+            Required keys: channel_id, title
+            Optional keys: description, video_tags
+        :return:
+        """
+        audit = BrandSafetyChannelAudit(channel_data, self.audit_utils)
+        audit.run()
+        if not full_audit:
+            audit = getattr(audit, BRAND_SAFETY_SCORE).overall_score
+        return audit
+
+    def audit_channels(self, channel_video_audits: dict = None) -> list:
+        """
+        Audits Channels by retrieving channel data and using sorted Video audit objects by channel id
+        :param channel_video_audits: BrandSafetyVideoAudit objects
+        :return: list -> BrandSafetyChannelAudit Audit objects
+        """
+        channel_audits = []
+        for _id, data in channel_video_audits.items():
+            try:
+                audit = self.audit_channel(data)
+                channel_audits.append(audit)
+            except KeyError as e:
+                # Ignore channels without full data
+                continue
+        return channel_audits
+
+    def _get_channel_videos(self, channel_ids: list) -> list:
+        """
+        Get videos for channels
+        :param channels: dict -> channel_id, channel_metadata
+        :return:
+        """
+        filters = QueryBuilder().build().must().terms().field(VIDEO_CHANNEL_ID_FIELD).value(channel_ids).get()
+        results = self.video_manager.search(filters=filters, limit=self.ES_LIMIT).execute()["hits"]["hits"]
+        mapped = []
+        for video in results:
+            try:
+                mapped.append(self.audit_utils.extract_video_data(video["_source"]))
+            except (KeyError, AttributeError):
+                continue
+        return mapped
+
+    def _extract_results(self, results: list):
+        """
+        Extracts nested results from each of the processes
+        :param results: list -> Dict results from each process
+        :return:
+        """
+        video_audits = []
+        channel_audits = []
+        for batch in results:
+            video_audits.extend(batch["video_audits"])
+            channel_audits.extend(batch["channel_audits"])
+        return video_audits, channel_audits
+
+    def _index_results(self, video_audits, channel_audits):
+        """
+        Upsert documents with brand safety data
+        :param video_audits: list -> BrandSafetyVideo audits
+        :param channel_audits: list -> BrandSafetyChannel audits
+        :return:
+        """
+        videos = [audit.instantiate_es() for audit in video_audits]
+        channels = [audit.instantiate_es() for audit in channel_audits]
+        self.channel_manager.upsert(channels)
+        self.video_manager.upsert(videos)
+
+    def _channel_generator(self, cursor_id=None):
+        """
+        Yields channels to audit
+        :param cursor_id: Cursor position to start audit
+        :return: list -> Elasticsearch channel documents
+        """
+        cursor_id = cursor_id or ""
+        while True:
+            batch_query = QueryBuilder().build().must().range().field(MAIN_ID_FIELD).gt(cursor_id).get()
+            results = self.channel_manager.search(batch_query, limit=self.CHANNEL_MASTER_BATCH_SIZE).execute()["hits"][
+                "hits"]
+            if not results:
+                self.audit_utils.set_cursor(self.script_tracker, None, integer=False)
+                break
+            to_update = self._get_channels_to_update(results)
+            yield to_update
+            cursor_id = results[-1]["_id"]
+            self.script_tracker = self.audit_utils.set_cursor(self.script_tracker, cursor_id, integer=False)
+            self.cursor_id = self.script_tracker.cursor_id
+            self.channel_batch_counter += 1
+
+    def _get_channels_to_update(self, channel_batch):
+        """
+        Gets channels to update
+            If either the last time the channel has been updated is greater than threshold time or if the number of
+            videos has changed since the last time the channel was scored, it should be updated
+        :param channel_batch: list
+        :return: list
+        """
+        channels = {}
+        channels_to_update = []
+        # Exclude channels that do not have data
+        for item in channel_batch:
+            try:
+                channels[item["_id"]] = self.audit_utils.extract_channel_data(item["_source"])
+            except KeyError:
+                # Ignore channels that we do not have full metadata for
+                continue
+        # Get videos for channels
+        videos = self._get_channel_videos(list(channels.keys()))
+        videos_by_channel = defaultdict(list)
+        for video in videos:
+            videos_by_channel[video["channel_id"]].append(video)
+        # Get counts of videos for each channel
+        channel_video_counts = Counter([item["channel_id"] for item in videos if item.get("channel_id")])
+
+        # For each channel retrieved in original query, check if it should be updated
+        for _id, data in channels.items():
+            should_update = False
+            if not data["updated_at"] or not data["videos_scored"]:
+                should_update = True
+            else:
+                time_elapsed = datetime.today() - datetime.strptime(data["updated_at"], "%Y-%m-%d")
+                elapsed_hours = time_elapsed.seconds // 3600
+
+                # If last time channel was updated is greater than threshold or number of channel's videos has changed, rescore
+                if not data["updated_at"] or elapsed_hours >= self.UPDATE_TIME_THRESHOLD or data["videos_scored"] != \
+                        channel_video_counts[_id]:
+                    should_update = True
+
+            if should_update:
+                data["videos"] = videos_by_channel.get(_id, [])
+                channels_to_update.append(data)
+        return channels_to_update
+
+    def manual_channel_audit(self, channel_ids: iter):
+        """
+        Score specific channels and videos
+        :param channel_ids: list | tuple
+        :return: None
+        """
+        to_audit = []
+        channels = self.audit_utils.get_items(channel_ids, self.channel_manager)
+        for item in channels:
+            try:
+                mapped = self.audit_utils.extract_channel_data(item["_source"])
+                mapped["videos"] = self._get_channel_videos([mapped["id"]])
+                to_audit.append(mapped)
+            except KeyError:
+                continue
+        pool = mp.Pool(processes=self.MAX_POOL_COUNT)
+        results = pool.map(self._process_audits, self.audit_utils.batch(to_audit, self.CHANNEL_POOL_BATCH_SIZE))
+
+        # Extract nested results from each process and index into es
+        video_audits, channel_audits = self._extract_results(results)
+        self._index_results(video_audits, channel_audits)
+        return channel_audits
+
+    def manual_video_audit(self, video_ids: iter):
+        """
+        Score specific videos
+        :param video_ids: list | tuple -> Youtube video id strings
+        :return: BrandSafetyVideoAudit objects
+        """
+        videos = self.audit_utils.get_items(video_ids, self.video_manager)
+        mapped = []
+        for item in videos:
+            try:
+                mapped.append(self.audit_utils.extract_video_data(item["_source"]))
+            except KeyError:
+                continue
+        video_audits = self.audit_videos(videos=mapped)
+        self._index_results(video_audits, [])
+        return video_audits
