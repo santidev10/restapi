e91cd8fb0c83bb7b6c54bb5ffc61ef734a0d9cf6
Kenneth <kenneth.oh@channelfactory.com>
2019-08-19 10:26:07 -0700

    Map langauge to human readable and change ItemsFromSegmentIdsApiView to use new elasticsearch indexes

--- segment/api/serializers/custom_segment_export_serializers.py
+++ segment/api/serializers/custom_segment_export_serializers.py
@@ -1,18 +1,27 @@
 from rest_framework.serializers import CharField
 from rest_framework.serializers import Serializer
 from rest_framework.serializers import SerializerMethodField
+from brand_safety.languages import LANGUAGES
 
 
 class CustomSegmentChannelExportSerializer(Serializer):
     URL = SerializerMethodField("get_url")
     Title = CharField(source="general_data.title")
-    Language = CharField(source="brand_safety.language")
+    Language = SerializerMethodField("get_language")
     Category = CharField(source="general_data.top_category")
     Subscribers = CharField(source="stats.subscribers")
     Overall_Score = CharField(source="brand_safety.overall_score")
 
     def get_url(self, obj):
         return f"https://www.youtube.com/channel/{obj.main.id}"
+
+    def get_language(self, obj):
+        brand_safety_language = getattr(obj.brand_safety, "language", None)
+        if brand_safety_language == "all":
+            language = "All"
+        else:
+            language = LANGUAGES.get(brand_safety_language, brand_safety_language)
+        return language
 
 
 class CustomSegmentVideoExportSerializer(Serializer):
@@ -25,3 +34,11 @@ class CustomSegmentVideoExportSerializer(Serializer):
 
     def get_url(self, obj):
         return f"https://www.youtube.com/watch?v={obj.main.id}"
+
+    def get_language(self, obj):
+        brand_safety_language = getattr(obj.brand_safety, "language", None)
+        if brand_safety_language == "all":
+            language = "All"
+        else:
+            language = LANGUAGES.get(brand_safety_language, brand_safety_language)
+        return language
