8f873372761d8f5ba1eacf4d30d003c613a0be12
Andrii Dobrovolskyi <andrii.dobrovolskyi@sigma.software>
2019-08-19 18:18:59 +0300

    VIQ-2333 tests

--- aw_creation/api/tests/test_targeting_items_search.py
+++ aw_creation/api/tests/test_targeting_items_search.py
@@ -1,7 +1,15 @@
+from time import sleep
 from django.core.urlresolvers import reverse
 from rest_framework.status import HTTP_200_OK, HTTP_403_FORBIDDEN
 
 from es_components.tests.utils import ESTestCase
+from es_components.constants import Sections
+from es_components.managers import ChannelManager
+from es_components.managers import KeywordManager
+from es_components.managers import VideoManager
+from es_components.models import Channel
+from es_components.models import Keyword
+from es_components.models import Video
 from utils.utittests.test_case import ExtendedAPITestCase
 
 
@@ -20,6 +28,10 @@ class TargetingItemsSearchAPITestCase(ExtendedAPITestCase, ESTestCase):
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
 
     def test_success_video(self):
+        video = Video(id=123)
+        video.populate_general_data(title="gangnam")
+        VideoManager(sections=Sections.GENERAL_DATA).upsert([video])
+        sleep(1)
         url = reverse("aw_creation_urls:targeting_items_search",
                       args=("video", "gangnam"))
         response = self.client.get(url)
@@ -28,10 +40,16 @@ class TargetingItemsSearchAPITestCase(ExtendedAPITestCase, ESTestCase):
         self.assertEqual(set(response.data[0].keys()), {"id", "name", "thumbnail", "criteria"})
 
     def test_success_channel(self):
+        channel = Channel(id=123)
+        channel.populate_general_data(title="smthing")
+        ChannelManager(sections=Sections.GENERAL_DATA).upsert([channel])
+        sleep(1)
         url = reverse("aw_creation_urls:targeting_items_search",
-                      args=("video", "smthing"))
+                      args=("channel", "smthing"))
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertGreater(len(response.data), 0)
+        self.assertEqual(set(response.data[0].keys()), {"id", "name", "thumbnail", "criteria"})
 
     def test_success_keyword(self):
         from keyword_tool.models import KeyWord
@@ -42,3 +60,6 @@ class TargetingItemsSearchAPITestCase(ExtendedAPITestCase, ESTestCase):
                       args=("keyword", "am"))
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+        self.assertEqual(set(response.data[0].keys()), {"name", "criteria"})
+        self.assertEqual(response.data[0]["name"], "am")
