2f7812f60ebf834163923259232be2092a11a0cb
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2019-08-23 14:06:31 +0300

    VIQ-2261: Fix default value for json fields

--- segment/models/custom_segment.py
+++ segment/models/custom_segment.py
@@ -66,7 +66,7 @@ class CustomSegment(Timestampable):
     }
 
     uuid = UUIDField(unique=True)
-    statistics = JSONField(default=dict())
+    statistics = JSONField(default=dict)
     list_type = IntegerField(choices=LIST_TYPE_CHOICES)
     owner = ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=CASCADE)
     segment_type = IntegerField(choices=SEGMENT_TYPE_CHOICES, db_index=True)
