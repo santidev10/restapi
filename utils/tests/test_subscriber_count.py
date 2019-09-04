ead62100c43cdd3cd0874b4f7312a0209552f694
George Su <george.su@channelfactory.com>
2019-09-04 09:28:28 -0700

    Added more test strings to test_subscriber_count_converter.

--- utils/tests/test_subscriber_count.py
+++ utils/tests/test_subscriber_count.py
@@ -10,9 +10,15 @@ class SubscriberCountTestCase(TestCase):
         subs_string_4 = "122.4M"
         subs_string_5 = "1.2b"
         subs_string_6 = "98m"
+        subs_string_7 = "13.4k"
+        subs_string_8 = "13.4m"
+        subs_string_9 = "13.4b"
         self.assertEqual(convert_subscriber_count(subs_string_1), 13450)
         self.assertEqual(convert_subscriber_count(subs_string_2), 839)
         self.assertEqual(convert_subscriber_count(subs_string_3), 0)
         self.assertEqual(convert_subscriber_count(subs_string_4), 122400000)
         self.assertEqual(convert_subscriber_count(subs_string_5), 1200000000)
         self.assertEqual(convert_subscriber_count(subs_string_6), 98000000)
+        self.assertEqual(convert_subscriber_count(subs_string_7), 13400)
+        self.assertEqual(convert_subscriber_count(subs_string_8), 13400000)
+        self.assertEqual(convert_subscriber_count(subs_string_9), 13400000000)
