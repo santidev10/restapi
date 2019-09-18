48d4d56a70ea9f7205295aea87b353cbd0f8f527
Kharko, Yaryna <yaryna.kharko@sigma.software>
2019-09-18 15:37:32 +0300

    VIQ-2433 Update channel/video api constants

--- channel/constants.py
+++ channel/constants.py
@@ -9,7 +9,8 @@ RANGE_FILTER = ("social.instagram_followers", "social.twitter_followers", "socia
                 "stats.last_30day_subscribers", "stats.subscribers", "ads_stats.average_cpv", "ads_stats.ctr_v",
                 "ads_stats.video_view_rate", "analytics.age13_17", "analytics.age18_24",
                 "analytics.age25_34", "analytics.age35_44", "analytics.age45_54",
-                "analytics.age55_64", "analytics.age65_")
+                "analytics.age55_64", "analytics.age65_", "brand_safety.overall_score",
+                "general_data.youtube_published_at")
 
 EXISTS_FILTER = ("general_data.emails", "ads_stats", "analytics")
 
