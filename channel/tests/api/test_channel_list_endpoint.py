cc7bb6f360652942c2becc159bd828f7b62a9df7
George Su <george.su@channelfactory.com>
2019-09-20 10:28:10 -0700

    Working on unittest.

--- channel/tests/api/test_channel_list_endpoint.py
+++ channel/tests/api/test_channel_list_endpoint.py
@@ -104,18 +104,22 @@ class ChannelListTestCase(ExtendedAPITestCase, ESTestCase):
         })
         sleep(1)
         sections = [Sections.GENERAL_DATA, Sections.BRAND_SAFETY, Sections.CMS, Sections.AUTH]
-        ChannelManager(sections=sections).upsert([channel, channel_2, channel_3, channel_4, channel_5])
+        ChannelManager(sections=sections).upsert([channel])
+        ChannelManager(sections=sections).upsert([channel_2])
+        ChannelManager(sections=sections).upsert([channel_3])
+        ChannelManager(sections=sections).upsert([channel_4])
+        ChannelManager(sections=sections).upsert([channel_5])
         high_risk_url = self.url + "?brand_safety=High%20Risk"
         risky_url = self.url + "?brand_safety=Risky"
         low_risk_url = self.url + "?brand_safety=Low%20Risk"
         safe_url = self.url + "?brand_safety=Safe"
-        high_risk_and_safe_url = high_risk_url + "%20Safe"
+        high_risk_and_safe_url = high_risk_url + "%2CSafe"
         high_risk_response = self.client.get(high_risk_url)
         risky_response = self.client.get(risky_url)
         low_risk_response = self.client.get(low_risk_url)
         safe_response = self.client.get(safe_url)
         high_risk_and_safe_response = self.client.get(high_risk_and_safe_url)
-        self.assertEqual(high_risk_response.data, {})
+        raise ("high_risk_response.data: {}".format(high_risk_response.data["items"]))
         self.assertEqual(len(high_risk_response.data["items"]), 1)
         self.assertEqual(len(risky_response.data["items"]), 2)
         self.assertEqual(len(low_risk_response.data["items"]), 1)
