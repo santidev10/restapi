54ea9fc20bb208e92e6dc9f1106b82a26d2aa7c8
George Su <george.su@channelfactory.com>
2019-09-24 10:14:56 -0700

    Working on pulling transcripts.

--- audit_tool/management/commands/pull_transcripts_via_url.py
+++ audit_tool/management/commands/pull_transcripts_via_url.py
@@ -11,10 +11,51 @@ from elasticsearch_dsl import Q
 import time
 
 from es_components.connections import init_es_connection
-init_es_connection()
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        with PidFile(piddir='.', pidname='export_queue.pid') as p:
-            pass
+        init_es_connection()
+        with PidFile(piddir='.', pidname='pull_transcripts.pid') as p:
+            unparsed_vids = self.get_unparsed_vids()
+
+
+    def get_unparsed_vids(self):
+        s = Search(using='default')
+        # Get English Videos Query
+        q1 = Q(
+            {
+                "term": {
+                    "general_data.language": {
+                        "value": "English"
+                    }
+                }
+            }
+        )
+        # Get Videos with no captions
+        q2 = Q(
+            {
+                "bool": {
+                    "must_not": {
+                        "exists": {
+                            "field": "captions"
+                        }
+                    }
+                }
+            }
+        )
+        # Only get Videos we haven't tried parsing with the URL yet
+        q3 = Q(
+            {
+                "term": {
+                    "transcript_checked": False
+                }
+            }
+        )
+
+        s = s.query(q1).query(q2).query(q3)
+        s = s.sort({"stats.views": {"order": "desc"}})
+
+        for item in s.scan():
+            yield item
