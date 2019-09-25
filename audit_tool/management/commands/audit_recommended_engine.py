c80bdbf2463e98c467049bc7e03338fc97b412f9
Andrew <andrew@kabuto.com>
2019-09-25 13:12:54 -0700

    5 threads per audit for recommendation

--- audit_tool/management/commands/audit_recommended_engine.py
+++ audit_tool/management/commands/audit_recommended_engine.py
@@ -64,7 +64,7 @@ class Command(BaseCommand):
             self.thread_id = 0
         with PidFile(piddir='.', pidname='recommendation_{}.pid'.format(self.thread_id)) as p:
             try:
-                self.audit = AuditProcessor.objects.filter(completed__isnull=True, audit_type=0).order_by("pause", "id")[int(self.thread_id/4)]
+                self.audit = AuditProcessor.objects.filter(completed__isnull=True, audit_type=0).order_by("pause", "id")[int(self.thread_id/5)]
                 self.language = self.audit.params.get('language')
                 self.location = self.audit.params.get('location')
                 self.location_radius = self.audit.params.get('location_radius')
@@ -89,7 +89,7 @@ class Command(BaseCommand):
             self.audit.save(update_fields=['started'])
         pending_videos = AuditVideoProcessor.objects.filter(audit=self.audit)
         thread_id = self.thread_id
-        if thread_id % 4 == 0:
+        if thread_id % 5 == 0:
             thread_id = 0
         if pending_videos.count() == 0:
             if thread_id == 0:
