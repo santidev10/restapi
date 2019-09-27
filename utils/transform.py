1cef7ddf5173a59d86e4719ace4567b7e43070be
George Su <george.su@channelfactory.com>
2019-09-27 11:25:54 -0700

    Refactored populate_video_custom_transcripts to be cleaner and updated bs4 requirement name.

--- utils/transform.py
+++ utils/transform.py
@@ -1,14 +1,11 @@
-def populate_video_custom_transcripts(video, transcript_texts, transcript_languages):
-    if not transcript_texts or not transcript_languages:
-        video.populate_custom_transcripts(transcripts_checked=True)
-        return
+def populate_video_custom_transcripts(video, transcript_texts=[], transcript_languages=[]):
     if len(transcript_texts) != len(transcript_languages):
         raise Exception("Len of transcript_texts and transcript_languages must be equal.")
     transcripts = [
         dict(
-            text=transcript_texts[i],
-            language_code=transcript_languages[i]
+            text=text,
+            language_code=language_code
         )
-        for i in range(len(transcript_texts))
+        for text, language_code in zip(transcript_texts, transcript_languages)
     ]
     video.populate_custom_transcripts(transcripts_checked=True, transcripts=transcripts)
