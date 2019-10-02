b62fb002985b392254729e1bb80d9396a6bb0b0c
George Su <george.su@channelfactory.com>
2019-10-01 17:06:44 -0700

    Added custom_transcript to BrandSafetyVideoSerializer.

--- brand_safety/auditors/serializers.py
+++ brand_safety/auditors/serializers.py
@@ -34,6 +34,7 @@ class BrandSafetyVideoSerializer(Serializer):
     description = CharField(source="general_data.description", default="")
     tags = SerializerMethodField()
     transcript = SerializerMethodField()
+    custom_transcript = SerializerMethodField()
 
     def get_tags(self, obj):
         tags = ",".join(getattr(obj.general_data, "tags", []))
@@ -47,3 +48,12 @@ class BrandSafetyVideoSerializer(Serializer):
                     text = caption.text
                     transcript = re.sub(REGEX_TO_REMOVE_TIMEMARKS, "", text)
         return transcript
+
+    def get_custom_transcript(self, video):
+        custom_transcript = None
+        if video.custom_transcripts and video.custom_transcripts.transcripts:
+            for custom_transcript in video.custom_transcripts.transcripts:
+                if custom_transcript.language_code == "en":
+                    text = custom_transcript.text
+                    custom_transcript = re.sub(REGEX_TO_REMOVE_TIMEMARKS, "", text)
+        return custom_transcript
