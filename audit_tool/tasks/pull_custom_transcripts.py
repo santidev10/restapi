c83d459f08200d6219dc96883bb40bc7f18cf158
George Su <george.su@channelfactory.com>
2019-10-10 13:03:01 -0700

    Refactored audit_tool celery tasks to be separate celery tasks in a directory.

--- audit_tool/tasks/pull_custom_transcripts.py
+++ audit_tool/tasks/pull_custom_transcripts.py
@@ -17,15 +17,6 @@ from utils.lang import replace_apostrophes
 logger = logging.getLogger(__name__)
 
 
-@celery_app.task
-def segmented_audit_next_channels_batch():
-    from audit_tool.segmented_audit import SegmentedAudit
-    audit = SegmentedAudit()
-    logger.info("Started segmented audit for the next batch of channels")
-    channels_count, videos_count = audit.run()
-    logger.info("Done (channels_count={}, videos_count={})".format(channels_count, videos_count))
-
-
 @celery_app.task()
 def pull_custom_transcripts():
     init_es_connection()
