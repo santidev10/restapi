5d2ed4ef088e1c8ae43d7e950b71c64b1ce2ed2a
George Su <george.su@channelfactory.com>
2019-10-15 08:50:12 -0700

    Changed logger.info to logger.error statements.

--- audit_tool/tasks/pull_custom_transcripts.py
+++ audit_tool/tasks/pull_custom_transcripts.py
@@ -34,14 +34,15 @@ def pull_custom_transcripts():
             transcript_text = replace_apostrophes(transcript_soup.text) if transcript_soup else ""
             if transcript_text != "":
                 AuditVideoTranscript.get_or_create(video_id=vid_id, language="en", transcript=str(transcript_soup))
+                logger.error("VIDEO WITH ID {} HAS A CUSTOM TRANSCRIPT.".format(vid_id))
                 transcripts_counter += 1
             populate_video_custom_captions(vid_obj, [transcript_text], ['en'])
             video_manager.upsert([vid_obj])
             counter += 1
-            logger.info("Parsed video with id: {}".format(vid_id))
-            logger.info("Number of videos parsed: {}".format(counter))
-            logger.info("Number of transcripts retrieved: {}".format(transcripts_counter))
-    logger.info("Finished pulling 10,000 custom transcripts.")
+            logger.error("Parsed video with id: {}".format(vid_id))
+            logger.error("Number of videos parsed: {}".format(counter))
+            logger.error("Number of transcripts retrieved: {}".format(transcripts_counter))
+    logger.error("Finished pulling 10,000 custom transcripts.")
 
 
 def get_video_soup(vid_id):
