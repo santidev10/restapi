689dcbf837642f1b96d6ce5c4b1e50fc952e810c
George Su <george.su@channelfactory.com>
2019-10-23 17:30:11 -0700

    Refactored pull_custom_transcripts to pull transcripts from url asynchronously.

--- audit_tool/tasks/pull_custom_transcripts.py
+++ audit_tool/tasks/pull_custom_transcripts.py
@@ -3,6 +3,9 @@ from saas import celery_app
 import requests
 from elasticsearch_dsl import Search
 from elasticsearch_dsl import Q
+import asyncio
+import aiohttp
+from aiohttp import ClientSession
 
 from es_components.connections import init_es_connection
 from bs4 import BeautifulSoup as bs
@@ -38,9 +41,10 @@ def pull_custom_transcripts(lang_codes):
             transcripts_counter = 0
             video_manager = VideoManager(sections=(Sections.CUSTOM_CAPTIONS,),
                                          upsert_sections=(Sections.CUSTOM_CAPTIONS,))
+            all_video_soups_dict = create_video_soups_dict(vid_ids, lang_code)
             for vid_id in vid_ids:
                 vid_obj = video_manager.get_or_create([vid_id])[0]
-                transcript_soup = get_video_soup(vid_id, lang_code)
+                transcript_soup = all_video_soups_dict[vid_id]
                 transcript_text = replace_apostrophes(transcript_soup.text) if transcript_soup else ""
                 if transcript_text != "":
                     AuditVideoTranscript.get_or_create(video_id=vid_id, language=lang_code, transcript=str(transcript_soup))
@@ -58,15 +62,39 @@ def pull_custom_transcripts(lang_codes):
         pass
 
 
-def get_video_soup(vid_id, lang_code):
+async def create_video_soups_dict(vid_ids: list, lang_code: str):
+    soups_dict = {}
+    async with ClientSession() as session:
+        await asyncio.gather(*[update_soup_dict(vid_id, lang_code, session, soups_dict) for vid_id in vid_ids])
+    return soups_dict
+
+
+async def update_soup_dict(vid_id: str, lang_code: str, session: ClientSession, soups_dict):
     transcript_url = "http://video.google.com/timedtext?lang={}&v=".format(lang_code)
     vid_transcript_url = transcript_url + vid_id
-    transcript_response = requests.get(vid_transcript_url)
-    if transcript_response.status_code == 200:
-        soup = bs(transcript_response.text, "xml")
-        return soup
+    transcript_response = await session.request(method="GET", url=vid_transcript_url)
+    if transcript_response.status == 200:
+        soup = bs(await transcript_response.text(), "xml")
+        soups_dict[vid_id] = soup
     else:
-        return None
+        soups_dict[vid_id] = None
+
+
+# async def get_all_video_soups(vid_ids: list, lang_code: str):
+#     async with ClientSession() as session:
+#         all_soups = await asyncio.gather(*[get_video_soup(vid_id, lang_code, session) for vid_id in vid_ids])
+#     return all_soups
+#
+#
+# async def get_video_soup(vid_id: str, lang_code: str, session: ClientSession):
+#     transcript_url = "http://video.google.com/timedtext?lang={}&v=".format(lang_code)
+#     vid_transcript_url = transcript_url + vid_id
+#     transcript_response = await session.request(method="GET", url=vid_transcript_url)
+#     if transcript_response.status == 200:
+#         soup = bs(await transcript_response.text(), "xml")
+#         return soup
+#     else:
+#         return None
 
 
 def get_unparsed_vids(language):
