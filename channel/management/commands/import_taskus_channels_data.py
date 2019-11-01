b8600ca81a412e82de716afb319d0f63308e2e11
George Su <george.su@channelfactory.com>
2019-11-01 12:52:48 -0700

    Updated import_taskus_channels_data command to store iab_categories for channels in all their videos as well.

--- channel/management/commands/import_taskus_channels_data.py
+++ channel/management/commands/import_taskus_channels_data.py
@@ -8,6 +8,7 @@ from pid import PidFileError
 
 from es_components.connections import init_es_connection
 from es_components.managers.channel import ChannelManager
+from es_components.managers.video import VideoManager
 from es_components.constants import Sections
 from brand_safety.models import BadWordCategory
 from audit_tool.models import BlacklistItem
@@ -37,6 +38,8 @@ class Command(BaseCommand):
                 init_es_connection()
                 channel_manager = ChannelManager(sections=(Sections.TASK_US_DATA, Sections.GENERAL_DATA),
                                                  upsert_sections=(Sections.TASK_US_DATA, Sections.GENERAL_DATA))
+                video_manager = VideoManager(sections=(Sections.GENERAL_DATA),
+                                             upsert_sections=(Sections.GENERAL_DATA))
                 all_channel_ids = set()
                 channels_taskus_data_dict = {}
                 channels_iab_categories_dict = {}
@@ -47,46 +50,46 @@ class Command(BaseCommand):
                         channel_id = row[0].split('/')[-2]
                         all_channel_ids.add(channel_id)
                         current_channel_taskus_data = dict()
-                        iab_category_1 = row[4].strip()
+                        iab_category_1 = row[1].strip()
                         try:
-                            iab_category_2 = row[5].strip()
+                            iab_category_2 = row[2].strip()
                         except Exception:
                             iab_category_2 = None
                         current_channel_iab_categories = [iab_category_1]
                         if iab_category_2:
                             current_channel_iab_categories.append(iab_category_2)
                         try:
-                            moderation = row[6].lower().strip()
+                            moderation = row[3].lower().strip()
                             if moderation == "safe":
                                 current_channel_taskus_data['is_safe'] = True
                             elif moderation == "unsafe":
                                 current_channel_taskus_data['is_safe'] = False
                                 flag = BlacklistItem.get_or_create(channel_id, BlacklistItem.CHANNEL_ITEM)
-                                flag_category = BadWordCategory.from_string(row[7])
+                                flag_category = BadWordCategory.from_string(row[4])
                                 flag.blacklist_category = {flag_category.id: 100}
                                 flag.save()
                         except Exception:
                             pass
                         try:
-                            content_type = row[9].upper().strip()
+                            content_type = row[6].upper().strip()
                             is_user_generated_content = True if content_type == "UGC" else False
                             current_channel_taskus_data['is_user_generated_content'] = is_user_generated_content
                         except Exception:
                             pass
                         try:
-                            monetized = True if row[10].lower().strip() == "monetized" else None
+                            monetized = True if row[7].lower().strip() == "monetized" else None
                             if monetized:
                                 current_channel_taskus_data['monetized'] = monetized
                         except Exception:
                             pass
                         try:
-                            scalable = row[11].capitalize().strip()
+                            scalable = row[8].capitalize().strip()
                             if scalable:
                                 current_channel_taskus_data['scalable'] = True if scalable == "Scalable" else False
                         except Exception:
                             pass
                         try:
-                            language = row[12].capitalize().strip() if row[12] != "Unknown" else ""
+                            language = row[9].capitalize().strip() if row[9] != "Unknown" else ""
                             if language:
                                 current_channel_taskus_data['language'] = language
                         except Exception:
@@ -98,6 +101,13 @@ class Command(BaseCommand):
                 for channel in all_channels:
                     channel.populate_task_us_data(**channels_taskus_data_dict[channel.main.id])
                     channel.populate_general_data(iab_categories=channels_iab_categories_dict[channel.main.id])
+                    videos_filter = video_manager.by_channel_ids_query(channel.main.id)
+                    channel_videos = video_manager.search(filters=videos_filter).scan()
+                    upsert_videos = []
+                    for video in channel_videos:
+                        video.populate_general_data(iab_categories=channels_iab_categories_dict[channel.main.id])
+                        upsert_videos.append(video)
+                    video_manager.upsert(upsert_videos)
                 channel_manager.upsert(all_channels)
         except PidFileError:
             raise PidFileError
