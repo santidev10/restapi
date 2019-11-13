b40de6807668c88768492b846b9674dabb1e7c29
George Su <george.su@channelfactory.com>
2019-11-13 11:40:40 -0800

    Revert "Use default web app client function for Adwords API client"
    This reverts commit d5d1890a677ab5897924a7f481b11aef29d0d9cc.

--- aw_reporting/google_ads/google_ads_updater.py
+++ aw_reporting/google_ads/google_ads_updater.py
@@ -7,11 +7,12 @@ from django.db.models import F
 from django.utils import timezone
 from oauth2client.client import HttpAccessTokenRefreshError
 from google.auth.exceptions import RefreshError
+from googleads.errors import AdManagerReportError
 from suds import WebFault
 
-from aw_reporting.adwords_api import get_default_web_app_client
 from aw_reporting.adwords_api import get_web_app_client
 from aw_reporting.adwords_reports import AccountInactiveError
+from aw_reporting.google_ads.google_ads_api import get_client
 from aw_reporting.google_ads.updaters.accounts import AccountUpdater
 from aw_reporting.google_ads.updaters.ad_groups import AdGroupUpdater
 from aw_reporting.google_ads.updaters.ads import AdUpdater
@@ -79,12 +80,9 @@ class GoogleAdsUpdater(object):
         Update / Save all reporting data except Campaign data
         :return:
         """
-        client = get_default_web_app_client(
-            client_customer_id=self.account.id
-        )
         for update_class in self.main_updaters:
             updater = update_class(self.account)
-            self.execute(updater, client)
+            self.execute_with_any_permission(updater)
         recalculate_de_norm_fields_for_account(self.account.id)
         self.account.update_time = timezone.now()
         self.account.save()
@@ -95,11 +93,8 @@ class GoogleAdsUpdater(object):
             Run in separate process on a more frequent interval than other reporting data
         :return:
         """
-        client = get_default_web_app_client(
-            client_customer_id=self.account.id
-        )
         campaign_updater = CampaignUpdater(self.account)
-        self.execute(campaign_updater, client)
+        self.execute_with_any_permission(campaign_updater)
         self.account.hourly_updated_at = timezone.now()
         self.account.save()
         recalculate_de_norm_fields_for_account(self.account.id)
@@ -107,12 +102,9 @@ class GoogleAdsUpdater(object):
     def update_accounts_as_mcc(self, mcc_account=None):
         if mcc_account:
             self.account = mcc_account
-        client = get_default_web_app_client(
-            client_customer_id=self.account.id
-        )
         """ Update /Save accounts managed by MCC """
         account_updater = AccountUpdater(self.account)
-        self.execute(account_updater, client)
+        self.execute_with_any_permission(account_updater, mcc_account=self.account)
 
     def full_update(self, mcc_account=None):
         """
@@ -120,13 +112,13 @@ class GoogleAdsUpdater(object):
         :param mcc_account: Account
         :return:
         """
-        client = get_default_web_app_client(
-            client_customer_id=self.account.id
-        )
         self.main_updaters = (CampaignUpdater,) + self.main_updaters
         for update_class in self.main_updaters:
             updater = update_class(self.account)
-            self.execute(updater, client)
+            if mcc_account:
+                self.execute_with_any_permission(updater, mcc_account=mcc_account)
+            else:
+                self.execute_with_any_permission(updater)
         recalculate_de_norm_fields_for_account(self.account.id)
         self.account.update_time = timezone.now()
         self.account.save()
@@ -190,7 +182,7 @@ class GoogleAdsUpdater(object):
     @staticmethod
     def update_geo_targets():
         """ Update Google ads GeoTargets """
-        client = get_default_web_app_client()
+        client = get_client()
         geo_target_updater = GeoTargetUpdater()
         geo_target_updater.update(client)
 
@@ -264,9 +256,6 @@ class GoogleAdsUpdater(object):
             updater.update(client)
         except RefreshError:
             raise
-        except AccountInactiveError:
-            self.account.is_active = False
-            self.account.save()
         except Exception:
             try:
                 self._retry(updater, client)
