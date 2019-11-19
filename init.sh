62aa73ac75990f79bd91af83ccc340ae3bb46a21
George Su <george.su@channelfactory.com>
2019-11-19 14:23:04 -0800

    Modified init.sh to migrate cache model first.

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ then
     exit 0
 fi
 
+./manage.py migrate cache
 ./manage.py migrate
 ./manage.py migrate --database=audit
 ./manage.py shell -c "from django.contrib.auth import get_user_model; not get_user_model().objects.filter(email='admin@admin.admin').exists() and get_user_model().objects.create_superuser('admin', 'admin@admin.admin', 'admin')"
