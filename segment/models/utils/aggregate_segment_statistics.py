6cb0f41373f0fc41ee24b5f9c38f09c5add92d07
Kenneth <kenneth.oh@channelfactory.com>
2019-12-03 13:46:08 -0800

    Optimize statistics query using raw sql

--- segment/models/utils/aggregate_segment_statistics.py
+++ segment/models/utils/aggregate_segment_statistics.py
@@ -1,9 +1,9 @@
 from django.db import connection
-
 from aw_reporting.models.ad_words.calculations import CALCULATED_STATS
 
 MIN_IMPRESSIONS = 1000
-COLUMNS = ["cost", "video_views", "clicks", "impressions", "video_views"]
+MAX_STATS_TO_GET = 2500
+STATS_COLUMNS = ["cost", "video_views", "clicks", "impressions"]
 
 
 def aggregate_segment_statistics(related_aw_statistics_model, yt_ids):
@@ -11,18 +11,19 @@ def aggregate_segment_statistics(related_aw_statistics_model, yt_ids):
     Prepare adwords statistics for segment
     """
     table_name = str(related_aw_statistics_model._meta).replace(".", "_")
-    select_fields = ", ".join(f"{table_name}.{col}" for col in COLUMNS)
-    yt_ids = ",".join("'{}'".format(_id) for _id in yt_ids)
-    query = f"""SELECT {select_fields} 
-            AS ad_group_video_views
-            FROM {table_name} 
+    select_fields = ", ".join(f"{table_name}.{col}" for col in STATS_COLUMNS)
+    yt_ids = ",".join(f"('{_id}')" for _id in yt_ids)
+    query = f"""SELECT {select_fields}, aw_reporting_adgroup.video_views AS ad_group_video_views
+            FROM {table_name}
             INNER JOIN aw_reporting_adgroup
-            ON ({table_name}.ad_group_id = aw_reporting_adgroup.id) 
-            WHERE ({table_name}.impressions >= {MIN_IMPRESSIONS} AND {table_name}.yt_id IN ({yt_ids}))"""
+            ON ({table_name}.ad_group_id = aw_reporting_adgroup.id)
+            WHERE ({table_name}.impressions >= 1000
+            AND {table_name}.yt_id IN (VALUES {yt_ids}))
+            """
     with connection.cursor() as cursor:
         cursor.execute(query)
         columns = [col[0] for col in cursor.description]
-        results = [
+        queryset = [
             dict(zip(columns, row))
             for row in cursor.fetchall()
         ]
@@ -34,7 +35,7 @@ def aggregate_segment_statistics(related_aw_statistics_model, yt_ids):
             "video_clicks": 0,
             "video_impressions": 0,
         }
-        for statistic in results:
+        for statistic in queryset:
             if statistic["ad_group_video_views"] > 0:
                 aggregated["video_clicks"] += statistic["clicks"]
                 aggregated["video_impressions"] += statistic["impressions"]
