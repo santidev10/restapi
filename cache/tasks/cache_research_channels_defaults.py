2cfa463dc00d6e2277f7f4a4f0c25b1413dca30e
George Su <george.su@channelfactory.com>
2019-12-04 12:33:46 -0800

    Changed caching tasks to always update cache, even when it already exists. Added is_staff check to monetization section on channel_list. Updated cache_key for ESQuerySetAdapter to use manager's sections so that users with different permissions will have different cached filters. Previously, there was a bug where users with different permissions would have the same API responses cached.

--- cache/tasks/cache_research_channels_defaults.py
+++ cache/tasks/cache_research_channels_defaults.py
@@ -1,23 +1,46 @@
 import logging
 
+from django.conf import settings
+
+from utils.redis import get_redis_client
+
 from saas import celery_app
 from es_components.constants import Sections
 from es_components.managers.channel import ChannelManager
 
 from utils.es_components_api_utils import ESQuerysetAdapter
+from utils.es_components_cache import get_from_cache
+from utils.es_components_cache import set_to_cache
 
 from cache.models import CacheItem
 from cache.constants import CHANNEL_AGGREGATIONS_KEY
 
 logger = logging.getLogger(__name__)
 
+redis = get_redis_client()
+
+TIMEOUT = 21400
+
+
+def update_cache(obj, part, timeout=TIMEOUT):
+    if part == "count":
+        options = ((), {})
+        data = obj.uncached_count()
+    elif part == "get_data":
+        options = ((0, 50), {})
+        data = obj.uncached_get_data(0, 50)
+    else:
+        return
+    set_to_cache(obj, part, options, data, timeout)
+
 
 @celery_app.task()
 def cache_research_channels_defaults():
     logger.debug("Starting default research channels caching.")
-    sections = (Sections.MAIN, Sections.GENERAL_DATA, Sections.STATS, Sections.ADS_STATS,
+    default_sections = (Sections.MAIN, Sections.GENERAL_DATA, Sections.STATS, Sections.ADS_STATS,
                 Sections.CUSTOM_PROPERTIES, Sections.SOCIAL, Sections.BRAND_SAFETY, Sections.CMS,
-                Sections.TASK_US_DATA, Sections.MONETIZATION, Sections.ANALYTICS)
+                Sections.TASK_US_DATA)
+    admin_sections = default_sections + (Sections.MONETIZATION, Sections.ANALYTICS,)
 
     fields_to_load = ['main', 'social', 'custom_properties', 'ads_stats', 'analytics.updated_at', 'analytics.cms_title',
                       'analytics.traffic_source', 'analytics.age', 'analytics.country', 'analytics.gender',
@@ -41,10 +64,9 @@ def cache_research_channels_defaults():
     except Exception as e:
         cached_aggregations = None
 
-    manager = ChannelManager(sections)
-
+    # Caching for Default Sections (not Admin)
+    manager = ChannelManager(default_sections)
     queryset_adapter = ESQuerysetAdapter(manager, cached_aggregations=cached_aggregations)
-
     queryset_adapter.aggregations = []
     queryset_adapter.fields_to_load = fields_to_load
     queryset_adapter.filter_query = [manager.forced_filters()]
@@ -53,7 +75,33 @@ def cache_research_channels_defaults():
 
     logger.debug("Caching default research channels count and filters.")
     print("Caching default research channels count and filters.")
-    queryset_adapter.count()
-    queryset_adapter.get_data(0, 50)
+
+    obj = queryset_adapter
+    # Caching Count
+    part = "count"
+    update_cache(obj, part)
+    # Caching Get_data
+    part = "get_data"
+    update_cache(obj, part)
+
     logger.debug("Finished default research channels caching.")
     print("Finished default research channels caching.")
+
+    # Caching for Admin Sections
+    admin_manager = ChannelManager(admin_sections)
+    admin_queryset_adapter = queryset_adapter
+    admin_queryset_adapter.manager = admin_manager
+
+    logger.debug("Caching admin research channels count and filters.")
+    print("Caching admin research channels count and filters.")
+
+    obj = admin_queryset_adapter
+    # Caching Count
+    part = "count"
+    update_cache(obj, part)
+    # Caching Get_data
+    part = "get_data"
+    update_cache(obj, part)
+
+    logger.debug("Finished admin research channels caching.")
+    print("Finished admin research channels caching.")
--- cache/tasks/cache_research_videos_defaults.py
+++ cache/tasks/cache_research_videos_defaults.py
@@ -1,23 +1,45 @@
 import logging
 
+from django.conf import settings
+
+from utils.redis import get_redis_client
+
 from saas import celery_app
 from es_components.constants import Sections
 from es_components.managers.video import VideoManager
 
 from utils.es_components_api_utils import ESQuerysetAdapter
+from utils.es_components_cache import set_to_cache
 
 from cache.models import CacheItem
 from cache.constants import VIDEO_AGGREGATIONS_KEY
 
 logger = logging.getLogger(__name__)
 
+redis = get_redis_client()
+
+TIMEOUT = 21400
+
+
+def update_cache(obj, part, timeout=TIMEOUT):
+    if part == "count":
+        options = ((), {})
+        data = obj.uncached_count()
+    elif part == "get_data":
+        options = ((0, 50), {})
+        data = obj.uncached_get_data(0, 50)
+    else:
+        return
+    set_to_cache(obj, part, options, data, timeout)
+
 
 @celery_app.task()
 def cache_research_videos_defaults():
     logger.debug("Starting default research videos caching.")
-    sections = (Sections.MAIN, Sections.CHANNEL, Sections.GENERAL_DATA, Sections.BRAND_SAFETY,
-                Sections.STATS, Sections.ADS_STATS, Sections.MONETIZATION, Sections.CAPTIONS, Sections.CMS,
-                Sections.CUSTOM_CAPTIONS, Sections.ANALYTICS)
+    default_sections = (Sections.MAIN, Sections.CHANNEL, Sections.GENERAL_DATA, Sections.BRAND_SAFETY,
+                        Sections.STATS, Sections.ADS_STATS, Sections.MONETIZATION, Sections.CAPTIONS, Sections.CMS,
+                        Sections.CUSTOM_CAPTIONS)
+    admin_sections = default_sections + (Sections.ANALYTICS,)
 
     fields_to_load = ['general_data', 'main', 'monetization', 'channel', 'analytics', 'ads_stats', 'captions',
                       'cms.cms_title', 'stats.subscribers', 'stats.last_video_published_at', 'stats.engage_rate',
@@ -38,10 +60,9 @@ def cache_research_videos_defaults():
     except Exception as e:
         cached_aggregations = None
 
-    manager = VideoManager(sections)
-
+    # Caching for Default Sections (not Admin)
+    manager = VideoManager(default_sections)
     queryset_adapter = ESQuerysetAdapter(manager, cached_aggregations=cached_aggregations)
-
     queryset_adapter.aggregations = []
     queryset_adapter.fields_to_load = fields_to_load
     queryset_adapter.filter_query = [manager.forced_filters()]
@@ -50,7 +71,29 @@ def cache_research_videos_defaults():
 
     logger.debug("Caching default research videos count and filters.")
     print("Caching default research videos count and filters.")
-    queryset_adapter.count()
-    queryset_adapter.get_data(0, 50)
+    obj = queryset_adapter
+    # Caching Count
+    part = "count"
+    update_cache(obj, part)
+    # Caching Get_data
+    part = "get_data"
+    update_cache(obj, part)
     logger.debug("Finished default research videos caching.")
     print("Finished default research videos caching.")
+
+    # Caching for Admin Sections
+    admin_manager = VideoManager(admin_sections)
+    admin_queryset_adapter = queryset_adapter
+    admin_queryset_adapter.manager = admin_manager
+
+    logger.debug("Caching admin research videos count and filters.")
+    print("Caching admin research videos count and filters.")
+    obj = admin_queryset_adapter
+    # Caching Count
+    part = "count"
+    update_cache(obj, part)
+    # Caching Get_data
+    part = "get_data"
+    update_cache(obj, part)
+    logger.debug("Finished admin research videos caching.")
+    print("Finished admin research videos caching.")
