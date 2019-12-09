1633b4d4671a4dae6757a6f22d9961279d3551f8
George Su <george.su@channelfactory.com>
2019-12-09 10:42:52 -0800

    Updated research caches to run every 20 minutes.

--- saas/configs/celery.py
+++ saas/configs/celery.py
@@ -90,23 +90,23 @@ CELERY_BEAT_SCHEDULE = {
     },
     "cache-video-aggregations": {
         "task": "cache.tasks.cache_video_aggregations.cache_video_aggregations",
-        "schedule": crontab(minute="*/15"),
+        "schedule": crontab(minute="*/20"),
     },
     "cache-channel-aggregations": {
         "task": "cache.tasks.cache_channel_aggregations.cache_channel_aggregations",
-        "schedule": crontab(minute="*/15"),
+        "schedule": crontab(minute="*/20"),
     },
     "cache-keyword-aggregations": {
         "task": "cache.tasks.cache_keyword_aggregations.cache_keyword_aggregations",
-        "schedule": crontab(minute="*/15"),
+        "schedule": crontab(minute="*/20"),
     },
     "cache-research-videos-defaults": {
         "task": "cache.tasks.cache_research_videos_defaults.cache_research_videos_defaults",
-        "schedule": crontab(minute="*/15"),
+        "schedule": crontab(minute="*/20"),
     },
     "cache-research-channels-defaults": {
         "task": "cache.tasks.cache_research_channels_defaults.cache_research_channels_defaults",
-        "schedule": crontab(minute="*/15"),
+        "schedule": crontab(minute="*/20"),
     },
     "update_custom_segment": {
         "task": "segment.tasks.update_custom_segment.update_custom_segment",
