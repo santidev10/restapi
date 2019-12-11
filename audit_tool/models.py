9439d203b0be38a494df816e4339736f3af80893
Andrew <andrew@kabuto.com>
2019-12-11 11:38:26 -0800

    add currently exporting support to list endpoint

--- audit_tool/models.py
+++ audit_tool/models.py
@@ -143,8 +143,12 @@ class AuditProcessor(models.Model):
         self.save()
 
     @staticmethod
-    def get(running=None, audit_type=None, num_days=60, output=None, search=None):
-        all = AuditProcessor.objects.all()
+    def get(running=None, audit_type=None, num_days=60, output=None, search=None, export=None):
+        if export:
+            exports = AuditExporter.objects.filter(completed__isnull=True).values_list('audit_id', flat=True)
+            all = AuditProcessor.objects.filter(id__in=exports)
+        else:
+            all = AuditProcessor.objects.all()
         if audit_type:
             all = all.filter(audit_type=audit_type)
         if running is not None:
