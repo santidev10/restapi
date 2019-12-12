cabaa7a8e1df6515736a80edf42be7fa458531e1
George Su <george.su@channelfactory.com>
2019-12-11 16:27:32 -0800

    Added files for replacing BSTE keywords.

---
+++ import_new_bad_words.py
@@ -0,0 +1,46 @@
+import csv
+from brand_safety.models import BadWord
+from brand_safety.models import BadWordCategory
+from audit_tool.models import AuditLanguage
+from brand_safety.languages import LANG_CODES
+
+file_name = "bste_keywords_final.csv"
+invalid_rows_file_name = "invalid_keywords.csv"
+
+invalid_rows = []
+
+
+BadWord.objects.all().delete()
+counter = 0
+with open(file_name, "r") as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        counter += 1
+        print(f"Parsing Row: {counter}")
+        try:
+            # Parse word
+            word = row[0]
+            # Parse category
+            category_string = row[1].lower().strip()
+            category = BadWordCategory.objects.from_string(category_string)
+            # Parse language
+            language_string = row[2].title()
+            lang_code = LANG_CODES[language_string.title()]
+            language = AuditLanguage.from_string(lang_code)
+            # Parse rating
+            negative_score = int(row[3])
+            if not word or not category or not language or not negative_score:
+                reason = ["One of word, category, language, or negative_score not found."]
+                invalid_rows.append(row + reason)
+                continue
+            BadWord.objects.get_or_create(name=word, category=category, language=language, negative_score=negative_score)
+        except Exception as e:
+            reason = [e]
+            invalid_rows.append(row + reason)
+            continue
+
+with open(invalid_rows_file_name, "a") as f2:
+    writer = csv.writer(f2)
+    for row in invalid_rows:
+        writer.writerow(row)
