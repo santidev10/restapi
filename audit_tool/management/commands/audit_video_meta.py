d21d34f8a89eac8fbd70d834bdc46bd17462030b
George Su <george.su@channelfactory.com>
2019-12-13 13:15:14 -0800

    Refactored load_exclusion_list in audit_video_meta to load keywords with languages as a dictionary.

--- audit_tool/management/commands/audit_video_meta.py
+++ audit_tool/management/commands/audit_video_meta.py
@@ -22,6 +22,7 @@ from pid import PidFile
 from audit_tool.api.views.audit_save import AuditFileS3Exporter
 from django.conf import settings
 from utils.lang import remove_mentions_hashes_urls
+from collections import defaultdict
 
 """
 requirements:
@@ -371,13 +372,21 @@ class Command(BaseCommand):
     def load_exclusion_list(self):
         if self.exclusion_list:
             return
-        input_list = self.audit.params.get("exclusion") if self.audit.params else None
+        input_list = self.audit.params.get("exclusion")
         if not input_list:
             return
-        regexp = "({})".format(
-                "|".join([r"\b{}\b".format(re.escape(w)) for w in input_list])
-        )
-        self.exclusion_list = re.compile(regexp)
+        language_keywords_dict = defaultdict(list)
+        exclusion_list = {}
+        for row in input_list:
+            word = row[0]
+            language = row[2]
+            language_keywords_dict[language].append(word)
+        for lang, keywords in language_keywords_dict.items():
+            lang_regexp = "({})".format(
+                "|".join([r"\b{}\b".format(re.escape(w)) for w in keywords])
+            )
+            exclusion_list[lang] = re.compile(lang_regexp)
+        self.exclusion_list = exclusion_list
 
     def check_exists(self, text, exp, count=1):
         keywords = re.findall(exp, text.lower())
