e14ee205d715d7bfda4df1c7c47f64dc58147e01
George Su <george.su@channelfactory.com>
2019-12-16 12:09:35 -0800

    Wrote script for importing porn names into BSTE.

---
+++ import_porn_names.py
@@ -0,0 +1,28 @@
+import csv
+from brand_safety.models import BadWord
+from brand_safety.models import BadWordCategory
+from audit_tool.models import AuditLanguage
+from brand_safety.languages import LANG_CODES
+
+file_name = "Adult Film Database Names.csv"
+
+counter = 0
+with open(file_name, "r") as f:
+    reader = csv.reader(f)
+    next(reader)
+    category = BadWordCategory.objects.get(name="pornographic")
+    language = AuditLanguage.from_string("un")
+    negative_score = 4
+    for row in reader:
+        word = row[0]
+        name = word.split(" ")
+        if len(name < 2):
+            continue
+        try:
+            bad_word = BadWord.all_objects.get(name=word, language=language)
+            bad_word.deleted_at = None
+            bad_word.category = category
+            bad_word.negative_score = negative_score
+            bad_word.save()
+        except Exception as e:
+            BadWord.objects.create(name=word, category=category, language=language, negative_score=negative_score)
