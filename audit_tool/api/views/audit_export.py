2d919d724b1b7a2a5f188112bd5ca9b4e62186a8
Andrew <andrew@kabuto.com>
2019-12-17 12:42:59 -0800

    bypass exceptions when calculating video scores so exports dont fail

--- audit_tool/api/views/audit_export.py
+++ audit_tool/api/views/audit_export.py
@@ -266,13 +266,17 @@ class AuditExportApiView(APIView):
             else:
                 all_bad_hit_words = ""
                 unique_bad_hit_words = ""
-            video_audit_score = auditor.audit_video({
-                "id": v.video.video_id,
-                "title": v.name,
-                "description": v.description,
-                "tags": v.keywords,
-            }, full_audit=False)
-            mapped_score = map_brand_safety_score(video_audit_score)
+            try:
+                video_audit_score = auditor.audit_video({
+                    "id": v.video.video_id,
+                    "title": v.name,
+                    "description": v.description,
+                    "tags": v.keywords,
+                }, full_audit=False)
+                mapped_score = map_brand_safety_score(video_audit_score)
+            except Exception as e:
+                mapped_score = ""
+                print("Problem calculating video score")
             data = [
                 "https://www.youtube.com/video/" + v.video.video_id,
                 v.name,
