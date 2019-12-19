3fe7e5f2283b96c7f4032ef808620d590124c33a
George Su <george.su@channelfactory.com>
2019-12-18 16:28:30 -0800

    Added scripts for importing new tags.

---
+++ bste_tags_to_import/import_international_terrorism.py
@@ -0,0 +1,28 @@
+import csv
+import string
+from brand_safety.models import BadWord
+from brand_safety.models import BadWordCategory
+from audit_tool.models import AuditLanguage
+from brand_safety.languages import LANG_CODES
+
+file_name = "bste_tags_to_import/international_terrorism.csv"
+
+counter = 0
+with open(file_name, "r") as f:
+    reader = csv.reader(f)
+    category = BadWordCategory.objects.get(name="terrorism")
+    language = AuditLanguage.from_string("un")
+    negative_score = 4
+    counter = 0
+    for row in reader:
+        counter += 1
+        print(f"Parsing row {counter}.")
+        word = row[0].lower().strip().translate(str.maketrans('', '', string.punctuation))
+        try:
+            bad_word = BadWord.all_objects.get(name=word, language=language)
+            bad_word.deleted_at = None
+            bad_word.category = category
+            bad_word.negative_score = negative_score
+            bad_word.save()
+        except Exception as e:
+            BadWord.objects.create(name=word, category=category, language=language, negative_score=negative_score)
