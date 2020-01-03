db98707f9d5f314b4a814b41680f11a91bf9b3a4
Kenneth <kenneth.oh@channelfactory.com>
2020-01-03 14:33:18 -0800

    Add new segment create v3 view
    New requirements vastly change how segment creation works, especially with the view now accepting creation of both video and channel lists.

--- segment/api/views/custom_segment/segment_create_v3.py
+++ segment/api/views/custom_segment/segment_create_v3.py
@@ -1,6 +1,5 @@
-from datetime import timedelta
+from datetime import datetime
 
-from django.utils import timezone
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
@@ -8,7 +7,6 @@ from rest_framework.status import HTTP_201_CREATED
 
 from audit_tool.models import get_hash_name
 from brand_safety.utils import BrandSafetyQueryBuilder
-from saas.configs.celery import Queue
 from segment.api.serializers.custom_segment_serializer import CustomSegmentSerializer
 from segment.models.custom_segment import CustomSegment
 from segment.models.custom_segment_file_upload import CustomSegmentFileUpload
@@ -17,67 +15,87 @@ from segment.utils.utils import validate_threshold
 
 
 class SegmentCreateApiViewV3(CreateAPIView):
-    REQUIRED_FIELDS = ["brand_safety_categories", "languages", "list_type", "minimum_option", "score_threshold", "title", "youtube_categories"]
+    response_fields = ("id", "title", "minimum_views", "minimum_subscribers", "segment_type", "list_type",
+                       "content_categories", "languages", "countries", "score_threshold", "sentiment")
     serializer_class = CustomSegmentSerializer
 
     def post(self, request, *args, **kwargs):
         data = request.data
-        validated_data = self._validate_data(data, request, kwargs)
-        data.update(validated_data)
-        creation_type = data["segment_type"]
-        to_create = []
+        try:
+            validated_data = self._validate_data(request.user.id, data)
+            data.update(validated_data)
+        except SegmentCreationError as error:
+            raise ValidationError(f"Exception trying to create segments: {error}")
+        segment_type = data["segment_type"]
+        created = []
+        response = []
         err = None
         try:
-            if creation_type == 2:
+            if segment_type == 2:
                 # Creation type will be 0-2, inclusive. Serializer expects segment_type of 0 or 1
-                for i in range(creation_type):
-                    data["segment_type"] = i
-                    segment = self._create(data)
-                    to_create.append(segment)
+                for i in range(segment_type):
+                    options = data.copy()
+                    options["segment_type"] = i
+                    segment = self._create(options)
+                    created.append((options, segment))
             else:
                 segment = self._create(data)
-                to_create.append(segment)
-
+                created.append((data, segment))
         except Exception as error:
-            CustomSegment.objects.filter(id__in=[item.id for item in to_create]).delete()
+            CustomSegment.objects.filter(id__in=[item[1].id for item in created]).delete()
             err = error
-
         if err:
             raise ValidationError(f"Exception trying to create segments: {err}")
-
-        for segment in to_create:
-            data["segment_type"] = segment.segment_type
-            query_builder = BrandSafetyQueryBuilder(data)
+        for options, segment in created:
+            query_builder = BrandSafetyQueryBuilder(options)
             query = {
                 "params": query_builder.query_params,
-                "body": query_builder.query_body
+                "body": query_builder.query_body.to_dict()
             }
             CustomSegmentFileUpload.enqueue(query=query, segment=segment)
-            generate_custom_segment.apply_async(args=[segment.id], queue=Queue.SEGMENTS)
-        return Response(status=HTTP_201_CREATED)
+            generate_custom_segment.delay(segment.id)
+            options["id"] = segment.id
+            res = {
+                key: options[key] for key in self.response_fields
+            }
+            res["statistics"] = {}
+            response.append(res)
+        return Response(status=HTTP_201_CREATED, data=response)
 
-    def _validate_data(self, data, request, kwargs):
+    def _validate_data(self, user_id: int, data: dict):
         validated = {}
-        self._validate_fields(data)
-        validated["segment_type"] = self.validate_numeric(data["segment_type"])
-        if not 0 <= validated["segment_type"] <= 2:
-            raise ValidationError(f"Invalid list_type: {data['segment_type']}. Must 0-2, inclusive")
-        validate_threshold(data["score_threshold"])
-        validated["minimum_option"] = self.validate_numeric(data["minimum_option"])
-        validated["minimum_views"] = data.get("minimum_views", 1000)
-        validated["minimum_subscribers"] = data.get("minimum_subscribers", 1000)
-        validated["last_upload_date"] = data.get("last_upload_date", (timezone.now() - timedelta(days=365)).date())
-        validated["score_threshold"] = self.validate_numeric(data["score_threshold"])
-        validated["segment_type"] = kwargs["segment_type"]
-        validated["owner"] = request.user.id
-        validated["title_hash"] = get_hash_name(data["title"].lower().strip())
-        validated["youtube_categories"] = BrandSafetyQueryBuilder.map_youtube_categories(data["youtube_categories"]),
-        validated["sentiment"] = data.get("sentiment", 50)
+        try:
+            if not 0 <= data["segment_type"] <= 2:
+                raise ValueError(f"Invalid list_type: {data['segment_type']}. Must 0-2, inclusive")
+            validate_threshold(data["score_threshold"])
+            validated["minimum_views"] = self.validate_numeric(data.get("minimum_views", 0))
+            validated["minimum_subscribers"] = self.validate_numeric(data.get("minimum_subscribers", 0))
+            validated["last_upload_date"] = self.validate_date(data.get("last_upload_date"))
+            validated["score_threshold"] = int(data.get("score_threshold", 0))
+            validated["sentiment"] = int(data.get("sentiment", 0))
+            validated["owner"] = user_id
+            validated["title_hash"] = get_hash_name(data["title"].lower().strip())
+            validated["content_categories"] = BrandSafetyQueryBuilder.map_content_categories(data["content_categories"])
+            validated["countries"] = data["countries"]
+        except (ValueError, TypeError, AttributeError, KeyError) as error:
+            raise SegmentCreationError(f"{type(error).__name__}: {error}")
         return validated
 
-    def _validate_fields(self, fields):
-        if set(self.REQUIRED_FIELDS) != set(fields):
-            raise ValidationError("Fields must consist of: {}".format(", ".join(self.REQUIRED_FIELDS)))
+    def _create(self, data: dict):
+        data["list_type"] = "whitelist"
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid()
+        segment = serializer.save()
+        return segment
+
+    @staticmethod
+    def validate_date(date_str: str):
+        if date_str:
+            try:
+                datetime.strptime(date_str, "%Y-%m-%d")
+            except ValueError:
+                raise SegmentCreationError(f"Invalid date: {date_str}. Date format must be YYYY-MM-DD")
+        return date_str
 
     @staticmethod
     def validate_numeric(value):
@@ -88,8 +106,6 @@ class SegmentCreateApiViewV3(CreateAPIView):
             raise ValidationError("The number: {} is not valid.".format(value))
         return to_num
 
-    def _create(self, data):
-        serializer = self.serializer_class(data=data)
-        serializer.is_valid()
-        segment = serializer.save()
-        return segment
+
+class SegmentCreationError(Exception):
+    pass
