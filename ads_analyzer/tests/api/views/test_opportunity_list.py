f86b91f99a1348fa3cbabbd2f2126a5ec6d438cd
Andrew <andrew@kabuto.com>
2020-01-10 12:34:11 -0800

    make test case year dynamic for future proofing

--- ads_analyzer/tests/api/views/test_opportunity_list.py
+++ ads_analyzer/tests/api/views/test_opportunity_list.py
@@ -94,7 +94,7 @@ class OpportunityTargetingReportAPIViewTestCase(OpportunityTargetingReportBaseAP
         self.assertEqual([dict(
             id=opportunity.id,
             name=opportunity.name,
-            start="'2020-12-01'",
+            start="{}-12-01".format(datetime.now().year),
         )], response.json())
 
     def test_no_active_opportunity(self):
