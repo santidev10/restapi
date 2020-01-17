840e8c9eefce44c532594b14a1fa740717720c4c
Kenneth <kenneth.oh@channelfactory.com>
2020-01-17 15:41:52 -0800

    Filter for opportunities with start dates within the past year

--- ads_analyzer/api/views/opportunity_list.py
+++ ads_analyzer/api/views/opportunity_list.py
@@ -1,9 +1,12 @@
+from datetime import timedelta
+
+from django.utils import timezone
 from rest_framework.generics import ListAPIView
+
 from ads_analyzer.api.serializers.opportunity_serializer import OpportunitySerializer
 from aw_reporting.models import Opportunity
 from utils.permissions import or_permission_classes
 from utils.permissions import user_has_permission
-from django.utils import timezone
 
 
 class OpportunityListAPIView(ListAPIView):
@@ -12,5 +15,5 @@ class OpportunityListAPIView(ListAPIView):
             user_has_permission("userprofile.view_opportunity_list"),
         ),
     )
-    queryset = Opportunity.objects.filter(start__lte=timezone.now()).values("id", "name", "start")
+    queryset = Opportunity.objects.filter(start__gte=timezone.now()-timedelta(days=365), start__lte=timezone.now()).values("id", "name", "start")
     serializer_class = OpportunitySerializer
