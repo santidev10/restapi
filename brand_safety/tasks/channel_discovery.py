bd25d5f62fcd3e09ca23e45bfd322b0d7330381a
Kenneth <kenneth.oh@channelfactory.com>
2020-01-27 12:21:58 -0800

    Change query for channel discovery task to ignore updated_at timestamp

--- brand_safety/tasks/channel_discovery.py
+++ brand_safety/tasks/channel_discovery.py
@@ -13,6 +13,9 @@ from utils.celery.tasks import celery_lock
 @celery_lock(Schedulers.ChannelDiscovery.NAME, expire=TaskExpiration.BRAND_SAFETY_CHANNEL_DISCOVERY, max_retries=0)
 def channel_discovery_scheduler():
     channel_manager = ChannelManager()
-    query = channel_manager.forced_filters() \
-            & QueryBuilder().build().must_not().exists().field(Sections.BRAND_SAFETY).get()
+    query = channel_manager.forced_filters()
+    # Remove must statements for last update_time
+    # Discovery should also retrieve channels with their brand_safety section removed by video_discovery task
+    query.must = QueryBuilder().build().must().exists().field(Sections.GENERAL_DATA).get() & \
+                 QueryBuilder().build().must_not().exists().field(Sections.BRAND_SAFETY).get()
     channel_update_helper(Schedulers.ChannelDiscovery, query, Queue.BRAND_SAFETY_CHANNEL_PRIORITY)
