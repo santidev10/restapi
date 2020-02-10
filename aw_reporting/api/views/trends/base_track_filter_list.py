2bc34d50be26f954c46081b6258162c24ef2534c
Kharko, Yaryna <yaryna.kharko@sigma.software>
2020-02-10 12:41:24 +0200

    VIQ-2910 Optimize CHF trends, added filters caching

--- aw_reporting/api/views/trends/base_track_filter_list.py
+++ aw_reporting/api/views/trends/base_track_filter_list.py
@@ -1,81 +1,15 @@
-from itertools import groupby
-
 from rest_framework.response import Response
 
 from aw_reporting.api.views.trends.base_track import TrackApiBase
-from aw_reporting.models import Campaign
+from aw_reporting.tools.trends_tool.base_filters import BaseTrackFiltersList
 
 
 class BaseTrackFiltersListApiView(TrackApiBase):
     """
     Lists of the filter names and values
     """
-
-    def _get_static_filters(self):
-        static_filters = dict(
-            indicator=[
-                dict(id=uid, name=name)
-                for uid, name in self.indicators
-            ],
-            breakdown=[
-                dict(id=uid, name=name)
-                for uid, name in self.breakdowns
-            ],
-            dimension=[
-                dict(id=uid, name=name)
-                for uid, name in self.dimensions
-            ],
-        )
-        return static_filters
-
-    def _get_accounts(self, request):
-        raise NotImplemented
-
-    def _get_filters(self, request, accounts=None):
-        if accounts is None:
-            accounts = self._get_accounts(request)
-
-        campaigns_map = self._get_campaigns_map()
-
-        return dict(
-            accounts=[
-                self._map_account(account, campaigns_map.get(account.id, []))
-                for account in accounts
-            ],
-            **self._get_static_filters()
-        )
-
-    def _map_account(self, account, campaigns):
-        starts = [c["start_date"] for c in campaigns]
-        ends = [c["end_date"] for c in campaigns]
-        return dict(
-            id=account.id,
-            name=account.name,
-            start_date=min(filter(lambda x: x is not None, starts)) if any(starts) else None,
-            end_date=max(filter(lambda x: x is not None, ends)) if any(ends) else None,
-            campaigns=[
-                self._map_campaigns(campaign)
-                for campaign in campaigns
-            ]
-        )
-
-    def _get_campaigns_map(self):
-        campaigns = Campaign.objects.all() \
-            .values("id", "name", "start_date", "end_date", "account_id") \
-            .order_by("account_id", "id")
-        return {
-            key: list(group)
-            for key, group in groupby(campaigns, lambda i: i["account_id"])
-        }
-
-    def _map_campaigns(self, campaign):
-        return dict(
-            id=campaign["id"],
-            name=campaign["name"],
-            start_date=campaign["start_date"],
-            end_date=campaign["end_date"],
-        )
+    filter_class = BaseTrackFiltersList
 
     def get(self, request, *args, **kwargs):
-        filters = self._get_filters(request)
+        filters = self.filter_class().get_filters(request.user)
         return Response(data=filters)
---
+++ cache/tasks/cache_global_trends_filters.py
@@ -0,0 +1,34 @@
+import logging
+
+from django.contrib.auth import get_user_model
+
+from aw_reporting.api.views.trends.base_global_trends import get_account_queryset
+from aw_reporting.tools.trends_tool.global_filters import GlobalTrendsFilters
+from saas import celery_app
+from cache.models import CacheItem
+from cache.constants import GLOBAL_TRENDS_FILTERS_KEY
+from saas.configs.celery import TaskExpiration
+from saas.configs.celery import TaskTimeout
+
+logger = logging.getLogger(__name__)
+
+
+@celery_app.task(expires=TaskExpiration.GLOBAL_TRENDS_FILTERS_CACHING,
+                 soft_time_limit=TaskTimeout.GLOBAL_TRENDS_FILTERS_CACHING)
+def cache_global_trends_filters():
+    logger.debug("Starting global trends filters caching.")
+
+    for user in get_user_model().objects.filter(is_active=True).all():
+        accounts = get_account_queryset(user)
+
+        if not accounts.exists():
+            continue
+
+        global_trends_filters = GlobalTrendsFilters().get_filters(user=user)
+
+        cached_pricing_tool_filters, _ = CacheItem.objects.get_or_create(key=f"{user.id}_{GLOBAL_TRENDS_FILTERS_KEY}")
+
+        logger.debug("Saving keyword aggregations for user with id %s.", user.id)
+        cached_pricing_tool_filters.value = global_trends_filters
+        cached_pricing_tool_filters.save()
+    logger.debug("Finished keyword aggregations caching.")
