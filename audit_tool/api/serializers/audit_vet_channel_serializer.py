7e2cce42e7026d9614d53b8e1bae5c9a42264b6d
Kenneth <kenneth.oh@channelfactory.com>
2020-02-09 23:44:46 -0800

    Add additional validation to channel vet serializer, handle skipping vetting items

--- audit_tool/api/serializers/audit_vet_channel_serializer.py
+++ audit_tool/api/serializers/audit_vet_channel_serializer.py
@@ -1,26 +1,68 @@
+from rest_framework.exceptions import ValidationError
+from rest_framework.serializers import BooleanField
+from rest_framework.serializers import CharField
+from rest_framework.serializers import DateTimeField
+from rest_framework.serializers import ListField
 from rest_framework.serializers import Serializer
 from rest_framework.serializers import SerializerMethodField
-from rest_framework.serializers import ListField
-
-from rest_framework.serializers import CharField
-from rest_framework.serializers import BooleanField
-from rest_framework.serializers import IntegerField
-
-
 
 from audit_tool.models import AuditChannelVet
+from audit_tool.models import BlacklistItem
 from audit_tool.models import get_hash_name
 from audit_tool.validators import AuditToolValidator
+from aw_reporting.models import Gender
+from aw_reporting.models import AgeRange
+from brand_safety.models import BadWordCategory
+from es_components.models import Channel
+
 
 class AuditChannelVetSerializer(Serializer):
-    language = CharField(source="task_us_data.language", default=None)
-    iab_categories = ListField(source="task_us_data.iab_categories", default=None)
-    gender = CharField(source="task_us_data.gender", default=None)
-    age_group = CharField(source="task_us_data.age_group", default=None)
-    is_monetizable = BooleanField(source="monetization.is_monetizable", default=None)
-    country = CharField(source="task_us_data.country", default=None)
-    channel_type = CharField(source="task_us_data.channel_type", default=None)
+    age_group = CharField(source="task_us_data.age_group")
+    brand_safety = ListField(source="task_us_data.brand_safety")
+    channel_type = CharField(source="task_us_data.channel_type")
+    country = CharField(source="task_us_data.country")
+    gender = CharField(source="task_us_data.gender")
+    iab_categories = ListField(source="task_us_data.iab_categories")
+    is_monetizable = BooleanField(source="monetization.is_monetizable")
+    language = CharField(source="task_us_data.language")
+
     vetting_history = SerializerMethodField()
+    audit_instructions = SerializerMethodField()
+    segment_title = SerializerMethodField()
+
+    url = SerializerMethodField()
+    is_checked_out = BooleanField(required=False)
+    suitable = BooleanField(required=False)
+    processed = DateTimeField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        try:
+            self.channel_id = kwargs.pop("channel_id")
+            self.channel_audit = kwargs.pop("audit_item")
+            self.segment = kwargs.pop("segment")
+        except KeyError:
+            raise ValueError("kwargs for channel_id, channel_audit, and segment must be provided.")
+        super().__init__(*args, **kwargs)
+
+    def get_url(self, doc):
+        url = f"https://www.youtube.com/channel/{doc.main.id}/"
+        return url
+
+    def get_segment_title(self, *_, **__):
+        """
+        Get segment title if available
+        :return: None | str
+        """
+        title = self.segment.title
+        return title
+
+    def get_audit_instructions(self, *_, **__):
+        """
+        Get segment title if available
+        :return: None | str
+        """
+        instructions = self.segment.audit_instructions
+        return instructions
 
     def get_vetting_history(self, doc):
         """
@@ -46,34 +88,115 @@ class AuditChannelVetSerializer(Serializer):
         """
         language = AuditToolValidator.validate_language(value)
         return language
-    #
-    # def validate_category(self, value):
-    #     """
-    #     Retrieve audit_tool AuditCategory. Raises ValidationError if not found
-    #     :param value: str
-    #     :return: AuditCategory
-    #     """
-    #     category = AuditToolValidator.validate_category(value)
-    #     return category
-    #
-    # def validate_country(self, value):
-    #     """
-    #     Retrieve audit_tool AuditCountry. Raises ValidationError if not found
-    #     :param value: str
-    #     :return: AuditCountry
-    #     """
-    #     country = AuditToolValidator.validate_country(value)
-    #     return country
 
-    # def save(self, **kwargs):
-    #     """
-    #     Save values on self and related model
-    #     :param kwargs: dict
-    #     :return:
-    #     """
-    #     obj = self.obj
-    #     self.channel_metadata.age_group = self.age_group
-    #     self.channel_metadata.language = self.language
-    #     self.channel_metadata.category = self.category
-    #     self.channel_metadata.country = self.country
-    #     super().save(**kwargs)
+    def validate_iab_categories(self, value):
+        """
+        Retrieve AuditCategory iab_category values. Raises ValidationError if not found
+        :param value: str
+        :return: AuditCategory
+        """
+        iab_categories = AuditToolValidator.validate_iab_categories(value)
+        return iab_categories
+
+    def validate_gender(self, value):
+        """
+        Retrieve age_group enum value. Raises ValidationError if not found
+        :param value: str
+        :return: str
+        """
+        try:
+            value = value.upper()
+            getattr(Gender, value)
+        except AttributeError:
+            raise ValueError(f"Invalid gender: {value}")
+        return value
+
+    def validate_age_group(self, value):
+        """
+        Retrieve age_group enum value. Raises ValidationError if not found
+        :param value: str
+        :return: AuditCategory
+        """
+        try:
+            value = value.upper()
+            getattr(AgeRange, value)
+        except AttributeError:
+            raise ValueError(f"Invalid age group: {value}")
+        return value
+
+    def validate_channel_type(self, value):
+        """
+        Retrieve channel_type enum value. Raises ValidationError if not found
+        :param value: str
+        :return: str
+        """
+        channel_type = AuditToolValidator.validate_channel_type(value)
+        return channel_type
+
+    def validate_category(self, value):
+        """
+        Retrieve audit_tool AuditCategory. Raises ValidationError if not found
+        :param value: str
+        :return: AuditCategory
+        """
+        category = AuditToolValidator.validate_category(value)
+        return category
+
+    def validate_country(self, value):
+        """
+        Retrieve audit_tool AuditCountry. Raises ValidationError if not found
+        :param value: str
+        :return: AuditCountry
+        """
+        country = AuditToolValidator.validate_country(value)
+        return country
+
+    def validate_brand_safety(self, values):
+        try:
+            [BadWordCategory.objects.get(name=value) for value in values]
+        except BadWordCategory.DoesNotExist as e:
+            raise ValidationError(f"Category not found: {e}")
+        return values
+
+    def save(self, **kwargs):
+        """
+        Save values on self and related model
+        :param kwargs: dict
+        :return:
+        """
+        # update channel meta monetization
+        is_monetizable = self.validated_data["monetization"]["is_monetizable"]
+        task_us_data = self.validated_data["task_us_data"]
+
+        channel_meta = self.channel_audit.channel.auditchannelmeta
+        channel_meta.monetised = is_monetizable
+        channel_meta.save()
+
+        blacklist_item, created = BlacklistItem.objects.get_or_create(
+            item_id=self.channel_id,
+            item_type=1,
+            defaults={
+                "item_id_hash": get_hash_name(self.channel_id),
+                "blacklist_category": self.validated_data["brand_safety"],
+        })
+        # Update existing categories
+        if created is False:
+            all_categories = blacklist_item.blacklist_category + self.validated_data["brand_safety"]
+            blacklist_item.blacklist_category = all_categories
+            blacklist_item.save()
+
+        # Update Elasticsearch document
+        doc = Channel(**{
+            "monetization": {
+                "is_monetizable": is_monetizable,
+            },
+            "task_us_data": {
+                "iab_categories": task_us_data["iab_categories"],
+                "language": task_us_data["language"].language,
+                "gender": task_us_data["gender"],
+                "age_group": task_us_data["age_group"],
+                "country": task_us_data["country"].country,
+                "channel_type": task_us_data["channel_type"].channel_type,
+            }
+        })
+        self.es_manager.upsert([doc])
--- audit_tool/api/views/audit_vet_retrieve_update_api_view.py
+++ audit_tool/api/views/audit_vet_retrieve_update_api_view.py
@@ -35,12 +35,10 @@ class AuditVetRetrieveUpdateAPIView(APIView):
         """
         segment_id = kwargs["pk"]
         segment = self._get_segment(segment_id)
+
         data = request.data
-        try:
-            audit = AuditProcessor.objects.get(id=segment.audit_id)
-        except AuditProcessor.DoesNotExist:
-            raise ValidationError("Segment has no audit.")
-        vetting_model = audit.segment.audit_vetting_model
+        skipped = data.get("skipped")
+        vetting_model = segment.audit_utils.vetting_model
         try:
             vetting_id = data["vetting_id"]
             vetting_item = vetting_model.objects.get(id=vetting_id)
@@ -48,12 +46,17 @@ class AuditVetRetrieveUpdateAPIView(APIView):
             raise ValidationError("You must provide a vetting_id.")
         except vetting_model.DoesNotExist:
             raise ValidationError("Vetting item does not exist.")
-        if vetting_item.checked_out_by:
+
+        if vetting_item.is_checked_out:
             raise ValidationError("Vetting item is currently checked out.")
-        vetting_item.checked_out_by = None
-        serializer = self.serializer(data)
-        serializer.is_valid()
-        serializer.update()
+        if skipped:
+            self._processs_skipped(skipped, vetting_item)
+
+        data["is_checked_out"] = False
+        data["processed"] = timezone.now()
+        serializer = self.serializer(data=data, audit_item=vetting_item)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         doc = Channel(serializer.data)
         ChannelManager(upsert_sections=(Sections.TASK_US_DATA,)).upsert([doc])
         return Response(status=HTTP_201_CREATED)
@@ -76,15 +79,41 @@ class AuditVetRetrieveUpdateAPIView(APIView):
             item_id = attrgetter(id_key)(next_item)
             segment.es_manager.sections = self.es_sections
             response = segment.es_manager.get([item_id])[0]
-            data = AuditChannelVetSerializer(response).data
+            data = AuditChannelVetSerializer(response, segment=segment).data
             data["vetting_id"] = next_item.id
         else:
             data = "All items are currently being vetted."
         return data
 
     def _get_segment(self, segment_id):
+        """
+        Retrieve CustomSegment. Raises ValidationError if not found
+        :param segment_id: int
+        :return:
+        """
         try:
             segment = CustomSegment.objects.get(id=segment_id)
         except CustomSegment.DoesNotExist:
             raise ValidationError(f"Segment with id: {segment_id} not found.", code=HTTP_404_NOT_FOUND)
         return segment
+
+    def _processs_skipped(self, skipped_type, vetting_item):
+        """
+        Handle user skipping vetting for item
+        If skipped_type is 0, then skipped since item is unavailable e.g. deleted from Youtube
+            Should set clean to False, skipped to False
+        If skipped_type is 1, then skipped since unavailable in region
+            Should set clean to False, skipped to True
+        :param skipped_type:
+        :param vetting_item:
+        :return:
+        """
+        vetting_item.clean = False
+        if skipped_type == 0:
+            # doesnt exist
+            vetting_item.skipped = False
+        elif skipped_type == 1:
+            vetting_item.skipped = True
+        else:
+            raise ValidationError(f"Invalid skip type. Must be 0-1, inclusive.")
+        vetting_item.save()
