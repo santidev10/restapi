4eea2b3d9057de1f0dc267de45aa2b9ce55047e4
Kenneth <kenneth.oh@channelfactory.com>
2020-02-12 16:02:21 -0800

    Assign new auth token when logging in as user to return correct permissions in subsequent requests, add test

--- administration/api/views/auth_as_a_user_admin.py
+++ administration/api/views/auth_as_a_user_admin.py
@@ -6,11 +6,11 @@ from rest_framework.status import HTTP_404_NOT_FOUND
 from rest_framework.views import APIView
 
 from userprofile.api.serializers import UserSerializer as RegularUserSerializer
-from userprofile.api.views.user_finalize_response import UserFinalizeResponse
 from userprofile.models import UserProfile
+from userprofile.models import UserDeviceToken
 
 
-class AuthAsAUserAdminApiView(UserFinalizeResponse, APIView):
+class AuthAsAUserAdminApiView(APIView):
     """
     Login as a user endpoint
     """
@@ -32,3 +32,14 @@ class AuthAsAUserAdminApiView(UserFinalizeResponse, APIView):
             for name, value in custom_auth_flags.items():
                 response_data[name] = value
         return Response(response_data)
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        """
+        Set new UserDeviceToken to set appropriate permissions for subsequent requests
+        """
+        device_token = UserDeviceToken.objects.create(user_id=response.data["id"])
+        response.data.update({
+            "token": device_token.key,
+            "device_id": device_token.device_id,
+        })
+        return super().finalize_response(request, response, *args, **kwargs)
