0975a087fdb345b57dba7e4e6e57014d7822960e
Kenneth <kenneth.oh@channelfactory.com>
2020-02-12 16:44:39 -0800

    Move shared logic into base serializer, add video vet serializer

--- audit_tool/api/serializers/audit_vet_base_serializer.py
+++ audit_tool/api/serializers/audit_vet_base_serializer.py
@@ -20,27 +20,21 @@ from es_components.models import Channel
 from es_components.constants import Sections
 
 
-class AuditChannelVetSerializer(Serializer):
+
+class AuditVetBaseSerializer(Serializer):
+    """
+    Base serializer for vetting models
+    """
+    document_model = None
+    general_data_language_field = None
+
     SECTIONS = (Sections.MAIN, Sections.TASK_US_DATA, Sections.MONETIZATION)
     has_vetting_history = False
-
-    """
-    age_group, channel_type, gender, and brand_safety values are stored as id values
-    """
-    # Postgres fields
-    vetting_history = SerializerMethodField()
-    segment_title = SerializerMethodField()
-    url = SerializerMethodField()
-    checked_out_at = DateTimeField(required=False, allow_null=True)
-    suitable = BooleanField(required=False)
-    processed = DateTimeField(required=False)
-    processed_by_user_id = IntegerField(required=False)
-    language_code = CharField(required=False) # Field for saving vetting item
 
     # Elasticsearch fields
     language = SerializerMethodField()
     age_group = CharField(source="task_us_data.age_group", default=None)
-    channel_type = CharField(source="task_us_data.channel_type", default=None)
+    content_type = CharField(source="task_us_data.content_type", default=None)
     gender = CharField(source="task_us_data.gender", default=None)
     brand_safety = ListField(source="task_us_data.brand_safety", default=[])
     iab_categories = ListField(source="task_us_data.iab_categories", default=[])
@@ -53,30 +47,17 @@ class AuditChannelVetSerializer(Serializer):
             pass
         super().__init__(*args, **kwargs)
 
-    def get_url(self, doc):
-        url = f"https://www.youtube.com/channel/{doc.main.id}/"
-        return url
+    def get_url(self, *args, **kwargs):
+        raise NotImplementedError
 
-    def get_vetting_history(self, doc):
-        """
-        Retrieve vetting history of all AuditChannelVet items with FK to AuditChannel
-        Only retrieve if serializing with Elasticsearch document
-        :param doc: Elasticsearch document
-        :return: dict
-        """
-        history = None
-        if hasattr(doc, "main"):
-            channel_id_hash = get_hash_name(doc.main.id)
-            vetting_items = AuditChannelVet.objects\
-                .filter(channel__channel_id_hash=channel_id_hash, processed__isnull=False)\
-                .select_related("channel__auditchannelmeta")
-            history = [{
-                "data": f"{item.channel.auditchannelmeta.name} - {item.processed.strftime('%b %d %Y')}",
-                "suitable": item.clean
-            } for item in vetting_items]
-        # Set bool for get_language method to return correct language field
-        self.has_vetting_history = bool(history)
-        return history
+    def get_vetting_history(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def save(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _save_vetting_item(self, *args, **kwargs):
+        raise NotImplementedError
 
     def get_language(self, doc):
         """
@@ -89,7 +70,7 @@ class AuditChannelVetSerializer(Serializer):
         if self.has_vetting_history:
             language = getattr(doc.task_us_data, "language", None)
         else:
-            language = getattr(doc.general_data, "top_language", None)
+            language = getattr(doc.general_data, self.general_data_language_field, None)
             language = LANG_CODES.get(language)
         return language
 
@@ -133,21 +114,21 @@ class AuditChannelVetSerializer(Serializer):
 
     def validate_age_group(self, value):
         """
-        Retrieve age_group enum value. Raises ValidationError if not found
+        Retrieve AuditAgeGroup value. Raises ValidationError if not found
         :param value: str
         :return: AuditCategory
         """
         age_group = AuditToolValidator.validate_age_group(int(value))
         return age_group
 
-    def validate_channel_type(self, value):
+    def validate_content_type(self, value):
         """
-        Retrieve channel_type enum value. Raises ValidationError if not found
+        Retrieve AuditContentType value. Raises ValidationError if not found
         :param value: str
         :return: str
         """
-        channel_type = AuditToolValidator.validate_channel_type(int(value))
-        return channel_type
+        content_type = AuditToolValidator.validate_content_type(int(value))
+        return content_type
 
     def validate_category(self, value):
         """
@@ -175,42 +156,7 @@ class AuditChannelVetSerializer(Serializer):
                 raise ValidationError(f"Category not found: {e}")
         return categories
 
-    def save(self, **kwargs):
-        """
-        Save values on self and related model
-        :param kwargs: dict
-        :return:
-        """
-        if not self.instance:
-            raise ValueError("To save serializer, must be provided instance object"
-                             "during instantiation.")
-        channel_meta = self.instance.channel.auditchannelmeta
-        channel_id = channel_meta.channel.channel_id
-
-        self._save_vetting_item(channel_meta)
-        blacklist_categories = self._save_brand_safety(channel_id)
-        self._save_elasticsearch(channel_id, blacklist_categories)
-
-    def _save_vetting_item(self, channel_meta):
-        """
-        Save only required fields for database
-        :param channel_meta: AuditChannelMeta
-        :return: None
-        """
-        data = {
-            "clean": self.validated_data["suitable"],
-            "checked_out_at": None,
-            "processed": timezone.now(),
-            "processed_by_user_id": self.validated_data["processed_by_user_id"],
-        }
-        for key, value in data.items():
-            setattr(self.instance, key, value)
-        self.instance.save(update_fields=list(data.keys()))
-        if self.validated_data["monetization"]["is_monetizable"] is True:
-            channel_meta.monetised = True
-            channel_meta.save()
-
-    def _save_brand_safety(self, channel_id):
+    def save_brand_safety(self, channel_id):
         """
         Save brand safety categories in BlacklistItem table
         :param channel_id: str
@@ -234,10 +180,10 @@ class AuditChannelVetSerializer(Serializer):
         data = list(blacklist_item.blacklist_category.keys())
         return data
 
-    def _save_elasticsearch(self, channel_id, blacklist_categories):
+    def save_elasticsearch(self, item_id, blacklist_categories):
         """
         Save vetting data to Elasticsearch
-        :param channel_id: str
+        :param item_id: str -> video id, channel id
         :param blacklist_categories: list -> [int, ...]
         :return: None
         """
@@ -246,19 +192,8 @@ class AuditChannelVetSerializer(Serializer):
         task_us_data["brand_safety"] = blacklist_categories
         task_us_data["language"] = self.validated_data["language_code"]
         # Update Elasticsearch document
-        channel_doc = Channel(channel_id)
-        channel_doc.populate_monetization(**self.validated_data["monetization"])
-        channel_doc.populate_task_us_data(**task_us_data)
+        doc = self.document_model(item_id)
+        doc.populate_monetization(**self.validated_data["monetization"])
+        doc.populate_task_us_data(**task_us_data)
         self.segment.es_manager.upsert_sections = self.SECTIONS
-        self.segment.es_manager.upsert([channel_doc])
-
-    def to_representation(self, instance):
-        """
-        Only repr Elasticsearch documents
-        :param instance:
-        :return:
-        """
-        res = instance
-        if hasattr(instance, "main"):
-            res = super().to_representation(instance)
-        return res
+        self.segment.es_manager.upsert([doc])
--- audit_tool/api/serializers/audit_video_vet_serializer.py
+++ audit_tool/api/serializers/audit_video_vet_serializer.py
@@ -6,21 +6,22 @@ from rest_framework.serializers import DateTimeField
 from rest_framework.serializers import IntegerField
 from rest_framework.serializers import SerializerMethodField
 
-from audit_tool.models import AuditChannelVet
+from audit_tool.models import AuditVideoVet
+from audit_tool.models import AuditChannel
 from audit_tool.models import get_hash_name
 from es_components.models import Channel
-from es_components.managers import VideoManager
-from es_components.query_builder import QueryBuilder
+from es_components.models import Video
 from es_components.constants import Sections
+from es_components.managers import ChannelManager
 from audit_tool.api.serializers.audit_vet_base_serializer import AuditVetBaseSerializer
 
 
-class AuditChannelVetSerializer(AuditVetBaseSerializer):
+class AuditVideoVetSerializer(AuditVetBaseSerializer):
     """
     age_group, channel_type, gender, and brand_safety values are stored as id values
     """
-    general_data_language_field = "top_language"
-    document_model = Channel
+    general_data_language_field = "language"
+    document_model = Video
 
     # Postgres fields
     vetting_history = SerializerMethodField()
@@ -33,49 +34,53 @@ class AuditChannelVetSerializer(AuditVetBaseSerializer):
     language_code = CharField(required=False) # Field for saving vetting item
 
     def get_url(self, doc):
-        url = f"https://www.youtube.com/channel/{doc.main.id}/"
+        url = f"https://www.youtube.com/video/{doc.main.id}/"
         return url
 
     def get_vetting_history(self, doc):
         """
-        Retrieve vetting history of all AuditChannelVet items with FK to AuditChannel
+        Retrieve vetting history of all AuditVideoVet items with FK to AuditChannel
         Only retrieve if serializing with Elasticsearch document
         :param doc: Elasticsearch document
         :return: dict
         """
         history = None
         if hasattr(doc, "main"):
-            channel_id_hash = get_hash_name(doc.main.id)
-            vetting_items = AuditChannelVet.objects\
-                .filter(channel__channel_id_hash=channel_id_hash, processed__isnull=False)\
-                .select_related("channel__auditchannelmeta")
+            video_id_hash = get_hash_name(doc.main.id)
+            vetting_items = AuditVideoVet.objects\
+                .select_related("video", "video__auditvideometa")\
+                .filter(video__video_id_hash=video_id_hash, processed__isnull=False)
             history = [{
-                "data": f"{item.channel.auditchannelmeta.name} - {item.processed.strftime('%b %d %Y')}",
+                "data": f"{item.video.auditvideometa.name} - {item.processed.strftime('%b %d %Y')}",
                 "suitable": item.clean
             } for item in vetting_items]
         # Set bool for get_language method to return correct language field
+        # If has never been vetted before, should return general data language, else vetted language
         self.has_vetting_history = bool(history)
         return history
 
     def save(self, **kwargs):
         """
-        Save values on AuditChannelVet instance, FK AuditChannelMeta, and Elasticsearch task_us_data section
-        If channel is monetized, will also update all videos monetization with _update_videos method
+        Save values on AuditVideoVet instance, FK AuditVideoMeta, and Elasticsearch task_us_data section
         :param kwargs: dict
         :return:
         """
         if not self.instance:
             raise ValueError("To save serializer, must be provided instance object"
                              "during instantiation.")
-        channel_meta = self.instance.channel.auditchannelmeta
-        channel_id = channel_meta.channel.channel_id
+        video_meta = self.instance.video.auditvideometa
+        video_id = video_meta.video.video_id
+        self._save_vetting_item()
+        blacklist_categories = self.save_brand_safety(video_id)
+        try:
+            channel_id = video_meta.video.channel.channel_id
+        except AttributeError:
+            channel_id = None
+        self.save_elasticsearch(video_id, blacklist_categories)
+        if channel_id:
+            self._update_channel(channel_id)
 
-        self._save_vetting_item(channel_meta, channel_id)
-        blacklist_categories = self.save_brand_safety(channel_id)
-        self.save_elasticsearch(channel_id, blacklist_categories)
-        self._update_videos(channel_id)
-
-    def _save_vetting_item(self, channel_meta, channel_id):
+    def _save_vetting_item(self):
         """
         Save only required fields for database
         :param channel_meta: AuditChannelMeta
@@ -90,17 +95,21 @@ class AuditChannelVetSerializer(AuditVetBaseSerializer):
         for key, value in data.items():
             setattr(self.instance, key, value)
         self.instance.save(update_fields=list(data.keys()))
-        if self.validated_data["monetization"]["is_monetizable"] is True:
-            channel_meta.monetised = True
-            channel_meta.save()
 
-    def _update_videos(self, channel_id):
+    def _update_channel(self, channel_id):
         """
-        Update all channel videos
+        Update channel monetization
         :param channel_id: str
         :return: None
         """
-        if self.validated_data["monetization"]["is_monetizable"] is True:
-            # Update all channel videos monetization
-            query = QueryBuilder().build().must().term().field(f"{Sections.CHANNEL}.id").value(channel_id).get()
-            VideoManager(sections=(Sections.MONETIZATION,)).update_monetization(query, True)
+        if channel_id and self.validated_data["monetization"]["is_monetizable"] is True:
+            try:
+                channel_meta = self.instance.channel.auditchannelmeta
+                channel_meta.monetised = True
+                channel_meta.save()
+            except AttributeError:
+                pass
+            manager = ChannelManager(sections=(Sections.MONETIZATION,))
+            channel_doc = Channel(channel_id)
+            channel_doc.populate_monetization(is_monetizable=True)
+            manager.upsert([channel_doc])
