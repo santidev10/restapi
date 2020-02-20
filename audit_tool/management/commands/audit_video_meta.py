3b1cbbea1d69ef8d3841378214bb950fd72b053d
Andrew <andrew@kabuto.com>
2020-02-19 19:30:05 -0800

    efficiency updates

--- audit_tool/management/commands/audit_video_meta.py
+++ audit_tool/management/commands/audit_video_meta.py
@@ -405,7 +405,7 @@ class Command(BaseCommand):
             data = remove_mentions_hashes_urls(data).lower()
             l = fasttext_lang(data)
             if l not in self.db_languages:
-                self.db_languages[l], _ = AuditLanguage.objects.get_or_create(language=l)
+                self.db_languages[l], _ = AuditLanguage.from_string(l)
             return self.db_languages[l]
         except Exception as e:
             pass
