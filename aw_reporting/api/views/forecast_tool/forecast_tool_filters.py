4a870141a057fb25636f70d5189fc0155b23b72a
Kharko, Yaryna <yaryna.kharko@sigma.software>
2020-03-06 15:00:35 +0200

    VIQ-3005 Improve performance of forecasting tool

--- aw_reporting/api/views/forecast_tool/forecast_tool_filters.py
+++ aw_reporting/api/views/forecast_tool/forecast_tool_filters.py
@@ -3,9 +3,20 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 
 from aw_reporting.tools.forecast_tool.forecast_tool import ForecastTool
+from cache.models import CacheItem
+from cache.constants import FORECAST_TOOL_FILTERS_KEY
 
 
 class ForecastToolFiltersApiView(RetrieveAPIView):
     def get(self, request, *args, **kwargs):
-        response = ForecastTool.get_filters()
-        return Response(data=response, status=HTTP_200_OK)
+        try:
+            cached_filters_object = CacheItem.objects.get(key=FORECAST_TOOL_FILTERS_KEY)
+            forecast_tool_filters = cached_filters_object.value
+        except CacheItem.DoesNotExist:
+            forecast_tool_filters = ForecastTool.get_filters()
+
+            cached_filters_object, _ = CacheItem.objects.get_or_create(key=FORECAST_TOOL_FILTERS_KEY)
+            cached_filters_object.value = forecast_tool_filters
+            cached_filters_object.save()
+
+        return Response(data=forecast_tool_filters, status=HTTP_200_OK)
