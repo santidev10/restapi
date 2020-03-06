211f684a9e4580e53b332909880ee5be63e5bb5a
Kenneth <kenneth.oh@channelfactory.com>
2020-03-06 14:17:09 -0800

    Extract subdomain from request host to use as config key for white label, add tests

--- userprofile/api/views/white_label.py
+++ userprofile/api/views/white_label.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from userprofile.constants import DEFAULT_DOMAIN
 from userprofile.models import WhiteLabel
 
 
@@ -8,6 +9,14 @@ class WhiteLabelApiView(APIView):
     permission_classes = ()
 
     def get(self, request):
-        domain = (request.get_host() or "").lower()
-        data = WhiteLabel.get(domain=domain).config
+        domain = (request.get_host() or DEFAULT_DOMAIN).lower()
+        # Handle rc- prefixes
+        sub_domain = domain.split("-")
+        try:
+            sub_domain = sub_domain[1]
+        except IndexError:
+            sub_domain = sub_domain[0]
+        # Remove .com
+        sub_domain = sub_domain.rsplit(".", 1)[0]
+        data = WhiteLabel.get(domain=sub_domain).config
         return Response(data)
