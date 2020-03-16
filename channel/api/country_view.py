68ea58c0d6bd6d7b659cc561e5612d598679497b
George Su <george.su@channelfactory.com>
2020-03-16 02:37:18 -0700

    Removed pycountry module requirement and unused country util functions.

--- channel/api/country_view.py
+++ channel/api/country_view.py
@@ -1,5 +1,3 @@
-from utils.country import get_country_code
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
