fd5be132348d88692e3d8c188250699b7d9f7bf6
George Su <george.su@channelfactory.com>
2020-03-30 17:19:03 -0700

    Fixed bug in how channel_ids_file is parsed.

--- channel/api/views/channel_track.py
+++ channel/api/views/channel_track.py
@@ -1,4 +1,5 @@
 import csv
+from io import StringIO
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from rest_framework.response import Response
@@ -19,20 +20,21 @@ class ChannelTrackApiView(APIView, PermissionRequiredMixin):
     )
 
     def post(self, request, *args, **kwargs):
-        channel_ids_file = request.data['channel_ids'] if "channel_ids" in request.data else None
+        channel_ids_file = request.data['channel_ids_file'] if "channel_ids_file" in request.data else None
         if not channel_ids_file:
-            raise ValidationError("channel_ids file is required.")
+            raise ValidationError("channel_ids_file is required.")
         channel_ids_file_split = channel_ids_file.name.split(".")
         if len(channel_ids_file_split) != 2:
-            raise ValidationError(f"Invalid channel_ids file. Expected CSV file. Received {channel_ids_file}.")
+            raise ValidationError(f"Invalid channel_ids_file. Expected CSV file. Received {channel_ids_file.name}.")
         channel_ids_file_type = channel_ids_file_split[1]
         if channel_ids_file_type != "csv":
-            raise ValidationError(f"Invalid channel_ids file type. Expected CSV. Received '{channel_ids_file_type}'.")
-        with open(channel_ids_file.name, "r") as f:
-            reader = csv.reader(f)
-            new_channel_ids = []
-            for row in reader:
-                new_channel_ids.append(row[0])
+            raise ValidationError(f"Invalid channel_ids_file type. Expected CSV. Received '{channel_ids_file_type}'.")
+        file = channel_ids_file.read().decode('utf-8-sig')
+        io_string = StringIO(file)
+        reader = csv.reader(io_string, delimiter=";", quotechar="|")
+        new_channel_ids = []
+        for row in reader:
+            new_channel_ids.append(row[0])
         try:
             num_tracked = track_channels(new_channel_ids)
         except Exception as e:
