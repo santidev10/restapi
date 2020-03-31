96af2119fdd91ccb2d3aaae1b53ba6e44700cb61
George Su <george.su@channelfactory.com>
2020-03-31 16:50:18 -0700

    Fixed unittests.

--- channel/tests/api/test_channel_track.py
+++ channel/tests/api/test_channel_track.py
@@ -35,7 +35,7 @@ class ChannelTrackTestCase(ExtendedAPITestCase, ESTestCase):
         channel_id_2 = str(next(int_iterator))
         channel_id_3 = str(next(int_iterator))
         channel_id_4 = str(next(int_iterator))
-        channel_ids = [channel_id_2, channel_id_3, channel_id_4]
+        channel_ids = ",".join([channel_id_2, channel_id_3, channel_id_4])
         response = self.client.post(self.url, data={'channel_ids': channel_ids})
         self.assertEqual(f"Added {len(channel_ids)} manually tracked channels.", response.data)
         channels = self.manager.get(channel_ids)
@@ -50,7 +50,8 @@ class ChannelTrackTestCase(ExtendedAPITestCase, ESTestCase):
         new_channel_id = str(next(int_iterator))
         new_channel_id_2 = str(next(int_iterator))
         new_channel_ids = [new_channel_id, new_channel_id_2]
-        response = self.client.post(self.url, data={'channel_ids': [self.channel_id_1] + new_channel_ids})
+        channel_ids = ",".join([self.channel_id_1] + new_channel_ids)
+        response = self.client.post(self.url, data={'channel_ids': channel_ids})
         self.assertEqual(f"Added {len(new_channel_ids)} manually tracked channels.", response.data)
         old_channel = self.manager.get([self.channel_id_1])[0]
         new_channels = self.manager.get([new_channel_ids])
