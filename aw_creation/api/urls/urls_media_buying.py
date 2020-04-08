7256d17577e3f0b6be5d55a890e384a5aef87de2
Kenneth <kenneth.oh@channelfactory.com>
2020-04-08 13:47:51 -0700

    Add kpi_filters endpoint to load filters separately

--- aw_creation/api/urls/urls_media_buying.py
+++ aw_creation/api/urls/urls_media_buying.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from aw_creation.api.urls.names import Name
 from aw_creation.api.views.media_buying import AccountDetailAPIView
 from aw_creation.api.views.media_buying.account_targeting import AccountTargetingAPIView
+from aw_creation.api.views.media_buying.account_kpi_filters import AccountKPIFiltersAPIView
 
 urlpatterns = [
     url(r'^account/(?P<pk>\w+)/$',
@@ -11,4 +12,7 @@ urlpatterns = [
     url(r'^account/(?P<pk>\w+)/targeting/$',
         AccountTargetingAPIView.as_view(),
         name="account_targeting"),
+    url(r'^account/(?P<pk>\w+)/targeting/kpi_filters/$',
+        AccountKPIFiltersAPIView.as_view(),
+        name="account_kpi_filters"),
 ]
