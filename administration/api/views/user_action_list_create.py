1754d41c7033460b60265aaed471ddfeaa69e2c8
Andrew Wong <andrew.wong@channelfactory.com>
2020-04-08 13:23:31 -0700

    Alphabetize imports.

--- administration/api/views/user_action_list_create.py
+++ administration/api/views/user_action_list_create.py
@@ -1,22 +1,18 @@
-import operator
-from functools import reduce
-
-from django.core.exceptions import ValidationError
-from django.db.models import Q
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
-
 from administration.api.serializers import UserActionCreateSerializer
 from administration.api.serializers import UserActionRetrieveSerializer
 from administration.models import UserAction
+from django.core.exceptions import ValidationError
+from django.db.models import Q
+from functools import reduce
+from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 from userprofile.api.views.user_finalize_response import UserFinalizeResponse
 from utils.api_paginator import CustomPageNumberPaginator
-from rest_framework.permissions import (
-    IsAdminUser,
-    IsAuthenticated
-)
-from rest_framework import permissions
+import operator
 
 
 class UserActionPermission(permissions.BasePermission):
