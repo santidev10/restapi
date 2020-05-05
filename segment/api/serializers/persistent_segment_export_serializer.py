3f435ce3323b7f9383d1c4af45320470d519e232
Andrew Wong <andrew.wong@channelfactory.com>
2020-05-05 14:23:11 -0700

    Updated PersistentSegmentSerializer to inherit from custom segment serializer and use parent's get_category method, which gets a comma-separated list of categories, instead of the top cat. Remove duplicate get_vetted method from custom segment serializer to mixin. Add vetting data to persistent export serializer.

--- segment/api/serializers/persistent_segment_export_serializer.py
+++ segment/api/serializers/persistent_segment_export_serializer.py
@@ -1,18 +1,21 @@
 """
 Segment api serializers module
 """
+from brand_safety.languages import LANGUAGES
 from rest_framework.serializers import CharField
 from rest_framework.serializers import IntegerField
-from rest_framework.serializers import Serializer
 from rest_framework.serializers import SerializerMethodField
-
-from brand_safety.languages import LANGUAGES
-from es_components.iab_categories import YOUTUBE_TO_IAB_CATEGORIES_MAPPING
+from segment.api.serializers.custom_segment_export_serializers import CustomSegmentChannelExportSerializer
+from segment.api.serializers.custom_segment_export_serializers import CustomSegmentVideoExportSerializer
 from utils.brand_safety import map_brand_safety_score
 
 
-class PersistentSegmentVideoExportSerializer(Serializer):
-    columns = ("URL", "Title", "Language", "Category", "Likes", "Dislikes", "Views", "Overall_Score")
+class PersistentSegmentVideoExportSerializer(
+    CustomSegmentVideoExportSerializer
+):
+    columns = ("URL", "Title", "Language", "Category", "Likes", "Dislikes",
+               "Views", "Overall_Score", "Vetted", "Brand_Safety", "Age_Group",
+               "Gender", "Content_Type")
 
     URL = SerializerMethodField("get_url")
     Title = CharField(source="general_data.title", default="")
@@ -22,6 +25,11 @@ class PersistentSegmentVideoExportSerializer(Serializer):
     Dislikes = IntegerField(source="stats.dislikes")
     Views = IntegerField(source="stats.views")
     Overall_Score = SerializerMethodField("get_overall_score")
+    Vetted = SerializerMethodField("get_vetted")
+    Brand_Safety = SerializerMethodField("get_brand_safety")
+    Age_Group = SerializerMethodField("get_age_group")
+    Gender = SerializerMethodField("get_gender")
+    Content_Type = SerializerMethodField("get_content_type")
 
     def get_url(self, obj):
         return f"https://www.youtube.com/video/{obj.main.id}"
@@ -35,17 +43,13 @@ class PersistentSegmentVideoExportSerializer(Serializer):
         score = map_brand_safety_score(obj.brand_safety.overall_score)
         return score
 
-    def get_category(self, obj):
-        youtube_category = (getattr(obj.general_data, "category", "") or "").lower()
-        try:
-            iab_category = YOUTUBE_TO_IAB_CATEGORIES_MAPPING.get(youtube_category)[-1]
-        except Exception as e:
-            iab_category = ""
-        return iab_category
 
-
-class PersistentSegmentChannelExportSerializer(Serializer):
-    columns = ("URL", "Title", "Language", "Category", "Subscribers", "Likes", "Dislikes", "Views", "Audited_Videos", "Overall_Score", "Vetted")
+class PersistentSegmentChannelExportSerializer(
+    CustomSegmentChannelExportSerializer
+):
+    columns = ("URL", "Title", "Language", "Category", "Subscribers", "Likes",
+               "Dislikes", "Views", "Audited_Videos", "Overall_Score", "Vetted",
+               "Brand_Safety", "Age_Group", "Gender", "Content_Type")
 
     # Fields map to segment export rows
     URL = SerializerMethodField("get_url")
@@ -59,6 +63,10 @@ class PersistentSegmentChannelExportSerializer(Serializer):
     Audited_Videos = IntegerField(source="brand_safety.videos_scored")
     Overall_Score = SerializerMethodField("get_overall_score")
     Vetted = SerializerMethodField('get_vetted')
+    Brand_Safety = SerializerMethodField("get_brand_safety")
+    Age_Group = SerializerMethodField("get_age_group")
+    Gender = SerializerMethodField("get_gender")
+    Content_Type = SerializerMethodField("get_content_type")
 
     def get_url(self, obj):
         return f"https://www.youtube.com/channel/{obj.main.id}"
@@ -71,19 +79,3 @@ class PersistentSegmentChannelExportSerializer(Serializer):
     def get_overall_score(self, obj):
         score = map_brand_safety_score(obj.brand_safety.overall_score)
         return score
-
-    def get_category(self, obj):
-        youtube_category = (getattr(obj.general_data, "top_category", "") or "").lower()
-        try:
-            iab_category = YOUTUBE_TO_IAB_CATEGORIES_MAPPING.get(youtube_category)[-1]
-        except Exception as e:
-            iab_category = ""
-        return iab_category
-
-    def get_vetted(self, obj):
-        """
-        Indicate that a channel is vetted with a 'Y' when it has a non-empty task_us_data node
-        """
-        if getattr(obj, 'task_us_data', {}):
-            return 'Y'
-        return ''
