35d62efbef4590367732a8a9dcbca7364a170143
George Su <george.su@channelfactory.com>
2020-05-06 12:01:54 -0700

    Updated celery scheduler to only run pull_tts_url_transcripts task.

--- saas/configs/celery.py
+++ saas/configs/celery.py
@@ -28,134 +28,141 @@ DMP_CELERY_RESULT_EXTENDED = True
 CELERY_TIMEZONE = "UTC"
 
 CELERY_BEAT_SCHEDULE = {
-    "google_ads_campaign_update": {
-        "task": "aw_reporting.google_ads.tasks.update_campaigns.setup_update_campaigns",
-        "schedule": crontab(hour="*", minute="*/5"),
-    },
-    "google_ads_update_without_campaigns": {
-        "task": "aw_reporting.google_ads.tasks.update_without_campaigns.setup_update_without_campaigns",
-        "schedule": crontab(hour="*", minute="*/5"),
-    },
-    "google_ads_update_audiences": {
-        "task": "aw_reporting.google_ads.tasks.update_audiences.update_audiences",
-        "schedule": crontab(day_of_month="1", hour="0", minute="0"),
-    },
-    "google_ads_update_geo_targets": {
-        "task": "aw_reporting.google_ads.tasks.update_geo_targets.update_geo_targets",
-        "schedule": crontab(day_of_month="1", hour="1", minute="0"),
-    },
-    "full-sf-update": {
-        "task": "aw_reporting.update.update_salesforce_data.update_salesforce_data",
-        "schedule": crontab(hour="*", minute="0"),
-        "kwargs": dict(do_update=os.getenv("DO_SALESFORCE_UPDATE", "0") == "1")
-    },
-    "schedule_daily_email_notifications": {
-        "task": "email_reports.tasks.schedule_daily_reports",
-        "schedule": crontab(hour="0", minute="0"),
-        "kwargs": dict(
-            reports=["CampaignUnderMargin", "TechFeeCapExceeded", "CampaignUnderPacing",
-                     "CampaignOverPacing", "FlightDeliveredReport"],
-        ),
-    },
-    "daily_es_monitoring_report": {
-        "task": "email_reports.tasks.send_daily_email_reports",
-        "schedule": crontab(hour="13", minute="30"),
-        "kwargs": dict(
-            reports=["ESMonitoringEmailReport"],
-        ),
-    },
-    "daily_apex_notifications": {
-        "task": "email_reports.tasks.send_daily_email_reports",
-        "schedule": crontab(hour="13", minute="30"),
-        "kwargs": dict(
-            reports=["DailyApexCampaignEmailReport"],
-        ),
-    },
-    "recreate-demo-data": {
-        "task": "aw_reporting.demo.recreate_demo_data.recreate_demo_data",
-        "schedule": crontab(hour="0", minute="0"),
-    },
-    "update-videos-percentiles": {
-        "task": "video.tasks.update_videos_percentiles.update_videos_percentiles",
-        "schedule": 3600,
-    },
-    "update-channels-percentiles": {
-        "task": "channel.tasks.update_channels_percentiles.update_channels_percentiles",
-        "schedule": 3600,
-    },
-    "update-keywords-percentiles": {
-        "task": "keywords.tasks.update_keywords_percentiles.update_keywords_percentiles",
-        "schedule": 3600,
-    },
-    "pull-custom-transcripts": {
-        "task": "transcripts.tasks.pull_custom_transcripts.pull_custom_transcripts",
+    "pull-tts-url-transcripts": {
+        "task": "transcripts.tasks.pull_tts_url_transcripts.pull_tts_url_transcripts",
         "schedule": 90
-    },
-    "cache-video-aggregations": {
-        "task": "cache.tasks.cache_video_aggregations.cache_video_aggregations",
-        "schedule": crontab(hour="*", minute="*/30"),
-    },
-    "cache-channel-aggregations": {
-        "task": "cache.tasks.cache_channel_aggregations.cache_channel_aggregations",
-        "schedule": crontab(hour="*", minute="*/30"),
-    },
-    "cache-keyword-aggregations": {
-        "task": "cache.tasks.cache_keyword_aggregations.cache_keyword_aggregations",
-        "schedule": crontab(hour="*", minute="*/30"),
-    },
-    "cache-research-videos-defaults": {
-        "task": "cache.tasks.cache_research_videos_defaults.cache_research_videos_defaults",
-        "schedule": crontab(hour="*", minute="*/30"),
-    },
-    "cache-research-channels-defaults": {
-        "task": "cache.tasks.cache_research_channels_defaults.cache_research_channels_defaults",
-        "schedule": crontab(hour="*", minute="*/30"),
-    },
-    "cache-research-keywords-defaults": {
-        "task": "cache.tasks.cache_research_keywords_defaults.cache_research_keywords_defaults",
-        "schedule": crontab(hour="*", minute="*/30"),
-    },
-    "cache_pricing_tool_filters": {
-        "task": "cache.tasks.cache_pricing_tool_filters.cache_pricing_tool_filters",
-        "schedule": crontab(minute=0, hour='*/6'),
-    },
-    "cache_global_trends_filters": {
-        "task": "cache.tasks.cache_global_trends_filters.cache_global_trends_filters",
-        "schedule": crontab(minute=0, hour='*/6'),
-    },
-    "cache_forecast_tool_filters": {
-        "task": "cache.tasks.cache_forecast_tool_filters.cache_forecast_tool_filters",
-        "schedule": crontab(minute=0, hour='*/6'),
-    },
-    "generate_persistent_segments": {
-        "task": "segment.tasks.generate_persistent_segments.generate_persistent_segments",
-        "schedule": crontab(minute="*/10"),
-    },
-    "brand_safety_channel_discovery": {
-        "task": "brand_safety.tasks.channel_discovery.channel_discovery_scheduler",
-        "schedule": 60 * 5,
-    },
-    "brand_safety_channel_outdated": {
-        "task": "brand_safety.tasks.channel_outdated.channel_outdated_scheduler",
-        "schedule": 60 * 5,
-    },
-    "brand_safety_video_discovery": {
-        "task": "brand_safety.tasks.video_discovery.video_discovery_scheduler",
-        "schedule": 60 * 5,
-    },
-    "userprofile_clean_device_auth_tokens": {
-        "task": "userprofile.tasks.clean_device_auth_tokens.clean_device_auth_tokens",
-        "schedule": crontab(day_of_month="1", hour="1", minute="0"),
-    },
-    "audit_tool_check_in_vetting_items": {
-        "task": "audit_tool.tasks.check_in_vetting_items.check_in_vetting_items_task",
-        "schedule": crontab(minute="*/5"),
-    },
-    "segment_update_statistics": {
-        "task": "segment.tasks.update_segment_statistics.update_segment_statistics",
-        "schedule": crontab(minute="*/10"),
     }
 }
+
+# CELERY_BEAT_SCHEDULE = {
+#     "google_ads_campaign_update": {
+#         "task": "aw_reporting.google_ads.tasks.update_campaigns.setup_update_campaigns",
+#         "schedule": crontab(hour="*", minute="*/5"),
+#     },
+#     "google_ads_update_without_campaigns": {
+#         "task": "aw_reporting.google_ads.tasks.update_without_campaigns.setup_update_without_campaigns",
+#         "schedule": crontab(hour="*", minute="*/5"),
+#     },
+#     "google_ads_update_audiences": {
+#         "task": "aw_reporting.google_ads.tasks.update_audiences.update_audiences",
+#         "schedule": crontab(day_of_month="1", hour="0", minute="0"),
+#     },
+#     "google_ads_update_geo_targets": {
+#         "task": "aw_reporting.google_ads.tasks.update_geo_targets.update_geo_targets",
+#         "schedule": crontab(day_of_month="1", hour="1", minute="0"),
+#     },
+#     "full-sf-update": {
+#         "task": "aw_reporting.update.update_salesforce_data.update_salesforce_data",
+#         "schedule": crontab(hour="*", minute="0"),
+#         "kwargs": dict(do_update=os.getenv("DO_SALESFORCE_UPDATE", "0") == "1")
+#     },
+#     "schedule_daily_email_notifications": {
+#         "task": "email_reports.tasks.schedule_daily_reports",
+#         "schedule": crontab(hour="0", minute="0"),
+#         "kwargs": dict(
+#             reports=["CampaignUnderMargin", "TechFeeCapExceeded", "CampaignUnderPacing",
+#                      "CampaignOverPacing", "FlightDeliveredReport"],
+#         ),
+#     },
+#     "daily_es_monitoring_report": {
+#         "task": "email_reports.tasks.send_daily_email_reports",
+#         "schedule": crontab(hour="13", minute="30"),
+#         "kwargs": dict(
+#             reports=["ESMonitoringEmailReport"],
+#         ),
+#     },
+#     "daily_apex_notifications": {
+#         "task": "email_reports.tasks.send_daily_email_reports",
+#         "schedule": crontab(hour="13", minute="30"),
+#         "kwargs": dict(
+#             reports=["DailyApexCampaignEmailReport"],
+#         ),
+#     },
+#     "recreate-demo-data": {
+#         "task": "aw_reporting.demo.recreate_demo_data.recreate_demo_data",
+#         "schedule": crontab(hour="0", minute="0"),
+#     },
+#     "update-videos-percentiles": {
+#         "task": "video.tasks.update_videos_percentiles.update_videos_percentiles",
+#         "schedule": 3600,
+#     },
+#     "update-channels-percentiles": {
+#         "task": "channel.tasks.update_channels_percentiles.update_channels_percentiles",
+#         "schedule": 3600,
+#     },
+#     "update-keywords-percentiles": {
+#         "task": "keywords.tasks.update_keywords_percentiles.update_keywords_percentiles",
+#         "schedule": 3600,
+#     },
+#     "pull-custom-transcripts": {
+#         "task": "transcripts.tasks.pull_custom_transcripts.pull_custom_transcripts",
+#         "schedule": 90
+#     },
+#     "cache-video-aggregations": {
+#         "task": "cache.tasks.cache_video_aggregations.cache_video_aggregations",
+#         "schedule": crontab(hour="*", minute="*/30"),
+#     },
+#     "cache-channel-aggregations": {
+#         "task": "cache.tasks.cache_channel_aggregations.cache_channel_aggregations",
+#         "schedule": crontab(hour="*", minute="*/30"),
+#     },
+#     "cache-keyword-aggregations": {
+#         "task": "cache.tasks.cache_keyword_aggregations.cache_keyword_aggregations",
+#         "schedule": crontab(hour="*", minute="*/30"),
+#     },
+#     "cache-research-videos-defaults": {
+#         "task": "cache.tasks.cache_research_videos_defaults.cache_research_videos_defaults",
+#         "schedule": crontab(hour="*", minute="*/30"),
+#     },
+#     "cache-research-channels-defaults": {
+#         "task": "cache.tasks.cache_research_channels_defaults.cache_research_channels_defaults",
+#         "schedule": crontab(hour="*", minute="*/30"),
+#     },
+#     "cache-research-keywords-defaults": {
+#         "task": "cache.tasks.cache_research_keywords_defaults.cache_research_keywords_defaults",
+#         "schedule": crontab(hour="*", minute="*/30"),
+#     },
+#     "cache_pricing_tool_filters": {
+#         "task": "cache.tasks.cache_pricing_tool_filters.cache_pricing_tool_filters",
+#         "schedule": crontab(minute=0, hour='*/6'),
+#     },
+#     "cache_global_trends_filters": {
+#         "task": "cache.tasks.cache_global_trends_filters.cache_global_trends_filters",
+#         "schedule": crontab(minute=0, hour='*/6'),
+#     },
+#     "cache_forecast_tool_filters": {
+#         "task": "cache.tasks.cache_forecast_tool_filters.cache_forecast_tool_filters",
+#         "schedule": crontab(minute=0, hour='*/6'),
+#     },
+#     "generate_persistent_segments": {
+#         "task": "segment.tasks.generate_persistent_segments.generate_persistent_segments",
+#         "schedule": crontab(minute="*/10"),
+#     },
+#     "brand_safety_channel_discovery": {
+#         "task": "brand_safety.tasks.channel_discovery.channel_discovery_scheduler",
+#         "schedule": 60 * 5,
+#     },
+#     "brand_safety_channel_outdated": {
+#         "task": "brand_safety.tasks.channel_outdated.channel_outdated_scheduler",
+#         "schedule": 60 * 5,
+#     },
+#     "brand_safety_video_discovery": {
+#         "task": "brand_safety.tasks.video_discovery.video_discovery_scheduler",
+#         "schedule": 60 * 5,
+#     },
+#     "userprofile_clean_device_auth_tokens": {
+#         "task": "userprofile.tasks.clean_device_auth_tokens.clean_device_auth_tokens",
+#         "schedule": crontab(day_of_month="1", hour="1", minute="0"),
+#     },
+#     "audit_tool_check_in_vetting_items": {
+#         "task": "audit_tool.tasks.check_in_vetting_items.check_in_vetting_items_task",
+#         "schedule": crontab(minute="*/5"),
+#     },
+#     "segment_update_statistics": {
+#         "task": "segment.tasks.update_segment_statistics.update_segment_statistics",
+#         "schedule": crontab(minute="*/10"),
+#     }
+# }
 
 
 class Queue:
