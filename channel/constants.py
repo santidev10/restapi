eccf2ca947d98b32891d4effd52e426d7d2386b5
George Su <george.su@channelfactory.com>
2020-05-18 11:51:14 -0700

    Updated TERMS_FILTER and EXISTS_FILTER for Channels/Videos to include new vetted aggs filters.

--- channel/constants.py
+++ channel/constants.py
@@ -1,7 +1,8 @@
 TERMS_FILTER = ("general_data.country", "general_data.country_code", "general_data.top_language",
                 "general_data.top_lang_code", "general_data.top_category", "custom_properties.preferred",
                 "analytics.verified", "stats.channel_group", "main.id", "general_data.iab_categories",
-                "task_us_data.age_group", "task_us_data.content_type", "task_us_data.gender")
+                "task_us_data.age_group", "task_us_data.content_type", "task_us_data.gender",
+                "custom_properties.is_tracked")
 
 MATCH_PHRASE_FILTER = ("general_data.title", "general_data.description")
 
