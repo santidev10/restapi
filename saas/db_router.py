aec5fd0292cd5c68e1388f7cf9f5b3aa32025867
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2020-05-27 01:21:22 +0300

    VIQ2-156: Fix pylint for saas

--- saas/db_router.py
+++ saas/db_router.py
@@ -1,8 +1,5 @@
-import os
-
-
-APP_NAMES = ['audit_tool', 'brand_safety', 'transcripts']
-DB_NAME = 'audit'
+APP_NAMES = ["audit_tool", "brand_safety", "transcripts"]
+DB_NAME = "audit"
 
 
 class AuditDBRouter:
@@ -10,25 +7,29 @@ class AuditDBRouter:
     A router to control all database operations on models in the
     audit_tool application.
     """
-    def db_for_read(self, model, **hints):
+
+    def db_for_read(self, model, **__):
         if model._meta.app_label in APP_NAMES:
             return DB_NAME
+        return None
 
-    def db_for_write(self, model, **hints):
+    def db_for_write(self, model, **__):
         if model._meta.app_label in APP_NAMES:
             return DB_NAME
+        return None
 
-    def allow_relation(self, obj1, obj2, **hints):
+    def allow_relation(self, obj1, obj2, **__):
         if obj1._meta.app_label in APP_NAMES and obj2._meta.app_label in APP_NAMES:
             return True
-        elif all([app_name not in [obj1._meta.app_label, obj2._meta.app_label] for app_name in APP_NAMES]):
+        if all([app_name not in [obj1._meta.app_label, obj2._meta.app_label] for app_name in APP_NAMES]):
             return None
         return False
 
-    def allow_migrate(self, db, app_label, model_name=None, **hints):
+    def allow_migrate(self, db, app_label, *_, **__):
         if app_label in APP_NAMES:
             # audit_tool app should be migrated only on the audit database.
             return db == DB_NAME
-        elif db == DB_NAME:
-            # ensure that all other apps don't get migrated on the audit database.
+        if db == DB_NAME:
+            # ensure that all other apps don"t get migrated on the audit database.
             return False
+        return False
