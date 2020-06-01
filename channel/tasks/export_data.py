634829db40e34c9ae2b1545e2bdb5a9c90c722ea
George Su <george.su@channelfactory.com>
2020-06-01 16:59:35 -0700

    Updated Research exports timeout to be 24 hours instead of 1 hour.

--- channel/tasks/export_data.py
+++ channel/tasks/export_data.py
@@ -52,7 +52,7 @@ def export_channels_data(query_params, export_name, user_emails, export_url):
     subject = "Export Channels"
     body = f"Export is ready to download.\n" \
            f"Please click <a href='{export_url}'>here</a> to download the report.\n" \
-           f"NOTE: Link will expire in 2 weeks.\n"
+           f"NOTE: Link will expire in 24 hours.\n"
 
     # E-mail
     from_email = settings.EXPORTS_EMAIL_ADDRESS
--- video/tasks/export_data.py
+++ video/tasks/export_data.py
@@ -50,7 +50,7 @@ def export_videos_data(query_params, export_name, user_emails, export_url):
     subject = "Export Videos"
     body = f"Export is ready to download.\n" \
            f"Please click <a href='{export_url}'>here</a> to download the report.\n" \
-           f"NOTE: Link will expire in 2 weeks.\n"
+           f"NOTE: Link will expire in 24 hours.\n"
 
     # E-mail
     from_email = settings.EXPORTS_EMAIL_ADDRESS
