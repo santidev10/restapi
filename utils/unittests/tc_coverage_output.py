5a7e0baf88194c39afad8158505bf313ca7ff947
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2020-06-04 21:11:11 +0300

    VIQ2-224: Fix pylint

--- utils/unittests/tc_coverage_output.py
+++ utils/unittests/tc_coverage_output.py
@@ -1,4 +1,3 @@
-# pylint: skip-file
 import argparse
 
 from lxml import etree
@@ -7,16 +6,16 @@ from teamcity.messages import TeamcityServiceMessages
 
 class MessageService(TeamcityServiceMessages):
     def buildStatisticLinesRate(self, linesRate):
-        self.message('buildStatisticValue', key='CodeCoverageL', value=str(linesRate))
+        self.message("buildStatisticValue", key="CodeCoverageL", value=str(linesRate))
 
     def buildStatisticBranchesCovered(self, branchesCovered):
-        self.message('buildStatisticValue', key='CodeCoverageAbsBCovered', value=str(branchesCovered))
+        self.message("buildStatisticValue", key="CodeCoverageAbsBCovered", value=str(branchesCovered))
 
     def buildStatisticTotalBranches(self, totalBranches):
-        self.message('buildStatisticValue', key='CodeCoverageAbsBTotal', value=str(totalBranches))
+        self.message("buildStatisticValue", key="CodeCoverageAbsBTotal", value=str(totalBranches))
 
     def buildStatisticBranchesRate(self, branchesRate):
-        self.message('buildStatisticValue', key='CodeCoverageB', value=str(branchesRate))
+        self.message("buildStatisticValue", key="CodeCoverageB", value=str(branchesRate))
 
 
 def get_file_path_from_args():
@@ -31,14 +30,14 @@ def get_file_path_from_args():
 def parse():
     msg_service = MessageService()
     tree = etree.parse(get_file_path_from_args())
-    node = tree.xpath('//coverage')[0]
-    msg_service.buildStatisticLinesCovered(node.get('lines-covered'))
-    msg_service.buildStatisticTotalLines(node.get('lines-valid'))
-    msg_service.buildStatisticLinesRate(float(node.get('line-rate')) * 100)
+    node = tree.xpath("//coverage")[0]
+    msg_service.buildStatisticLinesCovered(node.get("lines-covered"))
+    msg_service.buildStatisticTotalLines(node.get("lines-valid"))
+    msg_service.buildStatisticLinesRate(float(node.get("line-rate")) * 100)
 
-    msg_service.buildStatisticBranchesCovered(node.get('branches-covered'))
-    msg_service.buildStatisticTotalBranches(node.get('branches-valid'))
-    msg_service.buildStatisticBranchesRate(float(node.get('branch-rate')) * 100)
+    msg_service.buildStatisticBranchesCovered(node.get("branches-covered"))
+    msg_service.buildStatisticTotalBranches(node.get("branches-valid"))
+    msg_service.buildStatisticBranchesRate(float(node.get("branch-rate")) * 100)
 
 
 if __name__ == "__main__":
