f33aa70e0464bba2810a7f78c6ed3af8ad652fd5
Andrew Wong <andrew.wong@channelfactory.com>
2020-06-10 11:15:47 -0700

    Revert "Change aws credentails for s3 connector."
    This reverts commit 5a03b1a199a2a4d115c8c807d990f520d041b9f0.

--- utils/aws/base_service.py
+++ utils/aws/base_service.py
@@ -5,8 +5,8 @@ from django.conf import settings
 def get_client(service_name):
     return boto3.client(
         service_name,
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
+        aws_access_key_id=settings.AMAZON_S3_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AMAZON_S3_SECRET_ACCESS_KEY
     )
 
 
