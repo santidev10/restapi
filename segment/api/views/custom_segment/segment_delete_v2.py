b3592aa91c806a6ad35891d9f93fb1988186c121
George Su <george.su@channelfactory.com>
2020-06-16 23:18:30 -0700

    Working on segment_delete perms.

--- segment/api/views/custom_segment/segment_delete_v2.py
+++ segment/api/views/custom_segment/segment_delete_v2.py
@@ -1,12 +1,22 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import DestroyAPIView
+from rest_framework.permissions import IsAdminUser
 
 from segment.api.serializers.custom_segment_serializer import CustomSegmentSerializer
 from segment.models import CustomSegment
+from utils.permissions import user_has_permission
+from utils.permissions import or_permission_classes
 
 
 class SegmentDeleteApiViewV2(DestroyAPIView):
     serializer_class = CustomSegmentSerializer
+
+    permission_classes = (
+        or_permission_classes(
+            IsAdminUser,
+
+        ),
+    )
 
     def delete(self, request, *args, **kwargs):
         segment = self.get_object()
