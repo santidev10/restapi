a7266ab4f97ad4731e4332705761d77c6d942358
k <o>
2020-06-25 15:54:31 -0700

    Add model to persist alerts that can not be determined at runtime, add alerts, add tests

--- aw_reporting/models/signals/init_signals.py
+++ aw_reporting/models/signals/init_signals.py
@@ -4,6 +4,7 @@ from django.db.models.signals import pre_save
 
 def init_signals():
     init_flight_signals()
+    init_placement_signals()
 
 
 # pylint: disable=import-outside-toplevel
@@ -12,4 +13,12 @@ def init_flight_signals():
     from aw_reporting.models.signals.salesforce_signals import pre_save_flight_receiver
 
     pre_save.connect(pre_save_flight_receiver, sender=Flight, dispatch_uid="pre_save_flight_receiver")
+
+
+def init_placement_signals():
+    from aw_reporting.models.salesforce import OpPlacement
+    from aw_reporting.models.signals.salesforce_signals import pre_save_placement_receiver
+
+    pre_save.connect(pre_save_placement_receiver, sender=OpPlacement, dispatch_uid="pre_save_placement_receiver")
+
 # pylint: enable=import-outside-toplevel
