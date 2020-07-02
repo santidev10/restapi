ad7db3456a04364f32c37275e5f99a6f41379dba
George Su <george.su@channelfactory.com>
2020-07-02 13:39:21 -0700

    Revert "Added 'custom_target_list_creation' permission check to segment_create_v3 endpoint."
    This reverts commit 2f0898ae0ace04aa305ff8cf37ad5eb7034542f8.

--- segment/api/views/custom_segment/segment_create_v3.py
+++ segment/api/views/custom_segment/segment_create_v3.py
@@ -21,7 +21,6 @@ from segment.tasks.generate_custom_segment import generate_custom_segment
 from segment.utils.utils import validate_boolean
 from segment.utils.utils import validate_date
 from segment.utils.utils import validate_numeric
-from utils.permissions import or_permission_classes
 from utils.permissions import user_has_permission
 
 
@@ -35,10 +34,7 @@ class SegmentCreateApiViewV3(CreateAPIView):
     )
     serializer_class = CustomSegmentSerializer
     permission_classes = (
-        or_permission_classes(
-            user_has_permission("userprofile.vet_audit_admin"),
-            user_has_permission("userprofile.custom_target_list_creation")
-        )
+        user_has_permission("userprofile.vet_audit_admin"),
     )
     parser_classes = [MultiPartParser]
 
