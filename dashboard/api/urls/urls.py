3bdc3d229768b98764dad7c1e61ec2aa10b8abbb
Andrew <andrew@kabuto.com>
2020-07-30 13:52:17 -0700

    new audit widget API

--- dashboard/api/urls/urls.py
+++ dashboard/api/urls/urls.py
@@ -6,9 +6,12 @@ from django.conf.urls import url
 from .names import DashboardPathName
 from dashboard.api.views import DashboardListAPIView
 from dashboard.api.views import DashboardPacingAlertsAPIView
+from dashboard.api.views import DashboardAuditQueueAPIView
 
 urlpatterns = [
     url(r"^dashboard/$", DashboardListAPIView.as_view(), name=DashboardPathName.DASHBOARD_LIST),
     url(r"^dashboard/pacing_alerts/$", DashboardPacingAlertsAPIView.as_view(),
         name=DashboardPathName.DASHBOARD_PACING_ALERTS),
+    url(r"^dashboard/audit_queue/$", DashboardAuditQueueAPIView.as_view(),
+        name=DashboardPathName.DASHBOARD_AUDIT_QUEUE),
 ]
