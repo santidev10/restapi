de793124458b2f64895e34b28122c51fd5c43e97
George Su <george.su@channelfactory.com>
2020-07-30 15:01:33 -0700

    Working on Industry Performance Dashboard.

--- dashboard/api/views/dashboard_industry_performance.py
+++ dashboard/api/views/dashboard_industry_performance.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from cache.models import CacheItem
+from dashboard.api.views.constants import DASHBOARD_INDUSTRY_PERFORMANCE_CACHE_PREFIX
 from dashboard.utils import get_cache_key
 from es_components.constants import Sections
 from es_components.managers.channel import ChannelManager
@@ -13,7 +14,6 @@ from utils.datetime import now_in_default_tz
 
 
 class DashboardIndustryPerformanceAPIView(APIView):
-    CACHE_PREFIX = "dashboard_industry_performance_"
     CACHE_TTL = 3600
     ALLOWED_CHANNEL_SORTS = ["stats.last_30day_subscribers", "stats.last_30day_views", "ads_stats.video_view_rate",
                              "ads_stats.ctr_v"]
@@ -23,7 +23,7 @@ class DashboardIndustryPerformanceAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
         params = str(request.query_params) + str(kwargs)
-        cache_key = get_cache_key(params, prefix=self.CACHE_PREFIX)
+        cache_key = get_cache_key(params, prefix=DASHBOARD_INDUSTRY_PERFORMANCE_CACHE_PREFIX)
         try:
             cache = CacheItem.objects.get(key=cache_key).value
             if cache.updated_at < now_in_default_tz() - timedelta(seconds=self.CACHE_TTL):
@@ -39,6 +39,34 @@ class DashboardIndustryPerformanceAPIView(APIView):
         channel_sort = request.query_params.get("channel_sort") or "stats.last_30day_subscribers"
         video_sort = request.query_params.get("video_sort") or "stats.last_30day_views"
         category_sort = request.query_params.get("category_sort") or "stats.last_30day_subscribers"
+        channel_manager = ChannelManager(sections=(Sections.GENERAL_DATA, Sections.STATS, Sections.ADS_STATS),
+                                         upsert_sections=())
+        video_manager = VideoManager(sections=(Sections.GENERAL_DATA, Sections.STATS, Sections.ADS_STATS),
+                                     upsert_sections=())
+        channel_forced_filters = channel_manager.forced_filters(include_deleted=False)
+        video_forced_filters = video_manager.forced_filters(include_deleted=False)
+        category_forced_filters = channel_manager.forced_filters(include_deleted=False)
+
+        channel_sorting = {
+            channel_sort: {
+                "order": "desc"
+            }
+        }
+
+        video_sorting = {
+            video_sort: {
+                "order": "desc"
+            }
+        }
+
+        category_sorting = {
+            category_sort: {
+                "order": "desc"
+            }
+        }
+
+        top_channels = channel_manager.search(filters=channel_forced_filters, sort=channel_sorting, limit=10)
+        top_videos = video_manager.search(filters=video_forced_filters, sort=video_sorting, limit=10)
 
         pass
         # return data
