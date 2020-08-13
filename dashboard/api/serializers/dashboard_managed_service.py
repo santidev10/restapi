565053038f7079fa7d7fc98ef8efe1e1732ef05b
Andrew Wong <andrew.wong@channelfactory.com>
2020-08-12 21:05:11 -0700

    /dashboard/managed_service endpoint working. Still WIP.

--- dashboard/api/serializers/dashboard_managed_service.py
+++ dashboard/api/serializers/dashboard_managed_service.py
@@ -1,27 +1,30 @@
+from collections import defaultdict
 from numbers import Number
+from utils.lang import pick_dict
 
-from aw_reporting.models.salesforce_constants import SalesForceGoalType
-from rest_framework.fields import SerializerMethodField
-from rest_framework.serializers import Serializer
-from utils.serializers.fields import PercentField
-# from rest_framework.serializers import FloatField
+from django.db.models import Avg
+from django.db.models import Case
+from django.db.models import ExpressionWrapper
+from django.db.models import F
 from django.db.models import FloatField
-from rest_framework.serializers import CharField
+from django.db.models import Max
+from django.db.models import Min
+from django.db.models import Sum
+from django.db.models import When
+from rest_framework.fields import SerializerMethodField
+
 from aw_creation.api.serializers import DashboardAccountCreationListSerializer
 from aw_creation.models import AccountCreation
-from django.db.models import F, ExpressionWrapper, Sum, Avg
-from django.db.models import Case
-from django.db.models import When
+from aw_reporting.models import dict_add_calculated_stats
+from aw_reporting.models import dict_norm_base_stats
+from aw_reporting.models.ad_words.calculations import DASHBOARD_MANAGED_SERVICE_CALCULATED_STATS
+from aw_reporting.models.ad_words.calculations import dashboard_managed_service_aggregator
 from aw_reporting.models.ad_words.campaign import Campaign
 
 
-
-class DashboardManagedServiceAveragesSerializer(DashboardAccountCreationListSerializer):
-    # ctr = PercentField()
-    # video_view_rate = PercentField()
+class DashboardManagedServiceSerializer(DashboardAccountCreationListSerializer):
     completion_rate = SerializerMethodField()
     viewability = SerializerMethodField()
-    # margin = SerializerMethodField()
 
     class Meta:
         model = AccountCreation
@@ -34,23 +37,11 @@ class DashboardManagedServiceAveragesSerializer(DashboardAccountCreationListSeri
             "viewability",
             "video_view_rate",
             "completion_rate",
-            # admin fields
-            # "margin"
         )
 
-    # def _fields_to_exclude(self):
-    #     return (
-    #         "account", "all_conversions", "average_cpm", "average_cpv",
-    #         "brand", "clicks", "cost", "cost_method", "currency_code", "end",
-    #         "impressions", "is_changed", "is_disapproved", "plan_cpm",
-    #         "plan_cpv", "sf_account", "start", "statistic_max_date",
-    #         "statistic_min_date", "status", "updated_at", "video_views",
-    #         "weekly_chart",
-    #     )
-
     def _get_stats(self, account_creation_ids):
-        stats = super()._get_stats(account_creation_ids)
-        # stats by AccountCreation
+        stats = {}
+        # stats by AccountCreation: viewability and completion rate
         creations = AccountCreation.objects.filter(id__in=account_creation_ids) \
             .annotate(
                 completion_rate=ExpressionWrapper(
@@ -66,59 +57,61 @@ class DashboardManagedServiceAveragesSerializer(DashboardAccountCreationListSeri
                         output_field=FloatField(),
                     )
                 ),
-                # TODO we can remove these
-                completion_views=Sum("account__campaigns__statistics__video_views_100_quartile"),
-                impressions=Sum("account__campaigns__statistics__impressions"),
             )
         for creation in creations:
-            stats[creation.id]['completion_rate'] = creation.completion_rate
-            stats[creation.id]['viewability'] = creation.viewability
+            creation_stats = stats.get(creation.id, {})
+            creation_stats['completion_rate'] = creation.completion_rate
+            creation_stats['viewability'] = creation.viewability
+            stats[creation.id] = creation_stats
 
-        # TODO for view rate
-        # video_views_impressions = defaultdict(lambda: defaultdict(int))
-        # queryset = Campaign.objects.filter(**{
-        #     self.CAMPAIGN_ACCOUNT_ID_KEY + "__in": account_creation_ids
-        # })
-        # with_ag_type = queryset.annotate(ag_type=Max("ad_groups__type"))
-        # for campaign in with_ag_type:
-        #     creation_id = campaign.account.account_creation.id
-        #     if campaign.ag_type == "In-stream":
-        #         video_views_impressions[creation_id]["impressions"] += campaign.impressions
-        #         video_views_impressions[creation_id]["views"] += campaign.video_views
+        # for view rate
+        video_views_impressions = defaultdict(lambda: defaultdict(int))
+        queryset = Campaign.objects.filter(**{
+            self.CAMPAIGN_ACCOUNT_ID_KEY + "__in": account_creation_ids
+        })
 
-        # # stats by AW Campaign
-        # campaigns = Campaign.objects.filter(account__account_creation__id__in=account_creation_ids) \
-        #     .values('id', 'salesforce_placement__goal_type_id',) \
-        #     .exclude(salesforce_placement__goal_type_id__isnull=True) \
-        #     .annotate(
-        #         sum_video_views=Sum("statistics__video_views"),
-        #         sum_impressions=Sum("statistics__impressions"),
-        #         sum_cost=Sum("statistics__cost"),
-        #         ordered_rate=F("salesforce_placement__ordered_rate"),
-        #         creation_id=F("account__account_creation__id"),
-        #         placement_id=F("salesforce_placement__id"),
-        #     )
-        # for campaign in campaigns:
-        #     goal_type_id = campaign.get('salesforce_placement__goal_type_id')
-        #     rate = campaign.get('ordered_rate', 0) or 0
-        #     if goal_type_id == SalesForceGoalType.CPV:
-        #         sum_video_views = campaign.get('sum_video_views', 0) or 0
-        #         revenue = sum_video_views * rate
-        #     elif goal_type_id == SalesForceGoalType.CPM:
-        #         sum_impressions = campaign.get('sum_impressions', 0) or 0
-        #         revenue = sum_impressions * rate / 1000 if sum_impressions and rate else 0
-        #     else:
-        #         revenue = 0
-        #     creation_id = campaign['creation_id']
-        #     # store revenues
-        #     self.add_to_creation_stats(stats, creation_id, 'revenues', revenue)
-        #     # store costs
-        #     cost = campaign['sum_cost']
-        #     self.add_to_creation_stats(stats, creation_id, 'costs', cost)
+        with_ag_type = queryset.annotate(ag_type=Max("ad_groups__type"))
+        for campaign in with_ag_type:
+            creation_id = campaign.account.account_creation.id
+            if campaign.ag_type == "In-stream":
+                video_views_impressions[creation_id]["impressions"] += campaign.impressions
+                video_views_impressions[creation_id]["views"] += campaign.video_views
+
+        queryset = queryset \
+            .values(self.CAMPAIGN_ACCOUNT_ID_KEY) \
+            .order_by(self.CAMPAIGN_ACCOUNT_ID_KEY)
+
+        dates = queryset.annotate(
+            statistic_min_date=Min("statistics__date"),
+            statistic_max_date=Max("statistics__date"),
+        )
+        dates_by_id = {
+            item[self.CAMPAIGN_ACCOUNT_ID_KEY]: pick_dict(item, ["statistic_min_date", "statistic_max_date"])
+            for item in dates
+        }
+
+        data = queryset.annotate(
+            start=Min("start_date"),
+            end=Max("end_date"),
+            **dashboard_managed_service_aggregator()
+        )
+        for account_data in data:
+            account_id = account_data[self.CAMPAIGN_ACCOUNT_ID_KEY]
+            account_data.update(dates_by_id[account_id])
+            dict_norm_base_stats(account_data)
+            account_data["video_views"] = video_views_impressions.get(account_id, {}) \
+                .get("video_views", account_data["video_views"])
+            account_data["video_impressions"] = video_views_impressions.get(account_id, {}) \
+                .get("video_impressions", account_data["video_impressions"])
+            dict_add_calculated_stats(account_data, calculated_stats=DASHBOARD_MANAGED_SERVICE_CALCULATED_STATS)
+            creation_stats = stats.get(account_id, {})
+            creation_stats.update(account_data)
+            stats[account_id] = creation_stats
 
         return stats
 
     def _get_daily_chart(self, account_creation_ids):
+        """override parent"""
         pass
 
     @staticmethod
@@ -137,42 +130,4 @@ class DashboardManagedServiceAveragesSerializer(DashboardAccountCreationListSeri
 
     def get_viewability(self, instance):
         value = self.stats.get(instance.id, {}).get('viewability', None)
-        # return instance.account.active_view_viewability
         return value
-
-    def get_margin(self, instance):
-        revenues = self.stats.get(instance.id, {}).get('revenues', [])
-        costs = self.stats.get(instance.id, {}).get('costs', [])
-        revenues = [revenue for revenue in revenues if isinstance(revenue, Number)]
-        costs = [cost for cost in costs if isinstance(cost, Number)]
-        revenue = sum(revenues)
-        cost = sum(costs)
-        profit = revenue - cost
-        margin = profit / revenue if revenue else None
-        margin *= 100
-        return margin
-
-
-class DashboardManagedServiceAveragesAdminSerializer(DashboardManagedServiceAveragesSerializer):
-    pass
-    # margin = PercentField()
-    # pacing = PercentField()
-    # cpv = FloatField()
-
-
-class BaseOpportunitySerializer(Serializer):
-    pass
-    # name = CharField(max_length=250)
-    # aw_cid = CharField()
-
-
-class DashboardManagedServiceOpportunitySerializer(
-    BaseOpportunitySerializer,
-    DashboardManagedServiceAveragesSerializer):
-    pass
-
-
-class DashboardManagedServiceOpportunityAdminSerializer(
-    BaseOpportunitySerializer,
-    DashboardManagedServiceAveragesAdminSerializer):
-    pass
--- dashboard/api/views/dashboard_managed_service.py
+++ dashboard/api/views/dashboard_managed_service.py
@@ -1,41 +1,28 @@
-from datetime import timedelta
-from numbers import Number
-
 from django.conf import settings
-from django.db.models import ExpressionWrapper, Q
 from django.db.models import BooleanField
-from django.db.models import F
-from django.db.models import Sum
 from django.db.models import Case
+from django.db.models import Q
+from django.db.models import Sum
 from django.db.models import When
-from django.db.models import FloatField
-from django.db.models import IntegerField
 
 from rest_framework.permissions import IsAdminUser
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
-from aw_creation.api.serializers import DashboardAccountCreationListSerializer
 from aw_creation.api.views.analytics.account_creation_list import OptimizationAccountListPaginator
 from aw_creation.models import AccountCreation
 from aw_reporting.demo.data import DEMO_ACCOUNT_ID
+from aw_reporting.models import Campaign
+from aw_reporting.models.ad_words.account import Account
 from aw_reporting.reports.pacing_report import PacingReport
 from aw_reporting.reports.pacing_report import get_pacing_from_flights
-from aw_reporting.models import Campaign
-from cache.models import CacheItem
-from dashboard.api.serializers.dashboard_managed_service import DashboardManagedServiceAveragesAdminSerializer
-from dashboard.api.serializers.dashboard_managed_service import DashboardManagedServiceAveragesSerializer
-from dashboard.api.serializers.dashboard_managed_service import DashboardManagedServiceOpportunityAdminSerializer
-from dashboard.api.serializers.dashboard_managed_service import DashboardManagedServiceOpportunitySerializer
+from dashboard.api.serializers.dashboard_managed_service import DashboardManagedServiceSerializer
 from dashboard.api.views.constants import DASHBOARD_MANAGED_SERVICE_CACHE_PREFIX
 from dashboard.utils import get_cache_key
+from rest_framework.generics import ListAPIView
 from userprofile.constants import UserSettingsKey
+from utils.api_paginator import CustomPageNumberPaginator
 from utils.datetime import now_in_default_tz
 from utils.permissions import or_permission_classes
 from utils.permissions import user_has_permission
-from aw_reporting.models.ad_words.account import Account
-from rest_framework.generics import ListAPIView
-from utils.api_paginator import CustomPageNumberPaginator
 
 
 class DashboardManagedServicePaginator(CustomPageNumberPaginator):
@@ -43,7 +30,7 @@ class DashboardManagedServicePaginator(CustomPageNumberPaginator):
 
 
 class DashboardManagedServiceAPIView(ListAPIView):
-    serializer_class = DashboardAccountCreationListSerializer
+    serializer_class = DashboardManagedServiceSerializer
     pagination_class = OptimizationAccountListPaginator
     permission_classes = (
         or_permission_classes(
@@ -52,9 +39,6 @@ class DashboardManagedServiceAPIView(ListAPIView):
         ),
     )
 
-    FIELDS = ['video_view_rate', 'ctr', 'video_quartile_100_rate', 'margin',
-              'pacing', 'cpv']
-
     CACHE_TTL = 60 * 30
 
     def get_queryset(self, **filters):
@@ -62,7 +46,7 @@ class DashboardManagedServiceAPIView(ListAPIView):
         visibility_filter = Q() \
             if user_settings.get(UserSettingsKey.VISIBLE_ALL_ACCOUNTS) \
             else Q(account__id__in=user_settings.get(UserSettingsKey.VISIBLE_ACCOUNTS))
-        queryset = AccountCreation.objects.all() \
+        queryset = AccountCreation.objects \
             .annotate(is_demo=Case(When(account_id=DEMO_ACCOUNT_ID, then=True),
                                    default=False,
                                    output_field=BooleanField(), ), ) \
@@ -71,73 +55,25 @@ class DashboardManagedServiceAPIView(ListAPIView):
                     & visibility_filter)
         return queryset.order_by("-is_demo", "is_ended", "-created_at")
 
+
+    # def get(self, request, *args, **kwargs):
+    #     return self.list(request, *args, **kwargs)
+
     # def get(self, request, *args, **kwargs):
     #     cache_key = self.get_cache_key(request.user.id)
     #     now = now_in_default_tz()
     #     try:
-    #         raise CacheItem.DoesNotExist
     #         cache = CacheItem.objects.get(key=cache_key)
     #         data = cache.value
     #         if cache.created_at < now - timedelta(seconds=self.CACHE_TTL):
-    #             data = self._get_data()
+    #             data = self.list(request, *args, **kwargs)
     #             cache.value = data
     #             cache.created_at = now
     #             cache.save()
     #     except CacheItem.DoesNotExist:
-    #         data = self._get_data()
-    #         # CacheItem.objects.create(key=cache_key, value=data)
-    #     return Response(data=data)
-
-    def _get_data(self):
-        aw_settings = self.request.user.get_aw_settings()
-        all_visible = aw_settings.get(UserSettingsKey.VISIBLE_ALL_ACCOUNTS, False)
-        if not all_visible:
-            visible_account_ids = aw_settings.get(UserSettingsKey.VISIBLE_ACCOUNTS, [])
-            accounts = self.get_visible_accounts(visible_account_ids)
-        # opportunities = PacingReport().get_opportunities(get={}, user=self.request.user,
-        #                                                  aw_cid=visible_account_ids,
-        #                                                  managed_service_data=True)
-        # averages_serializer_class, opportunity_serializer_class = self.get_serializer_classes()
-        # return {
-        #     'averages': averages_serializer_class(self.get_averages(opportunities)).data,
-        #     'items': opportunity_serializer_class(opportunities, many=True).data,
-        # }
-
-    def get_visible_accounts(self, visible_account_ids: list=[]):
-        # TODO build query to get all accounts
-        if not visible_account_ids:
-            pass
-        query = Account.objects.filter(id__in=visible_account_ids) \
-            .annotate(**self.ANNOTATIONS)
-        for account in query:
-            print(account)
-
-    def get_serializer_class(self):
-        """return different serializer depending on user perms"""
-        if self.request.user.is_staff:
-            return DashboardManagedServiceOpportunityAdminSerializer
-        return DashboardManagedServiceOpportunitySerializer
-
-    def get_averages(self, opportunities):
-        """
-        compute mean averages for select Opportunity Fields
-        """
-        values = {}
-        # TODO: add viewability/viewable_rate to default fields after VIQ2-428
-        fields = self.FIELDS
-        for opportunity in opportunities:
-            for field_name in fields:
-                field_values = values.get(field_name, [])
-                field_values.append(opportunity.get(field_name, 0))
-                values[field_name] = field_values
-        averages = {}
-        for field_name in fields:
-            field_values = values.get(field_name, [])
-            filtered = [value for value in field_values if isinstance(value, Number)]
-            averages[field_name] = sum(filtered) / len(field_values) \
-                if len(field_values) and sum(filtered) \
-                else None
-        return averages
+    #         data = self.list(request, *args, **kwargs)
+    #         CacheItem.objects.create(key=cache_key, value=data)
+    #     return data
 
     @staticmethod
     def get_cache_key(user_id):
