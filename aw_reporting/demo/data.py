5dddae56fa56728454345f4063d14614a319d847
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2020-08-18 15:34:37 +0300

    VIQ2-280: Update brand name

--- aw_reporting/demo/data.py
+++ aw_reporting/demo/data.py
@@ -8,7 +8,7 @@ DAYS_LEFT = 11
 DEMO_AD_GROUPS = (
     "Topics", "Interests", "Keywords", "Channels", "Videos"
 )
-DEMO_BRAND = "Demo Brand"
+DEMO_BRAND = "Acme CO"
 DEMO_COST_METHOD = ["CPM", "CPV"]
 DEMO_SF_ACCOUNT = "Initiative LA"
 DEMO_DATA_HOURLY_LIMIT = 13
