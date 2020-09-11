907590ab8d7410ce452cf283d0271975d1b6f6ae
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2020-09-11 19:29:41 +0300

    VIQ2-530: Remove unused import

--- aw_creation/api/views/dashboard/account_creation_campaigns.py
+++ aw_creation/api/views/dashboard/account_creation_campaigns.py
@@ -1,5 +1,4 @@
 from django.http import Http404
-from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
