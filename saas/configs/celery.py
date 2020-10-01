07148d313cca06bbfd5ceaab83e920410a74108e
k <o>
2020-10-01 11:52:35 -0700

    Add task to schedule account update tasks, add celery config, add tests

--- saas/configs/celery.py
+++ saas/configs/celery.py
@@ -171,6 +171,10 @@ CELERY_BEAT_SCHEDULE = {
         "task": "channel.tasks.ingest_ias_channels.ingest_ias_channels",
         "schedule": crontab(hour=21, minute=0),
     },
+    "performiq_google_ads_update": {
+        "task": "performiq.tasks.google_ads_scheduler.google_ads_update_scheduler",
+        "schedule": crontab(minute="*/10")
+    },
 }
 
 
@@ -189,6 +193,7 @@ class Queue:
     BRAND_SAFETY_VIDEO_PRIORITY = "brand_safety_video_priority"
     SCHEDULERS = "schedulers"
     IAS = "ias"
+    PERFORMIQ = "performiq"
 
 
 CELERY_ROUTES_PREPARED = [
@@ -203,6 +208,7 @@ CELERY_ROUTES_PREPARED = [
     ("segment.tasks.*", {"queue": Queue.SEGMENTS}),
     ("*_scheduler", {"queue": Queue.SCHEDULERS}),
     ("channel.tasks.ingest_ias_channels.*", {"queue": Queue.IAS}),
+    ("performiq.tasks.*", {"queue": Queue.PERFORMIQ}),
     ("*", {"queue": Queue.DEFAULT}),
 ]
 # dirty fix for celery. fixes AttributeError
