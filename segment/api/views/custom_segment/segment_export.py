0aeba0b7f6f1deefd9bbca658df0847d138ddcef
Anthony Silva <anthony.silva@channelfactory.com>
2020-11-30 12:16:21 -0800

    Added IsAdminUser permission to segment export view

--- segment/api/views/custom_segment/segment_export.py
+++ segment/api/views/custom_segment/segment_export.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser
 
 from segment.models import CustomSegment
 from segment.models import CustomSegmentFileUpload
@@ -16,7 +17,8 @@ class SegmentExport(APIView):
     permission_classes = (
         or_permission_classes(
             CustomSegmentOwnerPermission,
-            user_has_permission("userprofile.vet_audit_admin")
+            user_has_permission("userprofile.vet_audit_admin"),
+            IsAdminUser
         ),
     )
 
