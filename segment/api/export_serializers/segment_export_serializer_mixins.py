35cddc71b2910d16521405db70347a84616776ac
Servando Berna <servando.berna@channelfactory.com>
2020-12-01 13:06:13 -0800

    added last_vetted_at > 2020/07/01 condition for ctl exports Vetted column

--- segment/api/export_serializers/segment_export_serializer_mixins.py
+++ segment/api/export_serializers/segment_export_serializer_mixins.py
@@ -1,3 +1,5 @@
+import time
+
 from audit_tool.models import AuditAgeGroup
 from audit_tool.models import AuditContentType
 from audit_tool.models import AuditGender
@@ -6,6 +8,7 @@ from audit_tool.utils.audit_utils import AuditUtils
 from brand_safety.languages import LANGUAGES
 from es_components.iab_categories import HIDDEN_IAB_CATEGORIES
 from utils.brand_safety import map_brand_safety_score
+from es_components.constants import LAST_VETTED_AT_MIN_DATE
 
 
 class SegmentExportSerializerMixin:
@@ -73,10 +76,12 @@ class SegmentExportSerializerMixin:
         return vetted_value
 
     def get_vetted(self, obj):
-        vetted = "Y" \
-            if getattr(obj.task_us_data, "created_at", None) is not None \
-            else None
-        return vetted
+        date_last_vetted_at = getattr(obj.task_us_data, "last_vetted_at", None)
+        if not date_last_vetted_at:
+            return "N"
+        elif time.strptime(date_last_vetted_at, "%Y/%m/%d") < time.strptime(LAST_VETTED_AT_MIN_DATE, "%Y/%m/%d"):
+            return "N"
+        return "Y"
 
     def get_overall_score(self, obj):
         score = map_brand_safety_score(obj.brand_safety.overall_score)
