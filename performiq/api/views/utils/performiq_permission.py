5ffcb8839e00cfdf0338a20d3cc8abd1f38f16ba
Kenneth <ohtkenneth@gmail.com>
2020-12-07 13:10:23 -0800

    Fix permission name

--- performiq/api/views/utils/performiq_permission.py
+++ performiq/api/views/utils/performiq_permission.py
@@ -3,4 +3,4 @@ from rest_framework import permissions
 
 class PerformIQPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.has_perm("performiq")
+        return request.user.has_perm("userprofile.performiq")
