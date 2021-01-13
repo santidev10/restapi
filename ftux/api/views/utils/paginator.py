3f2927d74e39564cf7244be4ef19fe05771e9766
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-01-13 15:15:24 -0800

    Remove other app code, add initial migration

--- ftux/api/views/utils/paginator.py
+++
@@ -1,5 +0,0 @@
-from utils.api_paginator import CustomPageNumberPaginator
-
-
-class PerformIQPaginatior(CustomPageNumberPaginator):
-    page_size = 10
--- ftux/api/views/utils/performiq_permission.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-
-class PerformIQPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.has_perm("userprofile.performiq")
