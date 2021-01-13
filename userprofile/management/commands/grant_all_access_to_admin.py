0ed305cddb6253de31231625aa8cf907ba335857
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-01-13 15:03:15 -0800

    Add new permissions sync to management command that runs during deployment

--- userprofile/management/commands/grant_all_access_to_admin.py
+++ userprofile/management/commands/grant_all_access_to_admin.py
@@ -5,6 +5,7 @@ from django.core.management import BaseCommand
 
 from userprofile.permissions import PermissionGroupNames
 from userprofile.permissions import Permissions
+from userprofile.models import PermissionItem
 
 logger = logging.getLogger(__name__)
 
@@ -13,9 +14,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info("Start")
         Permissions.sync_groups()
+        PermissionItem.load_permissions()
         admin = get_user_model().objects.get(id=1)
         all_permissions = [perm for perm in dir(PermissionGroupNames) if not perm.startswith("__")]
         for perm in all_permissions:
             group_name = getattr(PermissionGroupNames, perm)
             admin.add_custom_user_group(group_name)
             logger.info("User %s has been granted access to permission %s.", admin.email, group_name)
+
