89736f5133d35ba8d5d3207ceb89c71084efff3b
Andrew <andrew@kabuto.com>
2021-01-21 13:56:53 -0800

    more robustness to reco engine checking 404 instead of string error code only

--- audit_tool/management/commands/audit_recommended_engine.py
+++ audit_tool/management/commands/audit_recommended_engine.py
@@ -221,7 +221,7 @@ class Command(BaseCommand):
         r = requests.get(url)
         data = r.json()
         if "error" in data:
-            if data["error"]["message"] in ["Invalid video.", "Not Found", "Requested entity was not found."]:
+            if (data["error"].get("code") and str(data["error"]["code"]) == "404") or data["error"]["message"] in ["Invalid video.", "Not Found", "Requested entity was not found."]:
                 avp.processed = timezone.now()
                 avp.clean = False
                 avp.save(update_fields=["clean", "processed"])
