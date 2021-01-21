39e9843ec03f98713d262c67bad5725e8e917af5
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-01-21 13:09:20 -0800

    Update missed tests with new permissions

--- aw_creation/api/views/analytics/performance_export.py
+++ aw_creation/api/views/analytics/performance_export.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from functools import partial
 
 from django.db.models import Sum
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_404_NOT_FOUND
 from rest_framework.views import APIView
