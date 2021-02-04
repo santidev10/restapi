01aa79e83a3bd91e6859b46c7586f61547b9d662
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-02-04 12:17:14 -0800

    Make test more robust

--- brand_safety/tests/test_pickled_data_dec.py
+++ brand_safety/tests/test_pickled_data_dec.py
@@ -1,4 +1,5 @@
 import time
+import os
 from unittest import mock
 
 from utils.unittests.test_case import ExtendedAPITestCase
@@ -20,14 +21,21 @@ class PickedDataDecoratorTestCase(ExtendedAPITestCase):
             test_func()
             mock_load.assert_not_called()
 
-    def test_refreshes(self):
+    def test_loads_data(self):
         """ Test that pickled data is loaded if time has not expired """
         expires = 60
+        fp = "test_loads_data"
+        open(fp, "w").close()
 
-        @pickled_data("test", expires)
+        @pickled_data(fp, expires)
         def test_func():
             pass
 
+        # Pickled data expires in 60, and should be loaded as time elapsed since creating file is <= 60 sec
         with mock.patch("brand_safety.auditors.utils.pickle.load") as mock_load:
             test_func()
             mock_load.assert_called_once()
+        try:
+            os.remove(fp)
+        except OSError:
+            pass
