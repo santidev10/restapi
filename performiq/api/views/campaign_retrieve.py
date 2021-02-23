0881a0da6a1a27da5cc4120dacefdd5ccfd9b6b5
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-02-23 11:25:45 -0800

    Add performiq permission class to ensure only users can access their own analysis results, add tests

--- performiq/api/views/campaign_retrieve.py
+++ performiq/api/views/campaign_retrieve.py
@@ -1,13 +1,25 @@
+from django.contrib.auth import get_user_model
+from rest_framework import permissions
 from rest_framework.generics import RetrieveAPIView
 
 from performiq.api.serializers import IQCampaignSerializer
 from performiq.models import IQCampaign
 from userprofile.constants import StaticPermissions
+from utils.views import get_object
+
+
+class PerformIQAnalysisOwnerPermission(permissions.BasePermission):
+    """ Check if user is created PerformIQ Analysis """
+    def has_permission(self, request, view):
+        analysis = get_object(IQCampaign, id=view.kwargs["pk"], message="Analysis not found")
+        has_permission = isinstance(request.user, get_user_model()) \
+            and request.user.has_permission(StaticPermissions.PERFORMIQ) and analysis.user == request.user
+        return has_permission
 
 
 class PerformIQCampaignRetrieveAPIView(RetrieveAPIView):
     queryset = IQCampaign.objects.all()
     serializer_class = IQCampaignSerializer
     permission_classes = (
-        StaticPermissions.has_perms(StaticPermissions.PERFORMIQ),
+        PerformIQAnalysisOwnerPermission,
     )
