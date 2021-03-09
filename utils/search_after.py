6e7150837031297669da2802fda73939b8a9be32
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-03-08 20:19:19 -0800

    Add source kwarg

--- utils/search_after.py
+++ utils/search_after.py
@@ -3,17 +3,18 @@ from operator import attrgetter
 from es_components.query_builder import QueryBuilder
 
 
-def search_after(query: QueryBuilder, manager, sort=None, size=1000):
+def search_after(query: QueryBuilder, manager, sort=None, size=1000, source=None):
     """
     Uses search after API to retrieve results using sort cursor
     :param query: Query to retrieve documents
     :param manager: ChannelManager or VideoManager object
     :param sort: Sort
-    :param size:
+    :param size: Batch size
+    :param source: Fields to retrieve
     :return:
     """
     sort = sort or {"main.id": {"order": "asc"}}
-    data = manager.search(query, limit=size).sort(sort).execute()
+    data = _search(manager, query, size, sort, source).execute()
     yield data
 
     # Set up attribute getters to get sort values to pass into search_after
@@ -23,8 +24,14 @@ def search_after(query: QueryBuilder, manager, sort=None, size=1000):
             last = [getter(data[-1]) for getter in getters]
         except (IndexError, AttributeError):
             return
-        data = manager.search(query, limit=size).sort(sort).extra(search_after=last).execute()
+        data = _search(manager, query, size, sort, source).extra(search_after=last).execute()
         if not data:
             return
         yield data
 
+
+def _search(manager, query, size, sort, source):
+    search = manager.search(query, limit=size).sort(sort)
+    if source:
+        search = search.source(source)
+    return search
