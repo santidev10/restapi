d3125c3e6989cc2b914c2979a0f8109d1f7b0627
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-03-17 13:39:29 -0700

    Remove deprecated keywords feature

--- keywords/api/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .keyword_aw_stats import KeywordAWStatsApiView
-from .keyword_export import KeywordListExportApiView
-from .keyword_list import KeywordListApiView
-from .keyword_retrieve_update import KeywordRetrieveUpdateApiView
