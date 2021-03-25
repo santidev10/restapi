5c15abe72cf8e094a31d95e78326b1f6f21e3d98
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-03-25 15:41:34 -0700

    Update segment sync endpoint to prepare script for execution on google ads

--- segment/api/views/custom_segment/segment_sync.py
+++ segment/api/views/custom_segment/segment_sync.py
@@ -1,10 +1,13 @@
+import json
+
+from django.conf import settings
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
 from performiq.models import Account
-from performiq.models import OAuthAccount
+from segment.models.constants import SegmentTypeEnum
 from segment.models import CustomSegment
 from segment.models.constants import Params
 from utils.views import get_object
@@ -13,11 +16,7 @@ from utils.views import get_object
 class SegmentSyncAPIView(APIView):
     def get(self, request, *args, **kwargs):
         """
-        Get data from viewiq to update placements on GoogleAds
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
+        Get data from ViewIQ to update placements on GoogleAds
         """
         cid = request.query_params.get("cid")
         try:
@@ -26,24 +25,15 @@ class SegmentSyncAPIView(APIView):
         except CustomSegment.DoesNotExist:
             raise Http404
         else:
-            # Get export file from s3 and extract urls
-            urls = ctl.s3.get_extract_export_ids(as_url=True)
             data = {
-                "urls": urls,
-                "ad_group_ids": ctl.params.get(Params.GoogleAds.AD_GROUP_IDS)
+                "code": self._get_code(ctl)
             }
             return Response(data)
 
-    def patch(self, request, *args, **kwargs):
-        # update synced status for ctl
-        ctl = get_object(CustomSegment, id=kwargs.get("pk"))
-        ctl.gads_synced = True
-        ctl.update_sync_history(request.data["name"], Params.GoogleAds)
-        ctl.save(update_fields=["gads_synced", "statistics"])
-        return Response()
-
     def post(self, request, *args, **kwargs):
-        """ Update ctl for gads sync """
+        """
+        Update CTL with CID and Adgroup Ids to update Google Ads placements
+        """
         data = request.data
         ctl = get_object(CustomSegment, id=kwargs["pk"])
         cid = get_object(Account, id=data.get(Params.GoogleAds.CID))
@@ -57,3 +47,29 @@ class SegmentSyncAPIView(APIView):
         ctl.params[Params.GoogleAds.GADS_SYNC_DATA] = sync_data
         ctl.save(update_fields=["params"])
         return Response()
+
+    def patch(self, request, *args, **kwargs):
+        """
+        Update Google Ads CTL sync status
+        """
+        ctl = get_object(CustomSegment, id=kwargs.get("pk"))
+        ctl.gads_synced = True
+        ctl.update_sync_history(request.data["name"], Params.GoogleAds)
+        ctl.save(update_fields=["gads_synced", "statistics"])
+        return Response()
+
+    def _get_code(self, ctl):
+        script_fp = "segment/utils/create_placements.js"
+        with open(script_fp, mode="r") as file:
+            func = file.read()
+        placement_type = SegmentTypeEnum(ctl.segment_type).name.capitalize()
+        placement_ids = ctl.s3.get_extract_export_ids()
+        ad_group_ids = ctl.params[Params.GoogleAds.AD_GROUP_IDS]
+        func = func\
+            .replace("{DOMAIN}", settings.HOST)\
+            .replace("{adGroupIds}", json.dumps(ad_group_ids))\
+            .replace("{placementIds}", json.dumps(placement_ids))\
+            .replace("{placementBuilderType}", f"newYouTube{placement_type}Builder")\
+            .replace("{placementIdType}", f"with{placement_type}Id") \
+            .replace("{placementRemovalType}", f"youTube{placement_type}s")
+        return func
--- segment/utils/create_placements.js
+++ segment/utils/create_placements.js
@@ -1,21 +1,70 @@
-// request code and ctl urls from viewiq, or csv from s3
+var SYNC_ENDPOINT = '{DOMAIN}' + '/api/v2/segments/sync/'
 
+function main() {
+  // get code from viewiq to eval here
+  // var code = UrlFetchApp.fetch();
+  // var data = getData();
+  // var adGroupIds = {adGroupIds}
+  // var placementIds = {placementIds}
+  var adGroupIds = [97595304713];
+  var placementIds = ["UCJ5v_MCY6GNUBTO8-D3XoAg"]
 
-function getadGroups(campaignName) {
-    var condition = 'CampaignName CONTAINS ' + "'" + campaignName + "'"
-    return AdsApp.videoadGroups().withCondition(condition).get()
+  var adGroupIterator = getAdGroups(adGroupIds);
+  //var ag = adGroupIterator.next();
+  //removeExistingPlacements(ag, false);
+  createPlacements(adGroupIterator, placementIds, config);
 }
 
-function createPlacements(adGroupIterator, urls) {
-    while (adGroupIterator.hasNext()) {
-        var adGroup = adGroupIterator.next()
-        createPlacement(adGroup, urls)
-    }
+function getData() {
+  var cid = AdsApp.currentAccount().getCustomerId().split("-").join("");
+  var url = SYNC_ENDPOINT + "?cid=" + cid;
+  var response = UrlFetchApp.fetch(url);
+  var data = JSON.parse(response.getContentText());
+  return data
 }
 
-function createPlacement(adGroup, urls) {
-    var builder = adGroup.videoTargeting().newPlacementBuilder();
-    urls.forEach(function(url) {
-        var operation = builder.withUrl(url).build();
-    })
+
+function getAdGroups(adGroupIds) {
+    return AdsApp.videoAdGroups().withIds(adGroupIds).get();
+}
+
+function createPlacements(adGroupIterator, placementIds) {
+  while (adGroupIterator.hasNext()) {
+    var adGroup = adGroupIterator.next();
+    var placementBuilder = adGroup.videoTargeting()['{placementBuilderType}']();
+
+    placementIds.forEach(function(id) {
+      placementBuilder['{placementIdType}'](id).build();
+    });
+  }
+}
+
+function removeExistingPlacements(adGroup) {
+  // Remove all existing placements before adding new ones
+  var placementsIterator = adGroup.videoTargeting()['{placementRemovalType}']().get();
+  while (placementsIterator.hasNext()) {
+    placementsIterator.next().remove();
+  }
+}
+
+function updateSyncStatus(ctl_id) {
+  var options = {
+    'muteHttpExceptions' : true,
+    'method': 'PATCH',
+    'payload': JSON.stringify({ ctl_id: ctl_id }),
+   	'contentType': 'application/json'
+  };
+
+  var resp = UrlFetchApp.fetch(SYNC_ENDPOINT, options);
+  var message;
+
+  if (resp.getResponseCode() == 200) {
+    message = JSON.parse(resp.getContentText());
+  } else {
+    message = {
+  	  'errorCode': resp.getResponseCode(),
+      'message': resp.getContentText()
+    };
+  }
+  return message;
 }
