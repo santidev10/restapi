c58fcae409ca3508ba12589ad9f743ba2fe24b41
Anthony Silva <anthony.silva@channelfactory.com>
2021-03-24 19:01:03 -0700

    added ParamsTemplate to __init__.py imports

--- segment/models/__init__.py
+++ segment/models/__init__.py
@@ -1,5 +1,6 @@
 from .custom_segment import CustomSegment
 from .custom_segment import CustomSegmentRelated
+from .custom_segment import ParamsTemplate
 from .custom_segment_file_upload import CustomSegmentFileUpload
 from .custom_segment_file_upload import CustomSegmentSourceFileUpload
 from .custom_segment_file_upload import CustomSegmentVettedFileUpload
