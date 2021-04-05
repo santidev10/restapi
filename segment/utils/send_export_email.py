59c83670b4b4f2eb1743a0dd20d7ca3e4850eb1c
Anthony Silva <anthony.silva@channelfactory.com>
2021-04-05 12:57:22 -0700

    Updated subject and body in build export email for feature rename

--- segment/utils/send_export_email.py
+++ segment/utils/send_export_email.py
@@ -4,8 +4,8 @@ from administration.notifications import send_html_email
 
 
 def send_export_email(recipients, export_title, download_url):
-    subject = "Custom Target List: {}".format(export_title)
-    text_header = "Your Custom Target List {} is ready".format(export_title)
+    subject = "Build: {}".format(export_title)
+    text_header = "Your Build {} is ready".format(export_title)
     text_content = "<a href={download_url}>Click here to download</a>".format(download_url=download_url)
     send_html_email(
         subject=subject,
