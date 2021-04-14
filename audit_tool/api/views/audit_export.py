c85abb59a4f4e85608c4a17d9c116b531c6cd581
Abe Grair <abe.grair@channelfactory.com>
2021-04-14 16:23:13 -0700

    - Fix the Audit report for Channel Meta Audits by re-aggregating the data from the video items for the channels that don't have enough checked videos count

--- audit_tool/api/views/audit_export.py
+++ audit_tool/api/views/audit_export.py
@@ -20,6 +20,7 @@ from audit_tool.models import AuditCountry
 from audit_tool.models import AuditExporter
 from audit_tool.models import AuditLanguage
 from audit_tool.models import AuditProcessor
+from audit_tool.models import AuditVideoMeta
 from audit_tool.models import AuditVideoProcessor
 from audit_tool.models import BlacklistItem
 from brand_safety.auditors.video_auditor import VideoAuditor
@@ -30,6 +31,7 @@ from utils.aws.s3_exporter import S3Exporter
 from utils.brand_safety import map_brand_safety_score
 from utils.permissions import user_has_permission
 from utils.utils import chunks_generator
+from utils.utils import remove_tags_punctuation
 
 
 class AuditExportApiView(APIView):
@@ -595,6 +597,8 @@ class AuditExportApiView(APIView):
             channel_ids.append(full_channel_id)
             if audit.params.get('do_videos'):
                 try:
+                    if len(cid.word_hits.get('processed_video_ids')) < audit.get_num_videos():
+                        self.aggregate_channel_word_hits(audit=audit, acp=cid)
                     video_count[full_channel_id] = len(cid.word_hits.get('processed_video_ids'))
                 # pylint: disable=broad-except
                 except Exception:
@@ -789,6 +793,52 @@ class AuditExportApiView(APIView):
                 audit.params['export_{}'.format(clean_string)] = s3_file_name
                 audit.save(update_fields=['params'])
         return s3_file_name, download_file_name
+
+    def aggregate_channel_word_hits(self, audit, acp):
+        if not isinstance(audit, AuditProcessor) or not isinstance(acp, AuditChannelProcessor):
+            return
+        acp.refresh_from_db()
+        avps = AuditVideoProcessor.objects.filter(audit=audit, channel=acp.channel)
+        if len(avps) > len(acp.word_hits.get('processed_video_ids')):
+            for avp in avps:
+                if not BlacklistItem.get(avp.video.video_id, BlacklistItem.VIDEO_ITEM):
+                    self.check_video_is_clean(audit, acp, avp)
+                else:
+                    self.append_to_channel(acp, avp, [avp.video_id], "bad_video_ids")
+
+    def get_video_word_hits(self, audit, acp, avp):
+        """
+        This is a helper function to the aggregate_channel_word_hits function to collect the word_hits data
+        from the video and append it to the channel's word_hits
+        """
+        if audit.params.get("do_videos"):
+            db_video_meta = AuditVideoMeta.objects.get(video=avp.video)
+            self.append_to_channel(acp, [avp.video_id], "processed_video_ids")
+            if db_video_meta.made_for_kids:
+                self.append_to_channel(acp, [avp.video_id], "made_for_kids")
+            if db_video_meta.age_restricted:
+                self.append_to_channel(acp, [avp.video_id], "age_restricted_videos")
+                self.append_to_channel(acp, [avp.video_id], "bad_video_ids")
+            if "inclusion" in avp.word_hits and len(avp.word_hits["inclusion"]) > 0:
+                self.append_to_channel(acp, avp.word_hits["inclusion"], "inclusion_videos")
+            if "exclusion" in avp.word_hits and len(avp.word_hits["exclusion"]) > 0:
+                self.append_to_channel(acp, avp.word_hits["exclusion"], "exclusion_videos")
+                self.append_to_channel(acp, [avp.video_id], "bad_video_ids")
+            if "exclusion_title" in avp.word_hits and len(avp.word_hits["exclusion_title"]) > 0:
+                self.append_to_channel(acp, avp.word_hits["exclusion_title"], "exclusion_videos")
+                self.append_to_channel(acp, [avp.video_id], "bad_video_ids")
+
+    def append_to_channel(self, acp, hits, node):
+        """
+        This is a helper function to the aggregate_channel_word_hits function
+        it adds the items in "hits" to the list "acp.word_hits[node]", and saves the acp object after the change
+        """
+        if node not in acp.word_hits:
+            acp.word_hits[node] = []
+        node_items_set = set(acp.word_hits[node])
+        node_items_set.update(hits)
+        acp.word_hits[node] = list(node_items_set)
+        acp.save(update_fields=["word_hits"])
 
     def export_keywords(self, audit, audit_id=None, export=None):
         if not audit_id:
