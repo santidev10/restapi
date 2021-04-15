f08c81455288e068b27cb59a3951bfa6573b09ff
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-04-15 16:01:58 -0700

    Return err response if oauth is not valid, make placement deletion more robust with exponential backoff

--- oauth/api/views/google_ads_oauth.py
+++ oauth/api/views/google_ads_oauth.py
@@ -37,32 +37,33 @@ class GoogleAdsOAuthAPIView(BaseGoogleOAuthAPIView):
 
     def handler(self, oauth_account):
         # Get Name of First MCC Account
+        is_err = False
         try:
             mcc_accounts, cid_accounts = get_accounts(oauth_account.refresh_token)
         except WebFault as e:
+            is_err = True
             fault_string = e.fault.faultstring
             if "AuthenticationError.NOT_ADS_USER" in fault_string:
                 fault_string = "AdWords account does not exist"
             return Response(status=HTTP_400_BAD_REQUEST,
                             data=dict(error=fault_string))
         except HttpAccessTokenRefreshError as e:
+            is_err = True
             ex_token_error = "Token has been expired or revoked"
             if ex_token_error in str(e):
                 return Response(status=HTTP_400_BAD_REQUEST,
                                 data=dict(error=ex_token_error))
         except GoogleAdsServerFault as e:
+            is_err = True
             error_strings = []
             for error in e.errors:
                 error_strings.append(error.errorString)
                 # no assigned cids/not an ads user
                 if error.errorString in ["AuthenticationError.CUSTOMER_NOT_FOUND", "AuthenticationError.NOT_ADS_USER"]:
-                    response = AWAuthSerializer(oauth_account).data
-                    return Response(status=HTTP_200_OK, data=response)
+                    return Response(status=HTTP_400_BAD_REQUEST, data=dict(err="You do not have a Google Ads account."))
                 if error.errorString == "AuthenticationError.OAUTH_TOKEN_INVALID":
                     return Response(status=HTTP_400_BAD_REQUEST, data=dict(error="Invalid OAuth token!"))
-
             return Response(status=HTTP_400_BAD_REQUEST, data={f"GoogleAds ServerFault: {', '.join(error_strings)}"})
-
         else:
             if mcc_accounts:
                 first = mcc_accounts[0]
@@ -77,3 +78,7 @@ class GoogleAdsOAuthAPIView(BaseGoogleOAuthAPIView):
                 status = HTTP_400_BAD_REQUEST
             google_ads_update_task.delay([oauth_account.id])
             return Response(data=response, status=status)
+        finally:
+            if is_err:
+                oauth_account.synced = True
+                oauth_account.save(update_fields=["synced"])
