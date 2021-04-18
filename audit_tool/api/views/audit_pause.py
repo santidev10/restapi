f2ba59ad47f138c002e8167847a7cf8764834e81
Andrew <andrew@kabuto.com>
2021-04-18 16:53:54 -0700

    when pausing/resuming, save the user who took the action

--- audit_tool/api/views/audit_pause.py
+++ audit_tool/api/views/audit_pause.py
@@ -18,6 +18,7 @@ class AuditPauseApiView(APIView):
         audit_id = query_params["audit_id"] if "audit_id" in query_params else None
         pause = strtobool(query_params["pause"]) if "pause" in query_params else True
         if audit_id:
+            user_id = request.user.id
             try:
                 audit = AuditProcessor.objects.get(id=audit_id, completed__isnull=True)
             # pylint: disable=broad-except
@@ -28,7 +29,11 @@ class AuditPauseApiView(APIView):
                 raise ValidationError("can not pause a CTL audit")
             if audit.temp_stop != pause:
                 audit.temp_stop = pause
-                audit.save(update_fields=['temp_stop'])
+                if pause == 1:
+                    audit.params["last_paused_by"] = user_id
+                else:
+                    audit.params["last_resumed_by"] = user_id
+                audit.save(update_fields=['temp_stop', 'params'])
             else:
                 raise ValidationError("invalid action")
             return Response(audit.to_dict())
