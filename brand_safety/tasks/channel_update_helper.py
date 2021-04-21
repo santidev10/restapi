76b1cb174cb5723de65400cbb773ac5691a303a5
Kenneth Oh <kenneth.oh@channelfactory.com>
2021-04-21 15:25:11 -0700

    Ignore brand safety section during timestamp update

--- brand_safety/tasks/channel_update_helper.py
+++ brand_safety/tasks/channel_update_helper.py
@@ -36,4 +36,4 @@ def channel_update_helper(scheduler, query: Q, queue: str, sort=("-stats.subscri
         update_rescore_channels = channel_manager.get(channel_ids, skip_none=True)
         for channel in channels:
             channel.brand_safety.rescore = False
-        channel_manager.upsert(update_rescore_channels)
+        channel_manager.upsert(update_rescore_channels, ignore_update_time_sections=[Sections.BRAND_SAFETY])
